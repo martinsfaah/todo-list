7d06856b4581a548da85ae3407387273e70834bf
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-13 08:13:51 -0300

    Requisito 7 e 8 feitos

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ const taskInput = document.getElementById('texto-tarefa');
 const taskButtonAdd = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 
+// Add task to list
 taskButtonAdd.addEventListener('click', () => {
   const taskItem = document.createElement('li');
   taskItem.innerText = taskInput.value;
   taskInput.value = '';
   taskList.appendChild(taskItem);
+});
+
+function clearSelected() {
+  const tasks = taskList.children;
+  for (let i = 0; i < tasks.length; i += 1) {
+    tasks[i].style.backgroundColor = 'white';
+  }
+}
+
+// Color clicked tasks
+taskList.addEventListener('click', (task) => {
+  clearSelected();
+  const selectedTask = task;
+  selectedTask.target.style.backgroundColor = 'rgb(128, 128, 128)';
 });
