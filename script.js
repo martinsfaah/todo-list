e6aaf8cc148cefa5e5f3bce1a2308fe7849b3111
Caio Lima <caiojlimah@gmail.com>
2021-08-13 13:01:04 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const createButton = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const tasksList = document.getElementById('lista-tarefas');
+const backColor = 'rgb(128, 128, 128)';
 
 function addTask() {
   const newLi = document.createElement('li');
@@ -10,3 +11,18 @@ function addTask() {
 }
 
 createButton.addEventListener('click', addTask);
+
+function clearBackground() {
+  const li = document.querySelectorAll('li');
+  for (let elem = 0; elem < li.length; elem += 1) {
+    li[elem].style.backgroundColor = '';
+  }
+}
+
+function changeBackgroundColor(event) {
+  clearBackground();
+  const li = event;
+  li.target.style.backgroundColor = backColor;
+}
+
+tasksList.addEventListener('click', changeBackgroundColor);
