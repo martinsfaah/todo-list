e4ebdab3f90a62f215f61fa4c00808a883144da2
Luan Vitor <luan_vittor@hotmail.com>
2021-08-14 17:08:25 -0300

    requisitos 8 ao 11

--- script.js
+++ script.js
@@ -13,13 +13,53 @@ function addLi() {
   ol.appendChild(li);
 }
 
+//ex 8
+
 let creatLi = document.querySelector("#lista-tarefas");
 creatLi.addEventListener("click", backColor);
+let remove = creatLi.children
+
 
 function backColor(event) {
-  if (event.target === creatLi){
-  } else {
+  for (elemento of remove) {
+    elemento.style.removeProperty('background-color');
+  }
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+//ex 9
+
+creatLi.addEventListener("dblclick", check);
+
+function check(event) {
+  if (event.target.className === 'classLi') {
+    event.target.className += ' completed';
+  } else if (event.target.className === 'classLi completed') {
+    event.target.classList.remove('completed');
+  }
+}
+
+// ex 10
+
+let botaoRemove = document.querySelector('#apaga-tudo');
+botaoRemove.addEventListener('click', removeAll);
+let listas = document.querySelectorAll('#lista-tarefas')
+
+function removeAll() {
+  for (let elemento of listas) {
+    elemento.innerHTML = ''
+  }
+}
+
+let botaoRemoveDone = document.querySelector('#remover-finalizados');
+botaoRemoveDone.addEventListener('click', removeDone);
+
+//ex 11
+
+function removeDone() {
+  let listas2 = document.querySelectorAll('.completed')
+  for (let elemento of listas2) {
+    elemento.remove()
   }
 }
 
