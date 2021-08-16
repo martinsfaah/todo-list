635cc5259811b3c587d85047a5c85d4584085b81
ZekaEu <zeka.surf@hotmail.com>
2021-08-16 18:25:38 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const addBtn = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const list = document.getElementById('lista-tarefas');
+const tasks = document.getElementsByTagName('li');
+
+function colorTask(e) {
+  for (let i = 0; i < tasks.length; i += 1) {
+    tasks[i].classList.remove('selected');
+  }
+  const actualTask = e.target;
+  actualTask.classList.add('selected');
+}
 
 function addTask() {
-  const task = document.createElement('li');
-  list.appendChild(task);
-  task.innerHTML = input.value;
-  input.value = "";
+  const taskLi = document.createElement('li');
+  list.appendChild(taskLi);
+  taskLi.addEventListener('click', colorTask);
+  taskLi.innerHTML = input.value;
+  input.value = '';
 }
 
 addBtn.addEventListener('click', addTask);
