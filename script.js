63d0b9805f882a5a42ff5131f5d6bf236281a32e
LuisFillipeSoares <jobluisfillipe@gmail.com>
2021-08-16 20:12:47 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ function criarTarefa() {
   let textoTarefa = newTarefaText.value
   if (textoTarefa) {
     let liTarefasList = document.createElement('li'); //lista tarefas
-    liTarefasList.className = "listaTarefas"
+    liTarefasList.id = "listaTarefas"
     liTarefasList.innerText = textoTarefa;
     listaTarefas.appendChild(liTarefasList);
     console.log(newTarefaText);
     newTarefaText.value = "";
     liTarefasList.addEventListener('click', colorirLista);
+    liTarefasList.addEventListener('dblclick', riscarElemento); 
   }
   else {
     alert('Não é possível acrescentar tarefas em branco');
@@ -26,9 +27,14 @@ function colorirLista(evento) {
 }
 
 function qualcor(colorir) {
-  let tarefas = document.getElementsByClassName('listaTarefas');
+  let tarefas = document.querySelectorAll('#listaTarefas');
   for (let index = 0; index < tarefas.length; index+=1) {
     tarefas[index].style.backgroundColor = 'transparent';
     colorir.style.backgroundColor = 'rgb(128, 128, 128)';
   }
+}
+
+function riscarElemento(evento) {
+  const teste = evento.target;
+  teste.classList.toggle('completed');
 }
