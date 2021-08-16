c6419308f3c014f72770e4e39b434dcd5ef9e6fe
MatheusBurthon91 <matatiaspatrizio@hotmail.com>
2021-08-16 15:46:33 -0300

    feito o requisito 3.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('hello world!');
+
+function criarInput() {
+  const section = document.querySelector('#section-principal');
+  const input = document.createElement('input');
+  input.placeholder = 'digite aqui sua tarefa';
+  input.type = 'text';
+  input.id = 'texto-tarefa';
+  section.appendChild(input);
+}
+criarInput();
