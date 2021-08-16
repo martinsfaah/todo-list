ceb33f714cb4ff0d491707900b9f1cd073c26d32
Thiago <thiago_ssfr@hotmail.com>
2021-08-16 19:09:49 -0300

    Requisito 9 e 10

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const input = document.getElementById('texto-tarefa');
 const ol = document.getElementById('lista-tarefas');
 const button = document.getElementById('criar-tarefa');
-const li = document.createElement('li');
+const btnApgTudo = document.getElementById('apaga-tudo');
+
+btnApgTudo.addEventListener('click', () => {
+  while (ol.firstChild) {
+    ol.removeChild(ol.lastChild);
+  }
+});
 
 button.addEventListener('click', () => {
   const li = document.createElement('li');
-  li.innerHTML = input.value; 
+  li.innerHTML = input.value;
   li.className = 'list';
   ol.appendChild(li);
   const list = document.querySelectorAll('.list');
   if (list.length === 1) {
-    li.className = 'list selected'
+    li.className = 'list selected';
   }
   input.value = '';
 });
@@ -19,25 +25,30 @@ function selectColor(event) {
   const list = document.querySelectorAll('.list');
   const selectCor = 'rgb(128, 128, 128)';
   const color = event.target;
-  for(let index = 0; index < list.length; index += 1){
+  for (let index = 0; index < list.length; index += 1) {
     list[index].style.background = 'white';
   }
-
   color.style.backgroundColor = selectCor;
-    
 }
 
 function selectTarefa(event) {
-  const list = document.createElement('li');
-  const listSelected = document.querySelector('.selected');  
-  if (list.length === 1) {
-    event.target.classList.add('selected');    
-  } else {
-    listSelected.classList.remove('selected');
-    event.target.classList.add('selected');
-  }
+  const listSelected = document.querySelector('.selected');
+
+  listSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+
   selectColor(event);
 }
-const listLi = document.getElementById('lista-tarefas');
-listLi.addEventListener('click', selectTarefa);
-//listLi.addEventListener('click', selectColor);
+
+ol.addEventListener('click', selectTarefa);
+ol.addEventListener('dblclick', (event) => {
+  const decoration = 'line-through solid rgb(0, 0, 0)';
+  const mudaText = event.target;
+  if (event.target.style.textDecoration === decoration) {
+    mudaText.style.textDecoration = 'none';
+    mudaText.classList.remove('completed');
+  } else {
+    mudaText.style.textDecoration = decoration;
+    mudaText.classList.add('completed');
+  }
+});
