314a61a9fdc7a851f55ce5d79760dfefb681d286
SaraRabelo <rabelorsara@gmail.com>
2021-08-16 17:04:27 -0300

    Solucionado 5a e 6a Questões

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const tagBody = document.querySelector('body');
+/*windows.onload =() =>{
+    const inputTexto = document.querySelector('#texto-tarefa');
+    inputTexto.addEventListener('keyup', (evento) => {
+    let texto = evento.target.value;
+    });
+};*/
+
+const tagTarefas = document.querySelector('#tarefas');
 const btCriarTarefas = document.createElement('button');
+const inputTexto = document.querySelector('#texto-tarefa')
 btCriarTarefas.id = 'criar-tarefa';
+btCriarTarefas.innerHTML = 'Criar Tarefas';
+tagTarefas.appendChild(btCriarTarefas);
+btCriarTarefas.addEventListener('click', (evento) => {
+    const tagOl = document.querySelector('ol');
+    const tagLi = document.createElement('li');
+    tagLi.className = 'item';
+    tagLi.innerText = inputTexto.value;
+    tagOl.appendChild(tagLi);
+    inputTexto.value = '';
+});
