68477947252f39e4ef42f77c92e9621ba0dd1a91
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-08-16 16:48:12 -0300

    Requisitos  7 e 8

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const list = document.getElementById('lista-tarefas');
 const buttonAdd = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
+let line = document.querySelector('li');
 
-function selectList () {
-  const lineList = document.querySelector('li');
-  lineList.style.background = 'rgb(128, 128, 128)';
-
-}
-list.addEventListener('click', selectList);
-
-function insertList() {    
+function insertList() {
   const tarefa = document.createElement('li');
+  tarefa.className = 'lineList';
   tarefa.innerText = input.value;
   list.appendChild(tarefa);
+  tarefa.addEventListener('click', selectList);
   input.value = '';
 }
 
 buttonAdd.addEventListener('click', insertList);
+
+function selectList(event) {
+  const lineListClass = document.querySelectorAll('.lineList');
+  for (let index = 0; index < lineListClass.length; index += 1) {
+    lineListClass[index].style.background = '';
+  }  
+ event.target.style.background = 'rgb(128, 128, 128)';
+}
+
