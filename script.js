a8dd2f269965ae2a43683e06f4bf9049e1e0548a
João Felipe P <jf.pelliccione@gmail.com>
2021-08-16 15:26:39 -0300

    Conclusão do requisito 6.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-window.onload = function () {
-  addTask();
-}
-
-//Criando função que  será alocada no botão "Adicionar Tarefa":
+// Criando função que  será alocada no botão "Adicionar Tarefa":
 function addTask() {
   const addTaskBtn = document.getElementById('criar-tarefa');
 
-  addTaskBtn.addEventListener('click', function () {
-    const taskList  = document.getElementById('lista-tarefas');
-    const inputValue = document.getElementById('texto-tarefa').value;
-    const newLi = document.createElement('li');
-    taskList.appendChild(newLi);
-    const taskListLastElementChild  = taskList.lastElementChild;
-    taskListLastElementChild.innerHTML = inputValue;
-    document.getElementById('texto-tarefa').value = '';
-  })
-  }
+addTaskBtn.addEventListener('click', function () {
+  const taskList = document.getElementById('lista-tarefas');
+  const inputValue = document.getElementById('texto-tarefa').value;
+  const newLi = document.createElement('li');
+  taskList.appendChild(newLi);
+  const taskListLastElementChild = taskList.lastElementChild;
+  taskListLastElementChild.innerHTML = inputValue;
+  document.getElementById('texto-tarefa').value = '';
+})
+}
+
+window.onload = function () {
+  addTask();
+};
