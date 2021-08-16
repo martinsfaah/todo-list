d7a3daae2d3383ad47a4142049f5c63878c4ce47
Alex <alex_cubas1.0@hotmail.com>
2021-08-16 16:27:52 -0300

    exercicio 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-let textoTarefa = document.body
+function listaTarefas () {
+  let textoTarefa = document.querySelector("#texto-tarefa")
+  let criarTarefa = document.querySelector("#criar-tarefa")
+  let listaTarefas = document.querySelector("#lista-tarefas")
+ 
+
+  criarTarefa.addEventListener("click", function() {
+    let lista = document.createElement("li")
+    listaTarefas.appendChild(lista)
+    lista.innerHTML = textoTarefa.value
+  console.log(listaTarefas)
+    textoTarefa.value = "";
+  })
+}
+
+listaTarefas()
+// input id="texto-tarefa" type="text">
+//   <button id="criar-tarefa"></button>
+//   <ol id="lista-tarefas"></ol>
+
