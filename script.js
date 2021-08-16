d9d098082dda04bc21c8c36b1d5c5ebc5ab33e79
Igor Campos Moreira <igorcampos200@gmail.com>
2021-08-16 17:44:55 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-window.onload = function init(){
-
-
+window.onload = function init() {
 }
 
-let setTask = document.getElementById("criar-tarefa");
+const setTask = document.getElementById("criar-tarefa");
 setTask.addEventListener("click", form);
 
-function form(event){
-  
-  let input = document.getElementById("texto-tarefa");
+function form() {
+  const input = document.getElementById("texto-tarefa");
   let task = input.value;
    
-  document.getElementById("lista-tarefas").innerHTML += "<li>"+ task +"</li>";
+  document.getElementById("lista-tarefas").innerHTML += "<li id='item'>" + task + "</li>";
   input.value = '';
+  
+  const changeColor = document.getElementById("lista-tarefas");
+
+  for(let index = 0;index < changeColor.children.length; index +=1 ){
+  changeColor.children[index].addEventListener("click", change);
+  }
+}
+
+function change(event){
+  const changeColor = document.getElementById("lista-tarefas");
+  for (let index = 0 ;index < changeColor.children.length ; index +=1){
+    
+    changeColor.children[index].style.backgroundColor= "white";
+
+  }
+
+event.target.style.backgroundColor = " rgb(128, 128, 128)"
+
 }
