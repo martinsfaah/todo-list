42b221c3df3bddb3e8a65fc6d8449d6119cd0bf2
Emily Carvalho <nilsen.loli.emily@gmail.com>
2021-08-16 14:42:38 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const header = document.querySelector('header')
+const subtitulo = document.createElement('p');
+subtitulo.innerHTML = "Clique duas vezes em um item para marcá-lo como completo";
+subtitulo.id = 'funcionamento';
+header.appendChild(subtitulo);
