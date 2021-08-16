bee42f60311c3d8a2eb915b0810f2fe894e0e1b5
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-08-16 14:59:44 -0400

    Resolve requisito 4 projeto todo-list. Rodando perfeitamente no npm test

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ const callScript = document.getElementById('script');
 const callParent = callScript.parentNode;
 
 function addHeaderAndParagraph() {
-    const newHeader = document.createElement('header');
-    const newParagraph = document.createElement('p');
-    newParagraph.id = 'funcionamento';
-    newParagraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
-    newHeader.innerHTML = 'Minha Lista de Tarefas';
-    callParent.insertBefore(newHeader, callScript);
-    callParent.insertBefore(newParagraph, callScript);
+  const newHeader = document.createElement('header');
+  const newParagraph = document.createElement('p');
+  newParagraph.id = 'funcionamento';
+  newParagraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+  newHeader.innerHTML = 'Minha Lista de Tarefas';
+  callParent.insertBefore(newHeader, callScript);
+  callParent.insertBefore(newParagraph, callScript);
 }
 addHeaderAndParagraph();
 
 // Requisito 3 - Adicione um input com o id="texto-tarefa" onde a pessoa usuária poderá digitar o nome do item que deseja adicionar à lista.
 function createInput() {
-    const newInput = document.createElement('input');
-    newInput.id = 'texto-tarefa';
-    callParent.insertBefore(newInput, callScript);
+  const newInput = document.createElement('input');
+  newInput.id = 'texto-tarefa';
+  callParent.insertBefore(newInput, callScript);
 }
 createInput();
 
+// Adicione uma lista ordenada de tarefas com o id="lista-tarefas"
+function addOl() {
+  const newOl = document.createElement('ol');
+  newOl.id = 'lista-tarefas';
+  callParent.insertBefore(newOl, callScript);
+}
+addOl();
