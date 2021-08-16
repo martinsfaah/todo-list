ec25d21f4faaa9e7fcd14fb505491930a55d7f31
renatozr <renatozr07@gmail.com>
2021-08-16 17:45:47 -0300

    requisito 9 completo

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-// requisito 5 e 7 e 8
+// requisito 5, 7, 8 e 9
 const inputTextoTarefa = document.getElementById('texto-tarefa');
 const btnCriarTarefa = document.getElementById('criar-tarefa');
 
 const listaTarefas = document.getElementById('lista-tarefas');
 const tarefas = document.getElementsByClassName('tarefa');
 
+function completeTarefa(event) {
+  const dblclickedItem = event.target;
+
+  if (dblclickedItem.className.includes(' completed')) {
+    dblclickedItem.className = dblclickedItem.className.replace(' completed', '');
+  } else {
+    dblclickedItem.className += ' completed';
+  }
+}
+
 function selectTarefa(event) {
   for (let i = 0; i < tarefas.length; i += 1) {
-    if (tarefas[i].className === 'tarefa selected') {
-      tarefas[i].className = 'tarefa';
+    if (tarefas[i].className.includes(' selected')) {
+      tarefas[i].className = tarefas[i].className.replace(' selected', '');
     }
   }
 
   const clickedItem = event.target;
   clickedItem.className += ' selected';
 }
+
 function criaTarefa() {
   const tarefa = document.createElement('li');
   tarefa.innerHTML = inputTextoTarefa.value;
   tarefa.className = 'tarefa';
   tarefa.addEventListener('click', selectTarefa);
+  tarefa.addEventListener('dblclick', completeTarefa);
 
   listaTarefas.appendChild(tarefa);
   inputTextoTarefa.value = '';
