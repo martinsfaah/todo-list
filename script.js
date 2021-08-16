1d8e7f760b2082dbc1bf93aa418667a5f4f35fc9
herzogcassia <cassiahherzog@gmail.com>
2021-08-16 17:09:11 -0300

    requisito 4

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+ function createListOrder() {
+  const listOfOrder = document.querySelector("#lista-tarefas")
+  const createOrder = documen.querySelector("#criar-tarefa")
+  const inputButton = document.querySelector("texto-tarefa")
+  const elementList = document.createElement("li");
+listOfOrder.appendChild(elementList);
+   addEventListener(createOrder).value;
+ }
