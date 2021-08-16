26ea7e3952b8846b3ebf497902ab124a1c8f5cd8
Bruno Miranda Pereira <brunompe@gmail.com>
2021-08-16 16:57:45 -0400

    Requirement 03 to 06 - done

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
+const main = document.querySelector('body');
+let ol = document.getElementById('lista-tarefas');
+let btn = document.createElement('BUTTON');
+
+
+
+// Cria Header
 function createHeader () {
   const header = document.createElement('header');
   header.innerText = "Minha Lista de Tarefas";
-  document.querySelector('body').appendChild(header);
+  main.appendChild(header);
 }
 
 createHeader();
 
+// Cria Small Text
 function createSmallText () {
   const smallText = document.createElement('div');
   smallText. innerText = 'Clique duas vezes em um item para marcá-lo como completo';
   smallText.id = 'funcionamento';
-  document.querySelector('body').appendChild(smallText);
+  main.appendChild(smallText);
 }
 
 createSmallText();
+
+// Cria Input Box
+function createInputBox () {
+  const inputBox = document.createElement('input');
+  inputBox.id = 'texto-tarefa';
+  main.appendChild(inputBox);
+}
+
+createInputBox();
+
+//Cria Button
+function createInputBtn() {
+  btn.innerHTML = 'Add Task';
+  btn.id = 'criar-tarefa';
+  main.appendChild(btn);
+  btn.addEventListener('click', createTaskList);
+  }
+
+createInputBtn();
+
+//Cria OL - HTML
+function createOL () {
+  const ol = document.createElement('ol');
+  ol.id = 'lista-tarefas';
+  main.appendChild(ol);
+}
+
+createOL();
+
+// Cria a lista
+function createTaskList () {
+  let inputText = document.getElementById('texto-tarefa');
+  const li = document.createElement('li');
+  li.innerHTML = inputText.value;
+  document.getElementById('lista-tarefas').appendChild(li);
+  inputText.value = '';
+}
+
 
