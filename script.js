3da91ea73fbdc7c17815af80857b7469eaf1d9a0
MuriloAmaral <muralha2000@gmail.com>
2021-08-16 19:24:27 -0300

    requisito 11 e 9

--- script.js
+++ script.js
@@ -1,40 +1,58 @@
-let inputTask = document.getElementById("texto-tarefa");
-let buttonCreateTask = document.getElementById("criar-tarefa");
-let taskList = document.getElementById("lista-tarefas");
-let buttonEraseList = document.getElementById("apaga-tudo");
-let items = document.getElementsByTagName('li');
-
-buttonCreateTask.addEventListener('click', createTask);
-taskList.addEventListener('click', changeColor);
-taskList.addEventListener('dblclick', completeTask);
-buttonEraseList.addEventListener('click', eraseList);
-
-function createTask (){
-    let taskValue = inputTask.value;
-    let newTask = document.createElement("li");
-    newTask.innerHTML = taskValue;
-    taskList.appendChild(newTask);
-    inputTask.value = "";
-}
-
-function changeColor(event) {
-  let selected = document.querySelector('.selected');
-  if (selected != null) {
-    selected.classList.remove('selected');
-  }
-  let task = event.target;
-  task.className = 'selected';
-}
-
-function completeTask(event) {
-    let completed = document.querySelector('.completed');
-    if (completed != null) {
-      completed.classList.remove('completed');
-    }
-    let task = event.target;
-    task.className = 'completed';
-  }
+let inputTasks = document.getElementById("texto-tarefa");
+  let createTask = document.getElementById("criar-tarefa");
+  let taskList = document.getElementById("lista-tarefas");
+  let body = document.querySelector('body');
   
-function eraseList(){
-    taskList.innerHTML = '';
-}
+  createTask.addEventListener("click", adicionaTarefa);
+  function adicionaTarefa (){
+      let taskValue = inputTasks.value;
+      let newTask = document.createElement("li");
+      newTask.innerHTML = taskValue;
+      taskList.appendChild(newTask);
+      inputTasks.value = "";
+  }
+  function addSelected(element){
+      let tasks = Array.from(taskList.children);
+      for (let index = 0; index < tasks.length; index += 1){
+          let actualTask = tasks[index];
+          if(actualTask.classList.contains("selected")){
+              actualTask.classList.remove("selected");
+          }
+      }
+      element.classList.add("selected");
+  }
+  function checkItem(originEvent) {
+    const element = originEvent.target;
+    if (element.parentNode.id === 'lista-tarefas') {
+      addSelected(element);
+    }
+  }
+  body.addEventListener('click', checkItem);
+  function markCompleted(element) {
+    if (element.classList.contains('completed')) {
+      element.classList.remove('completed');
+    } else {
+      element.classList.add('completed');
+    }
+  }
+  function checkItem2(originEvent) {
+    let element = originEvent.target;
+    if (element.parentNode.id === "lista-tarefas") {
+      markCompleted(element);
+    }
+  }
+  body.addEventListener("dblclick", checkItem2);
+  function apagaLista(){
+     taskList.innerText = "";
+  }
+  document.getElementById("apaga-tudo").addEventListener("click", apagaLista);
+
+  function removeFinalizados(){
+    let arrayList = Array.from(taskList.children);
+      for(let index = 0; index < arrayList.length; index += 1){
+          if(arrayList[index].classList.contains("completed")){
+              taskList.removeChild(arrayList[index]);
+          }
+      }
+  }
+  document.getElementById("remover-finalizados").addEventListener("click", removeFinalizados); 
