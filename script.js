1514df3bb00e9329d938fccd7b74e4ccde48ecfe
Rafael <grs.rafaelgomes@gmail.com>
2021-08-16 14:59:12 -0300

    Requisito - 02

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //Requisito - 01
-
+ //HEADER
 let header = document.createElement("header");
 header.innerText = "Minha Lista de Tarefas";
-
 document.body.appendChild(header);
+
+// Requisito - 02
+// Parágrafo de HEADER.
+let headerP = document.createElement("p");
+headerP.innerText = "Clique duas vezes em um item para marcá-lo como completo";
+headerP.id = "funcionamento";
+
+document.querySelector("header").appendChild(headerP);
+
+// Requisito - 03
