c0bbf500b2f0083416074f3b60d4561e4ccff8e0
Júnior Zanoni <zanoni.odenir@gmail.com>
2021-08-16 18:12:04 -0300

    9-requisito

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Cria uma tarefa baseada no input
 const btnCriarTarefa = document.getElementById('criar-tarefa');
 function criarTarefa() {
   const listaTarefas = document.getElementById('lista-tarefas');
@@ -8,6 +7,7 @@ function criarTarefa() {
   document.getElementById('texto-tarefa').value = '';
 }
 btnCriarTarefa.addEventListener('click', criarTarefa);
+// Cria uma tarefa baseada no input
 
 // Altera a cor de fundo ao clicar sobre uma tarefa e restringe a apenas uma seleção
 function trocaCor() {
@@ -17,11 +17,20 @@ function trocaCor() {
   }
   this.style.backgroundColor = 'rgb(128, 128, 128)';
 }
+
+// Risca a tarefa após dois cliques
+function tarefaConcluida() {
+  if (this.className !== 'completed') {
+    this.className = 'completed';
+  } else this.classList.remove('completed');
+}
+
 // Atualiza a lista de tarefas após adicionar
 function atualizaLista() {
   const lista = document.querySelectorAll('li');
   for (let i = 0; i < lista.length; i += 1) {
     lista[i].addEventListener('click', trocaCor);
+    lista[i].addEventListener('dblclick', tarefaConcluida);
   }
 }
 btnCriarTarefa.addEventListener('click', atualizaLista);
