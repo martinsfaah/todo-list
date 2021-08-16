178971592828ba0d2aaf7d01c6ed8f77ed2c55b7
João V. Dias <diasvictorj@gmail.com>
2021-08-16 17:34:30 -0300

    Quarta e Quinto Requisito

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Inicializando o botão*/
+let botao = document.getElementById('criar-tarefa');
+botao.addEventListener('click', criaTarefa);
+
+/* Inicializando textbox*/
+let caixaDeTexto = document.getElementById('texto-tarefa')
+
+
+/* Função para criar criar a tarefa */
+function criaTarefa(){
+    let textoInserido = caixaDeTexto.value;
+    let itemLista = document.getElementById('lista-tarefa');
+    let item = document.createElement('li');
+
+    item.innerText = textoInserido; 
+    itemLista.appendChild(item);
+    caixaDeTexto.value = '';
+}
