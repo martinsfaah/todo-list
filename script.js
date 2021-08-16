1a4b67b97f70141d5e33a1abd185a8fc8f75eae1
arturbacilla <artur.bacilla1@gmail.com>
2021-08-16 18:10:53 -0300

    11th requisite: remove completed

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 let taskList = document.querySelector('#lista-tarefas');
 const addTaskBtn = document.querySelector('#criar-tarefa');
 const clearListBtn = document.querySelector('#apaga-tudo');
+const clearCompletedBtn = document.querySelector('#remover-finalizados');
 let fullTaskLi = document.querySelectorAll('ol#lista-tarefas li');
 
 addTaskBtn.addEventListener('click', () => {
   const taskToAddText = document.querySelector('#texto-tarefa');
   if (taskToAddText.value != '') {
-  const taskLi = document.createElement('li');
-  taskLi.innerHTML = taskToAddText.value;
-  taskLi.className = 'task';
-  taskList.appendChild(taskLi);
-  taskToAddText.value = '';
-  makeTasksClicable(taskLi);
-  toggleTaskCompletion(taskLi);
-  fullTaskLi = document.querySelectorAll('ol#lista-tarefas li');
+    const taskLi = document.createElement('li');
+    taskLi.innerHTML = taskToAddText.value;
+    taskLi.className = 'task';
+    taskList.appendChild(taskLi);
+    taskToAddText.value = '';
+    makeTasksClicable(taskLi);
+    toggleTaskCompletion(taskLi);
+    fullTaskLi = document.querySelectorAll('ol#lista-tarefas li');
   } else {
-    alert("Tarefa invállida");
+    alert('Tarefa invállida');
   }
 });
 
@@ -28,20 +29,28 @@ function clearTasksSelection() {
 
 function makeTasksClicable(whichTask) {
   whichTask.addEventListener('click', (task) => {
-      const thisTaskClasses = task.target.classList;
-      if (!thisTaskClasses.contains('selected')) {
-        clearTasksSelection();
-        thisTaskClasses.add('selected');
-      }
-    })
+    const thisTaskClasses = task.target.classList;
+    if (!thisTaskClasses.contains('selected')) {
+      clearTasksSelection();
+      thisTaskClasses.add('selected');
+    }
+  });
 }
 
 function toggleTaskCompletion(whichTask) {
   whichTask.addEventListener('dblclick', (task) => {
-    task.target.classList.toggle("completed");
-  })
+    task.target.classList.toggle('completed');
+  });
 }
 
 clearListBtn.addEventListener('click', () => {
   taskList.innerHTML = '';
+});
+
+clearCompletedBtn.addEventListener('click', () => {
+  for (const comp of fullTaskLi) {
+    if (comp.classList.contains('completed')) {
+      comp.parentNode.removeChild(comp);
+    }
+  }
 });
