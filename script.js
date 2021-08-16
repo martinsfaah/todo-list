a8a53423d2d7f64ac119e9e641a642de4689d2f4
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-08-16 18:38:37 -0300

    Requisitos  7 e 8

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const list = document.getElementById('lista-tarefas');
 const buttonAdd = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
-let line = document.querySelector('li');
+const buttonClearAll = document.getElementById('apaga-tudo');
 
 function insertList() {
   const tarefa = document.createElement('li');
@@ -9,6 +9,7 @@ function insertList() {
   tarefa.innerText = input.value;
   list.appendChild(tarefa);
   tarefa.addEventListener('click', selectList);
+  tarefa.addEventListener('dblclick', lineThrough);
   input.value = '';
 }
 
@@ -18,7 +19,25 @@ function selectList(event) {
   const lineListClass = document.querySelectorAll('.lineList');
   for (let index = 0; index < lineListClass.length; index += 1) {
     lineListClass[index].style.background = '';
-  }  
- event.target.style.background = 'rgb(128, 128, 128)';
+  }
+  event.target.style.background = 'rgb(128, 128, 128)';
 }
 
+function lineThrough(event) {
+  const classe2 = document.querySelector('.completed');
+
+  event.target.classList.add('completed');
+  classe2.addEventListener('dblclick', removeCompleted);
+}
+
+function removeCompleted(event) {
+  event.target.classList.remove('completed');
+}
+
+function clearList() {
+  const list = document.querySelectorAll('.lineList');
+  for (let index of list) {
+    index.parentNode.removeChild(index);
+  }
+}
+buttonClearAll.addEventListener('click', clearList);
