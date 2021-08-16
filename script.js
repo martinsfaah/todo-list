e5217461e35165520995bf95d975aba2a5a8f755
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2021-08-16 16:30:58 -0300

    Requisito 5 e 6 ok

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function createTag(tagName) {
   const tag = document.createElement(`${tagName}`);
-  document.body.appendChild(tag);
+  return document.body.appendChild(tag);
 }
+
+const btnCreateItem = document.querySelector('#criar-tarefa');
+const listItems = document.querySelector('#lista-tarefas');
+// const textInput = document.getElementById('texto-tarefa');
+
+function createListItems() {
+  const item = listItems.appendChild(createTag('li'));
+  const itemText = document.querySelector('#texto-tarefa').value;
+  item.innerText = itemText;
+  document.getElementById('texto-tarefa').value = '';
+  return item;
+}
+
+btnCreateItem.addEventListener('click', createListItems);
