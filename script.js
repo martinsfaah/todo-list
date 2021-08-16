8c2cb4a05afbaa3476df6fa0d4595cf8c81656ea
Braien <braienmp@outlook.com>
2021-08-16 16:25:56 -0300

    cria evento para riscar tarefas concluidas e para desfazer o risco

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const btnAddTask = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
-let tasks = taskList.children;
 
 function selectTask(event) {
   let selectedTask = document.getElementById('selected-task');
   if (selectedTask !== null) {
     selectedTask.id = '';
-  };
+  }
   selectedTask = event.target;
   selectedTask.id = 'selected-task';
+}
+
+function completeTask(event) {
+  let task = event.target;
+  if (task.className === '') {
+  task.className = 'completed';
+  } else {
+    task.className = '';
+  }
 }
 
 function addTask() {
   const task = document.createElement('li');
   task.innerHTML = inputTask.value;
   task.addEventListener('click', selectTask);
+  task.addEventListener('dblclick', completeTask);
   taskList.appendChild(task);
   inputTask.value = '';
 }
 
 btnAddTask.addEventListener('click', addTask);
-
