7cd89139cd9cc73e604ba0d55397e5a12ec8f597
Guilherme Polippo <luispolippo@hotmail.com>
2021-08-16 15:39:17 -0300

    Implementado requisito 7 e 8

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
+function selectItem(event) {
+  const listItens = document.getElementsByClassName('list-item');
+
+  for (let i = 0; i < listItens.length; i += 1) {
+    const item = listItens[i];
+    if (item.classList.contains('selected')) {
+      item.classList.remove('selected');
+    }
+  }
+
+  const listItem = event.target;
+  listItem.classList.add('selected');
+}
+
+function addListItemListener() {
+  const listItens = document.getElementsByClassName('list-item');
+  for (let i = 0; i < listItens.length; i += 1) {
+    const item = listItens[i];
+    item.addEventListener('click', selectItem);
+  }
+}
+
 function createListItem(value) {
   const listItem = document.createElement('li');
   const list = document.getElementById('lista-tarefas');
+  listItem.classList.add('list-item');
   listItem.innerHTML = value;
   list.appendChild(listItem);
+  addListItemListener();
 }
 
 function addTask() {
