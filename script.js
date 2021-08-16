bbc62826c5966952f77d339dd660bc86ddeae336
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-08-16 15:08:08 -0300

    itens 5 e 6

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-// 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, 
+// 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão,
 // um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
+const oList = document.createElement('ol');
 
-const oList = document.createElement('ol')
-document.querySelector('.lista-tarefas-section').appendChild(oList)
-oList.id = 'lista-tarefas'
-
-
-let textInput = document.querySelector('#texto-tarefa')
-textInput.addEventListener('keyof', adicionarItensLista)
-
-function adicionarItensLista () {
-
+function createOl() {
+  document.querySelector('#lista-tarefas-section').appendChild(oList);
+  oList.id = 'lista-tarefas';
 }
+createOl();
+
+function createList() {
+  let inputPlace = document.querySelector('#texto-tarefa');
+  
+  let buttonInput = document.querySelector('#criar-tarefa');
+  buttonInput.addEventListener('click', function () {
+    
+    const lists = document.createElement('li');
+    lists.innerHTML = inputPlace.value;
+
+    document.querySelector('#lista-tarefas').appendChild(lists);
+
+    inputPlace.value = '';
+  });
+}
+
+createList();
