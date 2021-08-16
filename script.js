2c2ff009f649052b3e51142f03df60dd66527bed
arturbacilla <artur.bacilla1@gmail.com>
2021-08-16 16:29:32 -0300

    7th requisite: base logic set

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const taskList = document.querySelector('#lista-tarefas');
+let taskList = document.querySelector('#lista-tarefas');
 const addTaskBtn = document.querySelector('#criar-tarefa');
+let fullTaskLi = document.querySelectorAll('ol#lista-tarefas li');
 
 addTaskBtn.addEventListener('click', () => {
   const taskToAddText = document.querySelector('#texto-tarefa');
   const taskLi = document.createElement('li');
   taskLi.innerHTML = taskToAddText.value;
+  taskLi.className = 'task';
   taskList.appendChild(taskLi);
   taskToAddText.value = '';
+  fullTaskLi = document.querySelectorAll('ol#lista-tarefas li');
 });
+
+// Create a function to reset the task selection
+function clearTasksSelection() {
+  for (let li = 0; li < fullTaskLi.length; li += 1) {
+    fullTaskLi[li].classList.remove('selected');
+  }
+}
+
+if (fullTaskLi !== null) {
+fullTaskLi.forEach((taskOnLi) => {
+  taskOnLi.addEventListener('click', (task) => {
+    const thisTaskClasses = task.target.classList;
+    if (!thisTaskClasses.contains('selected')) {
+      clearTasksSelection();
+      thisTaskClasses.add('selected');
+    } else {
+      alert(task.target.id);
+    }
+  });
+});
+};
