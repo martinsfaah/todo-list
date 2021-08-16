9fdf87338da6662dc711c6c8b6d0bbfcc43a7e78
deobrafantinif <deboraferfan@gmail.com>
2021-08-16 14:47:37 -0300

    resolucao do requisito 7

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ const listTask = document.querySelector('#lista-tarefas');
 function createTask() {
   const newTask = document.createElement('li');
   newTask.innerHTML = textInput.value;
+  newTask.className = 'task';
   listTask.appendChild(newTask);
   textInput.value = '';
 }
 
 buttonCreateTask.addEventListener('click', createTask);
+
+function changeColorTask(event) {
+  const task = event.target;
+  task.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+listTask.addEventListener('click', changeColorTask);
