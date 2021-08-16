a84be41dc3eead74eaadb887999ab2e5a063170d
lucas-morais <lucasmoraisss@gmail.com>
2021-08-16 15:36:04 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Adicona escutador para mudança de backgorund ao clicar
+function selectTask(task) {
+  task.addEventListener('click', (event) => {
+    const taskElement = event.target;
+    taskElement.style.background = 'rgb(128, 128, 128)';
+  });
+}
+
+// Função para incluir tarefas na lista
 function createTask() {
   const button = document.getElementById('criar-tarefa');
   const textInput = document.getElementById('texto-tarefa');
@@ -5,7 +14,9 @@ function createTask() {
   button.addEventListener('click', () => {
     if (textInput.value !== '') {
       const task = document.createElement('li');
+      task.className = 'tarefa';
       task.innerHTML = textInput.value;
+      selectTask(task);
       taskList.append(task);
       textInput.value = '';
     } else {
@@ -13,5 +24,4 @@ function createTask() {
     }
   });
 }
-
 createTask();
