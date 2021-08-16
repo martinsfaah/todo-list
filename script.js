fe80e3eb9a7d68194724cbe6a002642f0af63ec2
luigrosz <luigrosz08@gmail.com>
2021-08-16 17:49:11 -0300

    8

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 const button = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
-// 5. e 6.
-
 function addTask() {
   const stringText = document.querySelector('#texto-tarefa').value;
   const createList = document.createElement('li');
+  createList.className = 'listItem';
   createList.innerHTML = stringText;
   taskList.appendChild(createList);
   document.querySelector('#texto-tarefa').value = '';
+  createList.addEventListener('click', clickGray);
+//   createList.addEventListener('dblclick', lineThrough);
 }
 
 button.addEventListener('click', addTask);
 
-// 7. e 8.
+function clickGray(event) {
+  let li = event.target
+//pinte todas as li`s diferentes da target de branco
+  for (i=0; i<document.querySelectorAll('.listItem').length; i++){
+    if (document.querySelectorAll('.listItem')[i] !== li){
+      document.querySelectorAll('.listItem')[i].style.backgroundColor = 'rgb(255, 255, 255)'
+    }   
+  }
+//troque a cor da li target
+  if (li.style.backgroundColor === "rgb(128, 128, 128)"){
+    li.style.backgroundColor = 'rgb(255, 255, 255'
+  } else {
+    li.style.backgroundColor = 'rgb(128, 128, 128'
+  }
+}
 
+// function lineThrough(event) {
+//   let li = event.target
+//   if 
+// }
