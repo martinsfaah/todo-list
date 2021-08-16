030348e3db0526881161023a42cd31468b9a7b4e
renatozr <renatozr07@gmail.com>
2021-08-16 16:45:24 -0300

    requisito 7 completo

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-// Requisito 5 -
+// requisito 5 e 7
 const inputTextoTarefa = document.getElementById('texto-tarefa');
 const btnCriarTarefa = document.getElementById('criar-tarefa');
 
 const listaTarefas = document.getElementById('lista-tarefas');
 
+function changeBkgColor(event) {
+  const clickedItem = event.target;
+  clickedItem.style.backgroundColor = 'rgb(128,128,128)';
+}
 function criaTarefa() {
   const tarefa = document.createElement('li');
   tarefa.innerHTML = inputTextoTarefa.value;
+  tarefa.className = 'tarefa';
+  tarefa.addEventListener('click', changeBkgColor);
 
   listaTarefas.appendChild(tarefa);
   inputTextoTarefa.value = '';
