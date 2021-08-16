6c5d695bdc8fbe25424f4dd715269bc38ec1a2a5
viniciusldn <viniciusldn0@gmail.com>
2021-08-16 15:14:48 -0300

    R6 completed

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const createTodoList = document.getElementById('criar-tarefa');
-const todo = document.getElementById('texto-tarefa');
+
+let todo = document.getElementById('texto-tarefa');
+
 createTodoList.addEventListener('click', function() {
   let li = document.createElement('li');
   li.textContent = todo.value;
   document.getElementById('lista-tarefas').appendChild(li);
+  todo.value = '';
 })
