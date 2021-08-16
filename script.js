cfd65656a960e0e046fed4f64644660cb6dd72fa
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-08-16 13:41:15 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+function selecionaTarefa(evento) {
+  const ultimaTarefa = document.querySelector('.selected');
+  if (ultimaTarefa !== null) ultimaTarefa.classList.remove('selected');
+  evento.target.classList.add('selected');
+}
+
+function completaTarefa(evento) {
+  const tarefaCompleta = evento.target;
+  if (tarefaCompleta.className.includes('completed')) {
+    tarefaCompleta.classList.remove('completed');
+  } else {
+    tarefaCompleta.classList.add('completed');
+  }
+}
+
 function criaTarefa() {
   if (document.querySelector('#texto-tarefa').value) {
-    let tarefa = document.querySelector('#texto-tarefa');
+    const tarefa = document.querySelector('#texto-tarefa');
     const novoElementoLista = document.createElement('li');
     novoElementoLista.innerText = tarefa.value;
-    novoElementoLista.addEventListener('click',selecionaTarefa);
+    novoElementoLista.addEventListener('click', selecionaTarefa);
+    novoElementoLista.addEventListener('dblclick', completaTarefa);
     document.querySelector('#lista-tarefas').appendChild(novoElementoLista);
     tarefa.value = '';
   }
 }
 
-function selecionaTarefa(evento) {
-  document.querySelector('.selected') !== null && document.querySelector('.selected').classList.remove('selected');
-  evento.target.classList.add('selected')
-}
+document.querySelector('#criar-tarefa').onclick = criaTarefa;
