a38a2eeb05da4ee1b8c9aa623b6c80bc481ae49c
renatozr <renatozr07@gmail.com>
2021-08-16 17:02:29 -0300

    requisito 8 completo

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-// requisito 5 e 7
+// requisito 5 e 7 e 8
 const inputTextoTarefa = document.getElementById('texto-tarefa');
 const btnCriarTarefa = document.getElementById('criar-tarefa');
 
 const listaTarefas = document.getElementById('lista-tarefas');
+const tarefas = document.getElementsByClassName('tarefa');
 
-function changeBkgColor(event) {
+function selectTarefa(event) {
+  for (let i = 0; i < tarefas.length; i += 1) {
+    if (tarefas[i].className === 'tarefa selected') {
+      tarefas[i].className = 'tarefa';
+    }
+  }
+
   const clickedItem = event.target;
-  clickedItem.style.backgroundColor = 'rgb(128,128,128)';
+  clickedItem.className += ' selected';
 }
 function criaTarefa() {
   const tarefa = document.createElement('li');
   tarefa.innerHTML = inputTextoTarefa.value;
   tarefa.className = 'tarefa';
-  tarefa.addEventListener('click', changeBkgColor);
+  tarefa.addEventListener('click', selectTarefa);
 
   listaTarefas.appendChild(tarefa);
   inputTextoTarefa.value = '';
