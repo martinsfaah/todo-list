77a233b49c99845fb1726dc5663fadde60b03122
Ismael Peccin <ismaelpeccin@gmail.com>
2021-08-16 15:15:33 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ function createParagraph() {
   let paragraph = document.createElement('p');
   const myHeader = document.querySelector('header');
   myHeader.appendChild(paragraph);
-  paragraph.id = 'funcionamento'; 
-  paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo'; 
+  paragraph.id = 'funcionamento';
+  paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
 }
 createParagraph();
 
 function createInput() {
-  let input = document.createElement('input'); 
-  const mySection = document.querySelector('section'); 
-  mySection.appendChild(input); 
-  input.id = "texto-tarefa"; 
+  let input = document.createElement('input');
+  const mySection = document.querySelector('section');
+  mySection.appendChild(input);
+  input.id = "texto-tarefa";
 }
 createInput();
+
+function createOrdList() {
+  let ordList = document.createElement('ol');
+  const olSection = document.getElementById('olSection');
+  olSection.appendChild(ordList);
+  ordList.id = "lista-tarefas";
+}
+createOrdList();
