02d0bf055e54843535a5be7321e0cf7131d3580e
João Felipe P <jf.pelliccione@gmail.com>
2021-08-16 16:37:19 -0300

    Conclusão do requisito 9.

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 function addTask() {
   const addTaskBtn = document.getElementById('criar-tarefa');
 
-addTaskBtn.addEventListener('click', function () {
-  const taskList = document.getElementById('lista-tarefas');
-  const inputValue = document.getElementById('texto-tarefa').value;
-  const newLi = document.createElement('li');
-  taskList.appendChild(newLi);
-  const taskListLastElementChild = taskList.lastElementChild;
-  taskListLastElementChild.innerHTML = inputValue;
-  document.getElementById('texto-tarefa').value = '';
-})
+  addTaskBtn.addEventListener('click', function () {
+    const taskList = document.getElementById('lista-tarefas');
+    const inputValue = document.getElementById('texto-tarefa').value;
+    const newLi = document.createElement('li');
+    taskList.appendChild(newLi);
+    const taskListLastElementChild = taskList.lastElementChild;
+    taskListLastElementChild.innerHTML = inputValue;
+    document.getElementById('texto-tarefa').value = '';
+  });
 }
 
 // Criando função que adicionará a classe "selected" quando determinado <li> for  clicado:
@@ -18,13 +18,27 @@ addTaskBtn.addEventListener('click', function () {
 function selectLi() {
   const taskList = document.getElementById('lista-tarefas');
 
-  taskList.addEventListener('click', function(event) {
+  taskList.addEventListener('click', function (event) {
     const taskListArray = document.querySelector('#lista-tarefas').children;
-    for (let li of taskListArray) {
-      li.classList.remove("selected");
-      li.style.backgroundColor = "blueviolet";
-      event.target.classList.add("selected");
-      event.target.style.backgroundColor = "rgb(128, 128, 128)";
+    for (const li of taskListArray) {
+      li.classList.remove('selected');
+      li.style.backgroundColor = 'blueviolet';
+      event.target.classList.add('selected');
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+  });
+}
+
+// Clicar duas vezes em determinado <li> faz com que ele seja riscado.
+// Deve ser possível desfazer essa ação clicando novamente duas vezes no item.
+function tickUntickLi() {
+  const taskList = document.getElementById('lista-tarefas');
+
+  taskList.addEventListener('dblclick',  function (event) {
+    if (event.target.classList.contains('completed') === false) {
+      event.target.classList.add('completed');
+    } else {
+      event.target.classList.remove('completed');
     }
   })
 }
@@ -32,4 +46,5 @@ function selectLi() {
 window.onload = function () {
   addTask();
   selectLi();
+  tickUntickLi();
 };
