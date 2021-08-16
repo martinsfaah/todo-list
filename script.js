62e8ac147e6565b78634a44abcadf384062ef28a
Gustavo Lima <lima_gus@outlook.com>
2021-08-16 16:10:10 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+const taskList = document.getElementById('lista-tarefas');
+
 // Adiciona uma tarefa
 function addNewTask() {
-  const taskList = document.getElementById('lista-tarefas')
   const newTaskItem = document.createElement('li');
   const taskText = document.getElementById('texto-tarefa');
   newTaskItem.innerText = taskText.value;
@@ -10,3 +11,11 @@ function addNewTask() {
 
 const addTaskButton = document.getElementById('criar-tarefa');
 addTaskButton.addEventListener('click', addNewTask);
+
+// Altera background-color do item selecionado
+function changeBgColor(task) {
+  const clickedTask = task.target;
+  clickedTask.style.backgroundColor = 'rgb(128,128,128)';
+}
+
+taskList.addEventListener('click', changeBgColor);
