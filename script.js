c7ce7df9ce4d0900c677db61391fce9d592f446b
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-08-16 15:55:46 -0300

    requisitos 3, 4 e 5

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function createButtonAdd() {
   const buttonAdd = document.createElement('button');
   mainSection.appendChild(buttonAdd);
   buttonAdd.id = 'criar-tarefa';
+  buttonAdd.innerText = 'Adicionar';
 }
 createButtonAdd();
 
@@ -20,3 +21,15 @@ function createOrderList() {
   orderList.id = 'lista-tarefas';
 }
 createOrderList();
+
+function addTask() {
+  const inputField = document.querySelector('#texto-tarefa');
+  const orderList = document.querySelector('#lista-tarefas');
+  const listItem = document.createElement('li');
+  orderList.appendChild(listItem);
+  listItem.innerText = inputField.value;
+  inputField.value = '';
+}
+
+const buttonAdd = document.querySelector('#criar-tarefa');
+buttonAdd.addEventListener('click', addTask);
