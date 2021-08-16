bb50088fdcdd6260243480fd0e84ecc71e6cc2c9
Joao-Ortega <joao_ortega17@hotmail.com>
2021-08-16 16:56:48 -0300

    Requisitos 6 ao 8

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ function addListItem() {
     newLi.innerText = input.value;
     ol.appendChild(newLi);
     input.value = '';
+    let li = document.getElementsByTagName('li');
+for(let index of li) {
+    index.addEventListener('click', addGreyColor);
+}
+}
+function addGreyColor(event) {
+    let li = document.getElementsByTagName('li');
+    for(const index of li) {
+        index.style.backgroundColor = 'white';
+    }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
