8d838def7c66adb29fc08389f1fc0d1df2d312d7
Italo Sergio <italosergio@mail.com>
2021-08-16 17:25:58 -0300

    Resolve Desafio 5

--- script.js
+++ script.js
@@ -18,7 +18,21 @@ const main = document.createElement('main');
 body.appendChild(main);
 main.appendChild(input);
 
+// Desafio 5 ...
+const button = document.createElement('button');
+button.id = 'criar-tarefa';
+button.innerText = 'Adicionar';
+main.appendChild(button);
+
 // Desafio 4
 const taskList = document.createElement('ol');
 taskList.id = 'lista-tarefas';
 main.appendChild(taskList);
+
+// ... Desafio 5
+function addListItem() {
+  const lineTaskList = document.createElement('li');
+  lineTaskList.innerText = input.value;
+  taskList.appendChild(lineTaskList);
+}
+button.addEventListener('click', addListItem);
