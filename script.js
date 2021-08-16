ade12c10b53b75d6e2d481c1990dd7f6d5e0ebdb
Giovanni Duarte <gikigiio@gmail.com>
2021-08-16 15:31:41 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 let taskInput = document.getElementById('texto-tarefa');
-let taskButton = document.getElementById('criar-tarefa');
-taskButton.addEventListener('click', createTask);
 
+addListeners('#criar-tarefa', 'click', createTask);
+
+// Add Event Listeners
+function addListeners(targetElement, targetEvent, targetFunction) {
+  let elementArray = document.querySelectorAll(targetElement);
+  for (let i = 0; i < elementArray.length; i += 1) {
+    document
+      .querySelectorAll(targetElement)
+      [i].addEventListener(targetEvent, targetFunction);
+  }
+}
+
+// Create Task
 function createTask(task) {
   let newTask = document.createElement('li');
   let taskList = document.getElementById('lista-tarefas');
+  newTask.className = 'listItem';
   newTask.innerText = taskInput.value;
+  newTask.addEventListener('click', colorTask);
   taskList.appendChild(newTask);
   taskInput.value = '';
+}
+
+// Color Task
+function colorTask(task) {
+  let tasks = document.querySelectorAll('.listItem');
+  for (let i = 0; i < tasks.length; i += 1) {
+    tasks[i].classList.remove('selected');
+  }
+  task.target.classList.add('selected');
 }
