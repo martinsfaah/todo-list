e47ed999f8f24697e9b9edf85404588d067d507e
William <william.mk1@outlook.com>
2021-08-16 15:58:37 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const input = document.createElement('input');
 const body = document.querySelector('#body');
+
+const buttonSection = document.createElement('section');
+body.appendChild(buttonSection)
+const button = document.createElement('button');
+button.id = 'criar-tarefa';
+button.innerText = 'adicionar tarefa';
+buttonSection.appendChild(button);
+
+const input = document.createElement('input');
 input.id = 'texto-tarefa';
 body.appendChild(input);
 
-const list = document.createElement('ol');
-list.id = 'lista-tarefas';
-body.appendChild(list);
+const orderList = document.createElement('ol');
+orderList.id = 'lista-tarefas';
+body.appendChild(orderList);
+ 
+function addTask() {
+  button.onclick = function() {
+    const inputValue = input.value;
+    const createList = document.createElement('li');
+    createList.innerText = inputValue;
+    orderList.appendChild(createList);
+  }
+}
+addTask();
