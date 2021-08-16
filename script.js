4ecee79dcd672a1b27ec2a171ed5499b88e166df
Anderson-Zobel <anderson.zobels@gmail.com>
2021-08-16 17:12:07 -0300

    requisito 5 e 6

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
+//section
+let section = document.querySelector('section')
+
 //<p>
 let p = document.createElement('p');
 p.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 p.id = 'funcionamento';
-document.body.appendChild(p);
+section.appendChild(p);
 
 //input
 let input = document.createElement('input');
-input.type = 'text';
+
 input.id = 'texto-tarefa';
-document.body.appendChild(input);
+section.appendChild(input);
 
 //ol
 let ol = document.createElement('ol');
 ol.id = 'lista-tarefas'
 document.body.appendChild(ol);
+
+//button
+let button = document.createElement('button');
+button.id = 'criar-tarefa';
+button.innerText = 'adicionar'
+section.appendChild(button);
+
+//list
+let inputItem = document.querySelector('#texto-tarefa');
+let buttonCreate =  document.querySelector('#criar-tarefa');
+
+function inputAdd (){
+    li = document.createElement('li');
+    ol.appendChild(li)
+
+    li.innerText = inputItem.value
+        inputItem.value = '';
+    
+}
+
+    buttonCreate.addEventListener ('click', inputAdd);
+
+
