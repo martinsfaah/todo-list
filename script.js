bd5be0c5ca2e1d0d4920383c53b0f1263676f5f4
devgeorge <glalmeida.dev@gmail.com>
2021-08-16 17:22:06 -0300

    Requito 7

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ botao.addEventListener("click", adicionaTarefa);
 function adicionaTarefa (){
     let textoDigitado = document.getElementById("texto-tarefa").value;       
     let makeItem = document.createElement("li");
+    list.addEventListener("click" , adicionaBack);
     makeItem.innerText = textoDigitado;   
     console.log(makeItem);
     list.appendChild(makeItem);
     document.getElementById("texto-tarefa").value = "";
-         
+}
+function adicionaBack(event){
+    event.target.style.backgroundColor = "rgb(128, 128, 128)";
 }
