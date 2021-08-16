eff0e022ce244b07a6310d803602f3d022e2614c
William <william.mk1@outlook.com>
2021-08-16 14:43:16 -0300

    requisito 4

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const input = document.createElement('input');
 const body = document.querySelector('#body');
 input.id = 'texto-tarefa';
 body.appendChild(input);
+
+const list = document.createElement('ol');
+list.id = 'lista-tarefas';
+body.appendChild(list);
