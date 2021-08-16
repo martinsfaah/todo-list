f80ee50b7a00e5ea02b61c92bc9a53f941b102c7
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-08-16 17:24:26 -0300

    quinto requisito

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ CreateHeader();
 // Requisito 2  Adicionando paragrafo
 
 function CreateP (){
-  const setBody2 = document.getElementsByTagName('body')[0];
+  const setHeader = document.getElementsByTagName('header')[0];
   const creatTagP = document.createElement('p');
   creatTagP.id = "funcionamento"
   creatTagP.innerHTML = "Clique duas vezes em um item para marcá-lo como completo";
-  setBody2.appendChild(creatTagP);
+  setHeader.appendChild(creatTagP);
 }
 CreateP();
 
@@ -24,10 +24,13 @@ CreateP();
 
 function CreateInput (){
   const setBody3 = document.getElementsByTagName('body')[0];
+  const createSec = document.createElement('section');
+  setBody3.appendChild(createSec);
+  createSec.id ="input-section"
   const createInput = document.createElement('input');
   createInput.setAttribute('type', 'text');
   createInput.id = "texto-tarefa"
-  setBody3.appendChild(createInput);
+  createSec.appendChild(createInput);
 }
 CreateInput();
 
@@ -41,3 +44,27 @@ function CreateLista() {
   setBody4.appendChild(createLista);
 }
 CreateLista();
+
+//Requisito 5 Criando Botão
+
+function createButton(){
+  const setSection = document.querySelector('#input-section')
+  const button = document.createElement('button');
+  button.id = "criar-tarefa";
+  button.innerHTML = "Adicionar"
+  setSection.appendChild(button);
+}
+createButton();
+
+function addTask (){
+  const setList = document.querySelector('ol');
+  const creatLi = document.createElement('li')
+  const targetInput = document.querySelector('input');
+  setList.appendChild(creatLi)
+  creatLi.innerText = targetInput.value;
+  setList.appendChild(creatLi);
+  targetInput.value = '';
+}
+const setButton = document.querySelector('#criar-tarefa')
+setButton.addEventListener('click', addTask);
+
