cb4504cca2637ac316be21adc01a22075e4d0fe7
Rafael <rafaelmoura7033@gmail.com>
2021-08-16 15:23:57 -0300

    Adiciona exercicio 6 e 7

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const list = document.querySelector("ol")
+const listElements = list.children;
 
 button.addEventListener("click", addFromInputToList);
 
@@ -9,5 +10,16 @@ function addFromInputToList(event) {
     let listItem = document.createElement("li");
     listItem.innerHTML = item;
     list.appendChild(listItem)
+    list.addEventListener("click", colorChanger)
     input.value = "";
+}
+
+
+function colorChanger(event) {
+    let actualListElement = event.target
+    if (actualListElement.style.backgroundColor !== "rgb(128,128,128)") {
+        actualListElement.style.backgroundColor = "rgb(128,128,128)"
+    } else {
+        actualListElement.style.backgroundColor = "white"
+    }
 }
