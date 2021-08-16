08afbdcbeca97f122da63c4993a932422b0fb21b
phillipe <rodrigues153@gmail.com>
2021-08-16 16:29:49 -0300

    Adiciona requisito 7 e 8

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const button=document.getElementById("criar-tarefa");
 const input = document.getElementById("texto-tarefa");
 const listaOrdenada = document.getElementById("lista-tarefas");
+const lista = document.querySelector('.listaDeTarefas');
 
 function adicionaTarefa ()
 {
     let valor = document.createElement('li');
+    const selecionado =document.querySelector('.selected');
+    if (selecionado!==null){selecionado.classList.remove('selected');}
+    valor.className="listaDeTarefas selected";
+    
     valor.innerText=input.value;
     listaOrdenada.appendChild(valor); 
     input.value=''; 
 }
 button.addEventListener('click',adicionaTarefa)
- 
+
+function changeColor (event)
+{
+    const selected =document.querySelector('.selected');
+    selected.classList.remove('selected');
+    event.target.classList.add('selected');   
+    //document.getElementsByClassName('selected').style.backgrounColor=rgb(128, 128, 128);  
+}
+listaOrdenada.addEventListener('click',changeColor);
