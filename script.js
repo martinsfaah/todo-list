cb8fde6f59f001841600d37b4e9a3c5fbc8a0287
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-08-16 15:57:49 -0300

    Decimo-primeiro requisito

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputTarefa = document.getElementById('texto-tarefa');
 const botaoCriar = document.getElementById('criar-tarefa');
 const botaoApagar = document.getElementById('apaga-tudo');
 const botaoFinalizados = document.getElementById('remover-finalizados');
+const botaoSalvar = document.getElementById('salvar-tarefas');
 
 function adicionarEventos(elemento, evento, funcao) {
   if (elemento.length > 0) {
@@ -15,7 +16,6 @@ function adicionarEventos(elemento, evento, funcao) {
 }
 
 function fundoCinza(evento) {
-  console.log(evento.target);
   if (document.querySelector('.selected')) {
     document.querySelector('.selected').classList.remove('selected');
   }
@@ -41,16 +41,42 @@ function criarElementosLista() {
 }
 
 function apagarElementosLista() {
-  let itens = lista.children;
-  for(let index = 0; index < itens.length; index += 0) {
+  const itens = lista.children;
+  for (let index = 0; index < itens.length; index += 0) {
     lista.removeChild(itens[0]);
   }
 }
 
 function apagarElementosFinalizados() {
-  let itens = document.getElementsByClassName('completed');
-  for(let index = 0; index < itens.length; index += 0) {
+  const itens = document.getElementsByClassName('completed');
+  for (let index = 0; index < itens.length; index += 0) {
     lista.removeChild(itens[0]);
+  }
+}
+
+function salvarItens() {
+  if (localStorage.length !== 0) {
+    localStorage.clear();
+  }
+  const listaAtual = document.getElementsByTagName('li');
+  for (let index = 0; index < listaAtual.length; index += 1) {
+    let valor = listaAtual[index].innerText;
+    valor += ';; ';
+    valor += listaAtual[index].className;
+    localStorage.setItem(index, valor);
+  }
+}
+
+function recarregarElementosSalvos() {
+  const tamanhoStorage = localStorage.length;
+  for (let index = 0; index < tamanhoStorage; index += 1) {
+    const valor = localStorage.getItem(index).split(';; ');
+    const elemento = document.createElement('li');
+    elemento.innerText = valor[0];
+    elemento.className = valor[1];
+    adicionarEventos(elemento, 'click', fundoCinza);
+    adicionarEventos(elemento, 'dblclick', linhaRiscada);
+    lista.appendChild(elemento);
   }
 }
 
@@ -58,6 +84,9 @@ function carregarPagina() {
   adicionarEventos(botaoCriar, 'click', criarElementosLista);
   adicionarEventos(botaoApagar, 'click', apagarElementosLista);
   adicionarEventos(botaoFinalizados, 'click', apagarElementosFinalizados);
+  adicionarEventos(botaoSalvar, 'click', salvarItens);
+
+  recarregarElementosSalvos();
 }
 
 window.onload = carregarPagina;
