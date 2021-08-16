d47b46f415145fdc39c6fbecd83365f4f7443582
Eric Cruz <effolex@gmail.com>
2021-08-16 15:40:01 -0300

    quinto ao oitavo requisito concluido no js

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const criarTarefa = document.getElementById('criar-tarefa');
+const ListaTarefa = document.getElementById('lista-tarefas');
 
-function addNewToDo
+function addNewToDo() {
+  const newListItemInput = document.getElementById('texto-tarefa');
+  const newItem = document.createElement('li');
+  newItem.textContent = newListItemInput.value;
+  newListItemInput.value = '';
+  ListaTarefa.appendChild(newItem);
+}
 
-criarTarefa.addEventListener('click',addNewToDo)
+function backgroundChange(event) {
+  const olElement = event.target;
+  const oldSelected = document.querySelector('.selected');
+  if (olElement.classList.contains('selected')) {
+    return;
+  }
+  if (olElement.tagName === 'LI') {
+    olElement.style.backgroundColor = 'rgb(128,128,128)';
+    if (oldSelected) {
+      oldSelected.classList.remove('selected');
+      oldSelected.style.backgroundColor = '';
+    }
+    olElement.classList.add('selected');
+  }
+}
+
+criarTarefa.addEventListener('click', addNewToDo);
+
+ListaTarefa.addEventListener('click', backgroundChange);
