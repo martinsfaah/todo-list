ebf74c75b86d2455693960aceffeced7fc07c52a
JunioASilva <junioalves2005@hotmail.com>
2021-08-16 20:49:01 -0300

    cumprindo requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 window.onload = function(){
     let criaTarefa = document.getElementById("criar-tarefa");
     criaTarefa.addEventListener("click", function(){
@@ -9,8 +10,51 @@ window.onload = function(){
         itemLista.appendChild(novaMensagem);
 
         entradaMensagem.value = "";
+
+        novaMensagem.addEventListener("click", function(){
+            if(document.querySelector(".selected")){
+                document.querySelector(".selected").classList.remove("selected");
+            }
+            if(novaMensagem.classList.contains("selected")){
+                novaMensagem.classList.remove("selected");
+            }else{
+                novaMensagem.classList.add("selected");
+            }
+         
+        })
+        novaMensagem.addEventListener("dblclick", function(){
+            
+            if(novaMensagem.classList.contains("completed")){
+                novaMensagem.classList.remove("completed");
+            }else{
+                novaMensagem.classList.add("completed");
+            }
+        })
+
+
     })
 }
+// localStorage.querySelector(".apaga-tudo")
+// localStorage.querySelector(".salvar-tarefas");
 
+    // function dbClick();
+    //     let li = document.getElementsByTagName("li");
+    //     li.addEventListener("dbclick", function(event){
+    //         if(li.classList.contains("completed")){
+    //             li.classList.remove("completed");
+    //         }else{
+    //             li.classList.add("completed");
+    //         }
+    //     })
+    //     dbClick();
+   
+        
+
+
+
+
+
+    
+    
 
 
