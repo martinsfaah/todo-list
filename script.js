55595ba2db4d52586bd4f74c053b469a9b5b84e9
thomaz jefferson <thomaz.jefferson01@protonmail.com>
2021-08-16 20:45:08 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const botao = document.getElementById('criar-tarefa');
 const digitarLista = document.getElementById('texto-tarefa');
 let lista = document.getElementById('lista-tarefas')
+let selecao = document.querySelectorAll('.lista')
 
 
 
-botao.addEventListener('click', function criarElemntoLista() {
+botao.addEventListener('click', function criarElementoLista() {
   let itemLista = document.createElement('li');
   lista.appendChild(itemLista);
   itemLista.innerText = digitarLista.value;
   digitarLista.value = "";
+})
+
+lista.addEventListener('click', function cinzaBg(selecao) {
+  selecao.target.style.backgroundColor = "rgb(128, 128, 128)";
 })
 
