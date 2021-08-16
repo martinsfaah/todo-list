358eb4cecc027c727863815e35df2d3820f31f92
André <andre.bonaf@gmail.com>
2021-08-16 17:29:36 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function createHeader(){
+function createElements(){
     const body = document.querySelector('body');
     const header = document.createElement('header');
+    const p = document.createElement('p');
     header.innerText = 'Minha Lista de Tarefas';
+    p.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+    p.id = 'funcionamento';
 
     body.appendChild(header);
+    body.appendChild(p);
 }
 
-createHeader();
+createElements();
