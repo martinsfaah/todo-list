b82179b2c0ec5ba7db770af401444194b74f2d31
Joao Paulo Veloso <joaopaulox@outlook.com>
2021-08-16 15:38:46 -0300

    ate o requisito nove meia boca

--- script.js
+++ script.js
@@ -6,9 +6,44 @@ window.onload = function() {
     let lista = document.getElementById("lista-tarefas");
     novaTarefa.innerHTML = entrada.value;
 
-    lista.appendChild(novaTarefa);
+    if (entrada.value != ""){
+      lista.appendChild(novaTarefa);
 
-    entrada.value = "";
+      entrada.value = "";
+    }
+
   })
+
+
+  let itensLista = document.getElementsByTagName("li");
+
+  for (let index = 0; index < itensLista.length; itensLista += 1) {
+    itensLista[index].addEventListener("click", function(evento) {
+      itensLista[index].style.backgroundColor = "red";
+      evento.target.style.backgroundColor = "rgb(128, 128, 128";
+      console.log(evento.target)
+
+      if (itensLista[index].classList.contains("selected") == true) {
+        itensLista[index].classList.remove("selected");
+      }
+      else {
+        itensLista[index].classList.add("selected");
+      }
+    });
+  }
+
+  for (let index = 0; index < itensLista.length; index += 1) {
+    itensLista[index].addEventListener("dblclick", function() {
+      if (itensLista[index].classList.contains("completed") == true) {
+        itensLista[index].classList.remove("completed");
+      }
+      else {
+        itensLista[index].classList.add("completed");
+      }
+    });
+  }
+
+
+
 }
 
