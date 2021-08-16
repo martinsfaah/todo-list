bc9da1179bd39900c990943d51ee219f86f92214
douglas oliveira <douglasgama0@gmail.com>
2021-08-16 17:00:51 -0300

    Finalizaçao do requisito 5 e 6

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function inputTxt() {
   header.appendChild(textBox);
 }
 inputTxt();
+
+function addTask() {
+  let buttonAdd = document.querySelector('#criar-tarefa');
+  let ol = document.querySelector('#lista-tarefas');
+
+  buttonAdd.addEventListener('click', function eventeClick() {
+    const itemList = document.createElement('li');
+    itemList.innerText = document.getElementById('texto-tarefa').value;
+    ol.appendChild(itemList);
+    document.getElementById('texto-tarefa').value = null;
+  });
+}
+addTask();
