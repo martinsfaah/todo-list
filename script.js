08644f320c71985e028c0b3177cef18333303efd
Ricardo <ricardoanj.14@gmail.com>
2021-08-16 18:27:20 -0300

    recriando todos os requisitos dinamicamente e finalizando requisito cinco

---
+++ script.js
@@ -0,0 +1,30 @@
+const sec = document.querySelector('.add-button')
+const addText = document.createElement('input')
+addText.id = 'texto-tarefa'
+sec.appendChild(addText)
+
+let chamarEl = document.querySelector('.add-button')
+let botao = document.createElement('button')
+botao.innerText = 'Adicionar'
+botao.id = 'criar-tarefa'
+chamarEl.appendChild(botao);
+
+let listaO = document.querySelector('.lista')
+let list = document.createElement('ol')
+list.id = 'lista-tarefa';
+listaO.appendChild(list);
+
+
+function add () {
+    const input = document.querySelector('#texto-tarefa')
+    const button = document.querySelector('#criar-tarefa')
+    const ol = document.querySelector('#lista-tarefa')
+    
+    button.addEventListener("click", function () {
+        let listas = document.createElement("li")
+        ol.appendChild(listas)
+        listas.innerHTML = input.value;
+        input.value = "";
+})
+}
+add()
