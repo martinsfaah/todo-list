004408da3562c2663932321dd00e297ddd606aad
João Felipe P <jf.pelliccione@gmail.com>
2021-08-16 16:08:45 -0300

    Conclusão dos requisitos 7 e 8.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Criando função que  será alocada no botão "Adicionar Tarefa":
+// Criando função que será alocada no botão "Adicionar Tarefa":
 function addTask() {
   const addTaskBtn = document.getElementById('criar-tarefa');
 
@@ -13,6 +13,23 @@ addTaskBtn.addEventListener('click', function () {
 })
 }
 
+// Criando função que adicionará a classe "selected" quando determinado <li> for  clicado:
+// Além disso, o fundo do elemento clicado mudará  de cor.
+function selectLi() {
+  const taskList = document.getElementById('lista-tarefas');
+
+  taskList.addEventListener('click', function(event) {
+    const taskListArray = document.querySelector('#lista-tarefas').children;
+    for (let li of taskListArray) {
+      li.classList.remove("selected");
+      li.style.backgroundColor = "blueviolet";
+      event.target.classList.add("selected");
+      event.target.style.backgroundColor = "rgb(128, 128, 128)";
+    }
+  })
+}
+
 window.onload = function () {
   addTask();
+  selectLi();
 };
