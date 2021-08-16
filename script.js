190cedf782ce35d7814aecdb67b46aae78c43d82
Matheus <mathspa98@gmail.com>
2021-08-16 15:31:13 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let criarTarefa = document.getElementById('criar-tarefa');
 criarTarefa.addEventListener('click', adicionaItem);
 
+function adicionaCor(){
+    let itemNovo= document.getElementById("lista-tarefas").lastChild;
+    itemNovo.addEventListener('click',trocaCor);
+}
+
 function adicionaItem() {
   let item = document.getElementById('texto-tarefa').value;
   if (item != '') {
@@ -10,4 +15,21 @@ function adicionaItem() {
     lista.appendChild(itemLista);
   }
   document.getElementById('texto-tarefa').value='';
+  adicionaCor();
+}
+
+function trocaCor(evento){
+    let itemSelec = evento.target;
+    mudaClasseSelec(itemSelec);
+    itemSelec.style.backgroundColor="rgb(128, 128, 128)";
+}
+
+function mudaClasseSelec(alvo){
+    let itensChecagem = document.querySelectorAll("li");
+    for (let i=0; i<itensChecagem.length; i += 1){
+        if(itensChecagem[i].style.backgroundColor==='rgb(128, 128, 128)'){
+            itensChecagem[i].style.backgroundColor="white";
+        }
+    }
+
 }
