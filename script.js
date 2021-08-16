b0be574f9ad2c79455f2ca720708b116beb5b8db
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-08-16 15:08:18 -0300

    Requisitos 7

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const list = document.getElementById('lista-tarefas');
 const buttonAdd = document.getElementById('criar-tarefa');
-let input = document.getElementById('texto-tarefa');
+const input = document.getElementById('texto-tarefa');
+
+function selectList () {
+  const lineList = document.querySelector('li');
+  lineList.style.background = 'rgb(128, 128, 128)';
+
+}
+list.addEventListener('click', selectList);
 
 function insertList() {    
   const tarefa = document.createElement('li');
   tarefa.innerText = input.value;
-  list.appendChild(tarefa)
-  input.value ='';
+  list.appendChild(tarefa);
+  input.value = '';
 }
 
 buttonAdd.addEventListener('click', insertList);
