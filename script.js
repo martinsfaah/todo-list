7abaad19c98aff4dbbcbdfc053b475460030947c
biizerrah <biizerrah@outlook.com>
2021-08-16 18:35:56 -0300

    Requisito 07 completo

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
-
 const input = document.getElementById('texto-tarefa');
 const button = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
   
 button.addEventListener('click',function(){
-  const li = document.createElement('li')
-  const task = input.value;
-  li.innerText = task;
-  ol.appendChild(li);
-  input.value = '';
- });
+const li = document.createElement('li');
+const task = input.value;
+li.innerText = task;
+ol.appendChild(li);
+input.value = '';
+input.focus();
+});
 
- // Será verificado que, ao se carregar a página, os itens da lista não tem o estilo CSS background-color: rgb(128, 128, 128). Será verificado que, ao se clicar em um item da lista, ele passa a ter o estilo CSS background-color: rgb(128, 128, 128)
+  ol.addEventListener('click',function(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+ });
