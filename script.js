63b7eb2ca2ac288d67ba5bdea3add8e674e26f4d
Rafael Augusto Scherer <rafaelaugustscherer@gmail.com>
2021-08-16 15:05:05 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const buttonCriarTarefa = document.getElementById('criar-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 
-function mudarCorTarefa(evt) {
+function selecionarTarefa(evt) {
   const itemTarefa = evt.target;
 
   listaTarefas.childNodes.forEach((tarefa) => {
@@ -11,12 +11,23 @@ function mudarCorTarefa(evt) {
   itemTarefa.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
+function completarTarefa(evt) {
+  const itemTarefa = evt.target;
+
+  if (itemTarefa.classList.contains('completed')) {
+    itemTarefa.classList.remove('completed');
+  } else {
+    itemTarefa.classList.add('completed');
+  }
+}
+
 function inserirTarefa() {
   const inputTarefa = document.getElementById('texto-tarefa');
   const itemTarefa = document.createElement('li');
 
   itemTarefa.innerText = inputTarefa.value;
-  itemTarefa.addEventListener('click', mudarCorTarefa);
+  itemTarefa.addEventListener('click', selecionarTarefa);
+  itemTarefa.addEventListener('dblclick', completarTarefa);
   inputTarefa.value = '';
 
   listaTarefas.appendChild(itemTarefa);
