c1e70908b2ce8b81d72fe2d8c7babe22dac7a710
Renato Moreno <renats.mc@gmail.com>
2021-08-16 17:07:46 -0300

    requisito 7

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ function criarTarefa() {
   const tarefa = document.getElementById('texto-tarefa').value;
   const lista = document.getElementById('lista-tarefas');
   const novaTarefa = document.createElement('li');
+  novaTarefa.className = 'item';
   novaTarefa.innerHTML = tarefa;
   lista.appendChild(novaTarefa);
   document.getElementById('texto-tarefa').value = '';
+  novaTarefa.addEventListener('click', corDoFundo);
 }
 
 const botao = document.querySelector('#criar-tarefa');
 botao.addEventListener('click', criarTarefa);
+
+function corDoFundo(event) {
+  const itemList = event.target;
+  itemList.style.backgroundColor = 'rgb(128, 128, 128)';
+}
