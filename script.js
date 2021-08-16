4a98f3b39980a1f8a0357701bdd08ffaefb40d59
André <andre.bonaf@gmail.com>
2021-08-16 17:48:59 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -2,22 +2,18 @@ function createElements(){
     const body = document.querySelector('body');
     const header = document.createElement('header');
     const p = document.createElement('p');
+    const input = document.createElement('input');
+    const ol = document.createElement('ol');
     header.innerText = 'Minha Lista de Tarefas';
     p.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
     p.id = 'funcionamento';
+    input.id = 'texto-tarefa';
+    ol.id = 'lista-tarefas';
 
     body.appendChild(header);
     body.appendChild(p);
+    body.appendChild(input);
+    body.appendChild(ol);
 }
 
 createElements();
-
-function createInput() {
-    const body = document.querySelector('body');
-    const input = document.createElement('input');
-    input.id = 'texto-tarefa';
-
-    body.appendChild(input);
-}
-
-createInput();
