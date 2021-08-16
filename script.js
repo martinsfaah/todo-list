29a443ff03e9b468f606307fdcc2d706434d42ec
Anderson Alencar <anderson23alencar@gmail.com>
2021-08-16 16:29:58 -0300

    pinta item clicado de cinza

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const buttonAdd = document.getElementById('criar-tarefa');
 const inputItem = document.getElementById('texto-tarefa');
 const listTasks = document.getElementById('lista-tarefas');
 
+//Cria tarefas
 buttonAdd.addEventListener('click', addTask);
 
 function addTask() {
@@ -10,3 +11,15 @@ function addTask() {
   listTasks.appendChild(item);
   inputItem.value = '';
 }
+
+//Adiciona cor no item clicado
+listTasks.addEventListener('click', paintTask);
+
+function paintTask(event) {
+  for (let i = 0; i < listTasks.children.length; i += 1) {
+    listTasks.children[i].style.backgroundColor = 'white';
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+}
+
+
