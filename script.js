95b9be8b1cd527437ec22c03759b223890cf3271
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-08-16 16:38:00 -0300

    corrige requisitos

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
 const list = document.querySelector('#lista-tarefas');
 const ListIntens = list.children;
+
+function selectItem(event) {
+  for (let i = 0; i < ListIntens.length; i += 1) {
+    ListIntens[i].classList.remove('selected');
+  }
+  const evento = event.target;
+  evento.classList.add('selected');
+}
+
+function completItem(event) {
+  const evento = event.target;
+  if (evento.className === 'item completed selected' || evento.className === 'item completed') {
+    evento.classList.remove('completed');
+  } else {
+    evento.classList.add('completed');
+  }
+}
+
 function inputButton() {
   function onClik() {
     const listItem = document.createElement('li');
     listItem.className = 'item';
-    const inputText = document.querySelector('#texto-tarefa').value;
-    listItem.innerHTML = inputText;
+    const inputText = document.querySelector('#texto-tarefa');
+    listItem.innerHTML = inputText.value;
+    listItem.addEventListener('click', selectItem);
+    listItem.addEventListener('dblclick', completItem);
     list.appendChild(listItem);
+    inputText.value = '';
   }
   const buttonAdd = document.querySelector('#criar-tarefa');
   buttonAdd.addEventListener('click', onClik);
 }
 inputButton();
-
-function selectItem() {
-  function onClick(event) {
-    for (let i = 0; i < ListIntens.length; i += 1) {
-      ListIntens[i].classList.remove('selected');
-    }
-    const evento = event.target;
-    evento.classList.add('selected');
-  }
-  list.addEventListener('click', onClick);
-}
-selectItem();
-
-function completItem() {
-  function onClick(event) {
-    const evento = event.target;
-    if (evento.className === 'item completed selected' || evento.className === 'item completed') {
-      evento.classList.remove('completed');
-    } else {
-      evento.classList.add('completed');
-    }
-  }
-  list.addEventListener('dblclick', onClick);
-}
-completItem();
 
 function deleteAll() {
   function onClick() {
@@ -61,3 +57,5 @@ function deleteFinished() {
   rmvFinished.addEventListener('click', onClick);
 }
 deleteFinished();
+
+// functio
