d4651b091286cfc0a3bdc5123021a9d745f6fe35
William <william.mk1@outlook.com>
2021-08-16 20:49:27 -0300

    requisito 9

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const buttonSection = document.createElement('section');
 body.appendChild(buttonSection);
 const button = document.createElement('button');
 button.id = 'criar-tarefa';
-button.innerText = 'adicionar tarefa';
+button.innerText = 'Adicionar Tarefa';
 buttonSection.appendChild(button);
 
 const input = document.createElement('input');
@@ -23,16 +23,39 @@ function addTask() {
     createList.innerText = input.value;
     input.value = '';
     selectListItem();
-  }
-};
+    //completedItems();
+  };
+}
 addTask();
 
-function selectListItem () {
+function selectListItem() {
   orderList.addEventListener('click', function (event) {
     const selectedItem = document.getElementsByClassName('list-items');
-    for (i = 0; i < selectedItem.length; i += 1) {
+    for (let i = 0; i < selectedItem.length; i += 1) {
       selectedItem[i].style.backgroundColor = 'transparent';
       event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-    } 
+    }
+  });
+}
+
+// function completedItems() {
+//   orderList.addEventListener('dblclick', function (event) {
+//     const dbClickedItem = event.target
+//     dbClickedItem.classList.remove('completed');
+//     dbClickedItem.classList.add('completed');
+//   })
+// }
+const items = document.getElementsByClassName('list-items');
+function clearButton() {
+  const clearButton = document.createElement('button');
+  clearButton.id = 'apaga-tudo';
+  clearButton.innerText = 'Apagar Tudo';
+  buttonSection.appendChild(clearButton);
+  clearButton.addEventListener('click', function (event) {
+    for (let i = items.length - 1; i >= 0; i -= 1) {
+      orderList.removeChild(items[i]);
+    }
+    
   })
-};
+}
+clearButton();
