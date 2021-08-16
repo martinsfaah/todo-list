d0933c53318caee2cfe472994646089f64afa2c4
Renato Moreno <renats.mc@gmail.com>
2021-08-16 20:49:39 -0300

    requisito 10

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ function criarTarefa() {
 
 const botao = document.querySelector('#criar-tarefa');
 botao.addEventListener('click', criarTarefa);
+
+/* function limpaTarefas() {
+  // ref: http://devfuria.com.br/javascript/dom-remove-child/
+  const lista = document.getElementsByTagName('ol');
+  const itens = document.getElementsByTagName('li');
+  lista.removeChild(itens[0]);
+} */
+
+function limpaTarefas() {
+  const lista = document.getElementById('lista-tarefas');
+  lista.innerHTML = '';
+}
+
+const limpar = document.getElementById('apaga-tudo');
+limpar.addEventListener('click', limpaTarefas);
