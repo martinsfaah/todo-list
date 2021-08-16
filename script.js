d2043dd9c82237f0c0db9555c3444b7e91617a6c
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-08-16 19:30:58 -0300

    adds requirement 7

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// CREATE TASKS WHEN THE BUTTON IS CLICKED
+
 function taskCreator() {
   let newTaskButton = document.getElementById('criar-tarefa');
   newTaskButton.addEventListener('click', function(){
@@ -5,9 +7,28 @@ function taskCreator() {
     let taskList = document.getElementById('lista-tarefas');
     let newTask = document.createElement('li');
     newTask.innerText = userInput.value;
+    newTask.className = 'tarefa';
     taskList.appendChild(newTask);
     userInput.value = '';
   })
 }
 
 taskCreator();
+
+// CHANGE ITEN BACKGROUND COLOR WHEN CLICKED
+
+function selectTask() {
+  let tasksList = document.getElementById('lista-tarefas');
+  tasksList.addEventListener('click', function(event) {
+    // let tasksCreated = document.getElementsByClassName('tarefa');
+    // for (task of tasksCreated) {
+      if (event.target.className === 'tarefa') {
+        event.target.className = 'tarefa selecionada';
+      } else if (event.target.className === 'tarefa selecionada') {
+        event.target.className = 'tarefa';
+      }
+    // }
+  })
+}
+
+selectTask();
