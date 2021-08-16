01d295cf2a91a83471fb38645f084e8701f80309
gabriela-veloso <gabrielaveloso.engmateriais@gmail.com>
2021-08-16 20:24:40 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function listTasks() {
 listTasks();
 
 const task = document.getElementById('lista-tarefas');
-
+const allTasks = task.children;
 function clearColor() {
   const tasks = task.children;
   for (let i = 0; i < tasks.length; i += 1) {
@@ -31,4 +31,22 @@ task.addEventListener('click', (task2) => {
   clearColor();
   const selectedTask = task2;
   selectedTask.target.style.backgroundColor = 'rgb(128,128,128)';
+});
+
+function findSelectedClass(array) {
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === 'completed') {
+      return true;
+    }
+  }
+  return false;
+}
+
+task.addEventListener('dblclick', (task2) => {
+  const doubleClicked = task2.target.classList;
+  if (findSelectedClass(doubleClicked)) {
+    doubleClicked.remove('completed');
+  } else {
+    doubleClicked.add('completed');
+  }
 });
