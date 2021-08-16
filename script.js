27aef359c1c1f49730175e89444b75f21c46bd93
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-08-16 17:26:51 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -22,13 +22,24 @@ function createOrderList() {
 }
 createOrderList();
 
+function colorListItem(event) {
+  const itensLista = document.getElementsByClassName('itens-lista-individuais');
+  const alvo2 = event.target;
+  for (let i = 0; i < itensLista.length; i += 1) {
+    itensLista[i].style.backgroundColor = 'transparent';
+    alvo2.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+}
+
 function addTask() {
   const inputField = document.querySelector('#texto-tarefa');
   const orderList = document.querySelector('#lista-tarefas');
   const listItem = document.createElement('li');
   orderList.appendChild(listItem);
+  listItem.className = 'itens-lista-individuais';
   listItem.innerText = inputField.value;
   inputField.value = '';
+  listItem.addEventListener('click', colorListItem);
 }
 
 const buttonAdd = document.querySelector('#criar-tarefa');
