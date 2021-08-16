c02b078aac71f12cad8a2ea8d6d6852e8fd4cc7f
Pedro Figueiredo <pedrofigueiredo25@outlook.com>
2021-08-16 15:26:27 -0300

    Requisitos 8 e 9

--- script.js
+++ script.js
@@ -2,12 +2,29 @@ const criarTarefaBTN = document.getElementById("criar-tarefa");
 const inputTarefa = document.getElementById("texto-tarefa");
 const listaTarefas = document.getElementById("lista-tarefas");
 
-function adicionarTarefa(){
-const inputText = inputTarefa.value; //https://www.w3schools.com/jsref/prop_text_value.asp material consultado
-const novaTarefa = document.createElement("li")
-novaTarefa.innerHTML = inputText;
-listaTarefas.appendChild(novaTarefa);
-inputTarefa.value = "";
-}
+criarTarefaBTN.addEventListener("click", function () {
+  const inputText = inputTarefa.value; //https://www.w3schools.com/jsref/prop_text_value.asp material consultado
+  const novaTarefa = document.createElement("li")
+  novaTarefa.innerHTML = inputText;
+  listaTarefas.appendChild(novaTarefa);
+  inputTarefa.value = "";
+});
 
-criarTarefaBTN.addEventListener("click", adicionarTarefa);
+listaTarefas.addEventListener("click", function(event){
+    for (let index = 0; index < listaTarefas.children.length; index += 1){
+        listaTarefas.children[index].style.backgroundColor = 'white';
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+    } 
+})
+
+listaTarefas.addEventListener('dblclick', function(event){
+    if (event.target.style.textDecoration === 'line-through solid rgb(0, 0, 0)'){
+        event.target.style.textDecoration = '';
+        event.target.classList.remove('completed')
+    }else{
+        event.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+        event.target.classList.add('completed')
+    }
+})
+
+
