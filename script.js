05905283dc719eeff5cb5ec8bf04f586cf1b3b51
Emmanuelle Pereira <emma.persil@gmail.com>
2021-08-16 17:46:46 -0300

    Requisito 7 feito

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ const addTask = document.querySelector('#criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
 const textTask = document.querySelector('#texto-tarefa');
 
-addTask.addEventListener('click', novaTarefa);
-
-function novaTarefa() {   
+//Adiciona nova tarefa
+function novaTarefa() {
   const tarefa = document.createElement('li');
   tarefa.innerText = textTask.value;
   lista.appendChild(tarefa);
   textTask.value = '';
 }
+
+addTask.addEventListener('click', novaTarefa);
+
+//Troca cor do fundo de um item
+function trocaCor(evento) {
+  const fundoTarefa = evento.target;
+  fundoTarefa.style.backgroundColor = 'rgb(128,128,128)';
+}
+
+lista.addEventListener('click', trocaCor);
