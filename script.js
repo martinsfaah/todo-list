a28052b5043c1558cec3d8d7926510cb36456e07
Emily Carvalho <nilsen.loli.emily@gmail.com>
2021-08-16 15:28:32 -0300

    refatorando requisito 3

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-
-const header = document.querySelector('header')
+const header = document.querySelector('header'); // Requisito 2 : cria subtitulo abaixo do titulo
 const subtitulo = document.createElement('p');
-subtitulo.innerHTML = "Clique duas vezes em um item para marcá-lo como completo";
+subtitulo.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
 subtitulo.id = 'funcionamento';
 header.appendChild(subtitulo);
+
+// Requisito 3 : cria uma caixa input para receber valor
+const main = document.querySelector('main');
+const caixaTexto = document.createElement('input');
+caixaTexto.setAttribute('type', 'text');
+caixaTexto.id = 'texto-tarefa';
+main.appendChild(caixaTexto);
+
+// Requisito 4
