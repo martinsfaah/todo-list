12d553912a477f170aed8796e2a6a3c7071a1113
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-08-16 14:56:10 -0300

    refatoraçao dos requisitos 1-4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const header = document.getElementsByTagName('header');
-const main = document.getElementsByTagName('main');
+// const main = document.getElementsByTagName('main');
 const sectionListControl = document.getElementById('task-list-control');
-const taskListContent = document.getElementById('task-list-content');
+const sectionListContent = document.getElementById('task-list-content');
 
 const newSelector = (qttOfSelector, element, selector, selectorName) => {
   for (let index = 0; index < qttOfSelector; index += 1) {
@@ -12,16 +12,16 @@ const newSelector = (qttOfSelector, element, selector, selectorName) => {
     }
   }
 };
-const createNewElement = (element, elementContent, location, qttofElements) => {
+const createNewElement = (element, elementContent, location, qttofElements, selector, selectorName) => {
   for (let index = 0; index < qttofElements; index += 1) {
     const newElement = document.createElement(element);
     newElement.innerHTML = elementContent;
     if (typeof (location.length) === 'undefined') {
       location.appendChild(newElement);
-      // newSelector(qttofElements, location.appendChild(newElement), selector, selectorName);
+      newSelector(qttofElements, location.appendChild(newElement), selector, selectorName);
     } else {
       location[index].appendChild(newElement);
-      // newSelector(qttofElements, location[index].appendChild(newElement), selector, selectorName);
+      newSelector(qttofElements, location[index].appendChild(newElement), selector, selectorName);
     }
   }
 };
@@ -30,18 +30,14 @@ const createNewElement = (element, elementContent, location, qttofElements) => {
 createNewElement('h1', 'Minha Lista de Tarefas', header, 1);
 
 // #REQ 2
-createNewElement('p', 'Clique duas vezes em um item para marcá-lo como completo', header, 1);
-const pHeader = document.querySelector('header p');
-newSelector(1, pHeader, 'id', 'funcionamento');
+createNewElement('p', 'Clique duas vezes em um item para marcá-lo como completo', header, 1, 'id', 'funcionamento');
 
 // #REQ 3
-createNewElement('input', '', sectionListControl, 1);
-const inputAddListItem = document.querySelector('main input');
-newSelector(1, inputAddListItem, 'id', 'texto-tarefa');
+createNewElement('input', '', sectionListControl, 1, 'id', 'texto-tarefa');
+
 
 // #REQ 4
-createNewElement('ol', '', taskListContent, 1);
-const taskList = document.querySelector('main ol');
-newSelector(1, taskList, 'id', 'lista-tarefas');
+createNewElement('ol', '', sectionListContent, 1, 'id', 'lista-tarefas');
 
 // #REQ 5
+createNewElement('button', 'Adicionar', sectionListControl, 1, 'id', 'criar-tarefa');
