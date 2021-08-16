243fda0953ae49fafebde2ae9bfe3f2a6acd946b
Júnior Zanoni <zanoni.odenir@gmail.com>
2021-08-16 15:52:17 -0300

    7-requisito

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ function criarTarefa() {
   listaTarefas.appendChild(novaTarefa).innerText = input;
   document.getElementById('texto-tarefa').value = '';
 }
+
+// Altera a cor de fundo ao clicar sobre uma tarefa
+function trocaCor() {
+  this.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+// Atualiza a lista de tarefas após adicionar
+function atualizaLista() {
+  const lista = document.querySelectorAll('li');
+  for (let i = 0; i < lista.length; i += 1) {
+    lista[i].addEventListener('click', trocaCor);
+  }
+}
+
 btnCriarTarefa.addEventListener('click', criarTarefa);
+btnCriarTarefa.addEventListener('click', atualizaLista);
