fefb558c5a24bea678a84a0a9007b6daae9c1a41
alexandrexpj <alexandrexpjr@hotmail.com>
2021-08-16 16:23:35 -0300

    pequenas alterações para o lint

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-window.onload = function() {
+window.onload = function () {
   const getTask = document.querySelector('#texto-tarefa');
   const getButton = document.querySelector('#criar-tarefa');
   const getOL = document.querySelector('#lista-tarefas');
 
-  getButton.addEventListener('click', function() {
+  getButton.addEventListener ('click', function() {
     const createTask = document.createElement('li');
     createTask.innerHTML = getTask.value;
     getOL.appendChild(createTask);
     getTask.value = '';    
-  })
+  });
 
-  getOL.addEventListener('click', function(event) {
+  getOL.addEventListener ('click', function(event) {
     const changeStyle = event.target.style;
     const getLi = document.querySelectorAll('li');
     for (let index = 0; index < getLi.length; index += 1) {
-      getLi[index].style.backgroundColor = 'white'
+      getLi[index].style.backgroundColor = 'white';
     }
     changeStyle.backgroundColor = 'rgb(128,128,128)';
-  })
+  });
 
-  getOL.addEventListener('dblclick', function(event) {
+  getOL.addEventListener ('dblclick', function(event) {
     const changeStyle = event.target.style;
     const changeClass = event.target.className;
     const getLi = document.querySelectorAll('li');
@@ -28,25 +28,24 @@ window.onload = function() {
     } else {
       event.target.className = '';
     }
-  })
+  });
 
-  const getClearButton = document.querySelector('#apaga-tudo');
+  const getClearButton = document.querySelector ('#apaga-tudo');
 
-  getClearButton.addEventListener('click', function() {
+  getClearButton.addEventListener ('click', function() {
     const getLi = document.querySelectorAll('li');
     for (let index = 0; index < getLi.length; index += 1) {
       getLi[index].remove();
     }
-  })
+  });
 
-  const getRemoveButton = document.querySelector('#remover-finalizados');
+  const getRemoveButton = document.querySelector ('#remover-finalizados');
 
-  getRemoveButton.addEventListener('click', function() {
+  getRemoveButton.addEventListener ('click', function() {
     const getLi = document.querySelectorAll('li');
     const getCompleted = document.querySelectorAll('.completed');
     for (let index = 0; index < getCompleted.length; index += 1) {
       getCompleted[index].remove();
     }
-  })
-
+  });
 }
