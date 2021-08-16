d1e1126b0845fd6358544e8e454e4f8b30ce2893
viniciusldn <viniciusldn0@gmail.com>
2021-08-16 17:53:31 -0300

    R8 completed

--- script.js
+++ script.js
@@ -8,16 +8,39 @@ createTodoList.addEventListener('click', function() {
   let li = document.createElement('li');
   li.className = 'todo-item';
   li.textContent = todo.value;
-  li.addEventListener('click', function() {
-    li.style.backgroundColor = 'rgb(128, 128, 128)';
-  })
+  li.addEventListener('click', selectionHandler);
   document.getElementById('lista-tarefas').appendChild(li);
   todo.value = '';
 })
 
-// for (let i = 0; i < todoItems.length; i += 1) {
-//   if (todoItems[i].style.backgroundColor === 'rgb(128, 128, 128)') {
-//     todoItems[i].style.backgroundColor = null;
+function selectionHandler() {
+  for (i = 0; i < todoItems.length; i += 1) {
+    if (todoItems[i].classList.contains('selected')) {
+      todoItems[i].classList.remove('selected');
+      todoItems[i].style.backgroundColor = null;
+    }
+  }
+  this.classList.add('selected');
+  this.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+// function selectItem() {
+//   for (let i = 0; i < event.length; i += 1) {
+//     event[i].classList.remove('selected');
 //   }
-//   todoItems[i].style.backgroundColor = 'rgb(128, 128, 128)';
+//   for (let i = 0; i < event.length; i += 1) {
+//     event[i].classList.add('selected');
+//     selectedColor = event[i].style.backgroundColor;
+//     console.log(`${selectedColor} selecionado`)
+//   }
+// }
+
+// function highlight() {
+//   let selected = document.querySelector('selected');
+//   for (let i = 0; i < todoItems.length; i += 1) {
+//     if (todoItems[i] === selected) {
+//       todoItems[i].style.backgroundColor = 'rgb(128, 128, 128)';
+//     } else {
+//       todoItems[i].style.backgroundColor = undefined;
+//     }
+//   }
 // }
