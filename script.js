c70e873ed46cc8efc11aa34c9c68c032f0cd1d83
luigrosz <luigrosz08@gmail.com>
2021-08-16 15:30:41 -0300

    a

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// 5.
-
 const button = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
-function addTask(){
-  let stringText = document.querySelector('#texto-tarefa').value;
-  let createList = document.createElement('li');
+// 5. e 6.
+
+function addTask() {
+  const stringText = document.querySelector('#texto-tarefa').value;
+  const createList = document.createElement('li');
   createList.innerHTML = stringText;
   taskList.appendChild(createList);
   document.querySelector('#texto-tarefa').value = '';
 }
 
 button.addEventListener('click', addTask);
+
+// 7. e 8.
+
