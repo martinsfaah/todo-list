f9d4c1e0589b9ebe5fdb91f35330f53fffd6c62a
Italo Sergio <italosergio@mail.com>
2021-08-16 17:21:52 -0300

    Resolve Desafio 1

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Desafio 1
 const header = document.createElement('header');
 header.innerText = 'Minha Lista de Tarefas';
-const body = document.getElementsByTagName('body')[0];
+const body = document.querySelector('body');
 body.appendChild(header);
 
-const paragraph = document.createElement('p');
-paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo'
+
+
+
