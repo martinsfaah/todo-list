a9107c3bd4726ff7dd7ccfa865629c513c313fdb
Braien <braienmp@outlook.com>
2021-08-16 15:55:24 -0300

    cria evento para selecionar tarefas da lista

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const btnAddTask = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
+let tasks = taskList.children;
+
+function selectTask(event) {
+  let selectedTask = document.getElementById('selected-task');
+  if (selectedTask !== null) {
+    selectedTask.id = '';
+  };
+  selectedTask = event.target;
+  selectedTask.id = 'selected-task';
+}
 
 function addTask() {
   const task = document.createElement('li');
   task.innerHTML = inputTask.value;
+  task.addEventListener('click', selectTask);
   taskList.appendChild(task);
   inputTask.value = '';
 }
 
 btnAddTask.addEventListener('click', addTask);
+
