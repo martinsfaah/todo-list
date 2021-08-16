c4247fbf91020aff630edb3edb8837d9520dd1db
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-08-16 13:16:24 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function criaTarefa() {
     let tarefa = document.querySelector('#texto-tarefa');
     const novoElementoLista = document.createElement('li');
     novoElementoLista.innerText = tarefa.value;
+    novoElementoLista.addEventListener('click',selecionaTarefa);
     document.querySelector('#lista-tarefas').appendChild(novoElementoLista);
     tarefa.value = '';
   }
+}
+
+function selecionaTarefa(evento) {
+  document.querySelector('.selected') !== null && document.querySelector('.selected').classList.remove('selected');
+  evento.target.classList.add('selected')
 }
