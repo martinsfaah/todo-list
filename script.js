8af12ad8f70d83dd0da75e2d8372cb62b73dc671
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-08-16 14:04:52 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Função para mudar o fundo da tarefa selecionada
 function selecionaTarefa(evento) {
   const ultimaTarefa = document.querySelector('.selected');
   if (ultimaTarefa !== null) ultimaTarefa.classList.remove('selected');
   evento.target.classList.add('selected');
 }
 
+// Função para riscar a tarefa completa
 function completaTarefa(evento) {
   const tarefaCompleta = evento.target;
   if (tarefaCompleta.className.includes('completed')) {
@@ -13,6 +15,7 @@ function completaTarefa(evento) {
   }
 }
 
+// Função para criar tarefas na lista
 function criaTarefa() {
   if (document.querySelector('#texto-tarefa').value) {
     const tarefa = document.querySelector('#texto-tarefa');
@@ -25,4 +28,14 @@ function criaTarefa() {
   }
 }
 
+// Função para apagar lista de tarefas
+function apagaTudo() {
+  if (window.confirm('Você está apagando todos os itens da sua lista, deseja continuar?')) {
+    const lista = document.getElementsByTagName('li');
+    while (lista[0]) lista[0].parentNode.removeChild(lista[0]);
+  }
+}
+
+// Criando os eventos para os elementos criados no html
 document.querySelector('#criar-tarefa').onclick = criaTarefa;
+document.querySelector('#apaga-tudo').onclick = apagaTudo;
