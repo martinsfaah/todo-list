88b45454c2e0dbbe55329a0d9737d023b562e9b7
André <andre.bonaf@gmail.com>
2021-08-16 19:20:48 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -4,16 +4,30 @@ function createElements(){
     const p = document.createElement('p');
     const input = document.createElement('input');
     const ol = document.createElement('ol');
+    const button = document.createElement('button');
     header.innerText = 'Minha Lista de Tarefas';
     p.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
     p.id = 'funcionamento';
     input.id = 'texto-tarefa';
     ol.id = 'lista-tarefas';
+    button.id = 'criar-tarefa';
+    button.innerText = 'Adicionar Tarefa';
+    button.addEventListener('click', createTask);
 
     body.appendChild(header);
     body.appendChild(p);
     body.appendChild(input);
     body.appendChild(ol);
+    body.appendChild(button);
 }
 
 createElements();
+
+function createTask(){
+    const writtenTask = document.querySelector('#texto-tarefa').value;
+    const list = document.querySelector('#lista-tarefas');
+    const newTask = document.createElement('li');
+    newTask.innerText = writtenTask;
+    list.appendChild(newTask);
+    document.querySelector('#texto-tarefa').value = '';
+}
