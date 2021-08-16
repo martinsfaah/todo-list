df9fadcf9cc8fc66aefca1b0a066bffc9ba4194b
ricardo-zabir <ricardofonseca.zabir@hotmail.com>
2021-08-16 15:57:28 -0300

    Realização dos 10 primeiros requisitos

--- script.js
+++ script.js
@@ -3,29 +3,33 @@ const input = document.querySelector('input');
 const taskList = document.querySelector('ol');
 
 function addItem() {
-  let itemTask = document.createElement('li');
-  let task = input.value;
+  const itemTask = document.createElement('li');
+  const task = input.value;
   itemTask.innerHTML = task;
   taskList.appendChild(itemTask);
   input.value = '';
   itemTask.addEventListener('click', () => {
-    let arrayOfItems = document.querySelectorAll("li");
-    for(let index = 0;index < arrayOfItems.length; index += 1) {
-      if(arrayOfItems[index].style.backgroundColor === 'rgb(128, 128, 128)') {
+     const arrayOfItems = document.querySelectorAll('li');
+    for (let index = 0; index < arrayOfItems.length; index += 1) {
+      if (arrayOfItems[index].style.backgroundColor === 'rgb(128, 128, 128)') {
         arrayOfItems[index].style.backgroundColor = 'white';
       }
     }
-    itemTask.style.backgroundColor = 'rgb(128, 128, 128)'
+    itemTask.style.backgroundColor = 'rgb(128, 128, 128)';
   });
-  itemTask.addEventListener('dblclick',() => {
-    if(itemTask.className === 'completed') {
+  itemTask.addEventListener('dblclick', () => {
+    if (itemTask.className === 'completed') {
       itemTask.className -= 'completed';
-    }
-    else {
+    } else {
       itemTask.className = 'completed';
     }
-  })
+  });
 }
-createButton.addEventListener('click',addItem);
+createButton.addEventListener('click', addItem);
 const deleteAllTasks = document.querySelector('#apaga-tudo');
-deleteAllTasks.addEventListener('');
+deleteAllTasks.addEventListener('click',() => {
+  let childrenOfTheList = taskList.children;
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  } 
+})
