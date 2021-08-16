76be8552252e97afa8e13c3501d3c1f8791a3bd7
Pedro Henrique <pedrohrds05@hotmail.com>
2021-08-16 19:10:47 -0300

    Cumprindo o requisito 10

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const accessButton = document.querySelector('#criar-tarefa');
 const accessOl = document.querySelector('#lista-tarefas');
 const accessInput = document.querySelector('#texto-tarefa');
+const accessDelete = document.querySelector('#apaga-tudo')
 
 function addText() {
   const createLi = document.createElement('li');
   createLi.innerText = accessInput.value;
+  createLi.classList.add('schedule')
   accessOl.appendChild(createLi);
   accessInput.value = ''; 
 }
 
 accessButton.addEventListener('click', addText)
+
+function deleteList() {
+  accessOl.innerText = ''
+}
+
+accessDelete.addEventListener('click', deleteList)
