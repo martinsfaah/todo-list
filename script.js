547f1df0d6bf79cc4f2f6228a1c3e18ae79ab174
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-08-16 16:43:41 -0300

    8 e 9

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-// 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão,
-// um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
-const oList = document.createElement('ol');
+let oList = document.createElement('ol');
 
 function createOl() {
   document.querySelector('#lista-tarefas-section').appendChild(oList);
   oList.id = 'lista-tarefas';
 }
-createOl();
+
 
 function createList() {
-  let inputPlace = document.querySelector('#texto-tarefa');
-  
+  const inputPlace = document.querySelector('#texto-tarefa');
+
   let buttonInput = document.querySelector('#criar-tarefa');
-  buttonInput.addEventListener('click', function () {
-    
+  buttonInput.addEventListener('click', function (event) {
     const lists = document.createElement('li');
     lists.innerHTML = inputPlace.value;
+    // lists.classList.add('item-List');
+    // event.target.style.backgroundColor = 'rgb(128,128,128)'
 
     document.querySelector('#lista-tarefas').appendChild(lists);
 
@@ -23,4 +22,31 @@ function createList() {
   });
 }
 
+function selectItens() {
+  oList.addEventListener('click', function marker(event) {
+    let oListArray = document.querySelector('#lista-tarefas').children;
+
+    for (let idx = 0; idx < oListArray.length; idx += 1) {
+      oListArray[idx].classList.remove('marked');
+    }
+
+    event.target.classList.add('marked');
+  });
+}
+
+function completedItens() {
+  // oList.addEventListener('dblclick', function (event) {
+  //   event.target.classList.toggle('completed');
+  // });
+
+  oList.addEventListener('dblclick', function(event){
+    event.target.classList.toggle('completed')
+
+  })
+
+}
+
+createOl();
 createList();
+selectItens();
+completedItens();
