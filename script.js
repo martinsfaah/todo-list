523eb850e22a6dd1930158022171e5f0c5d034db
Eric Cruz <effolex@gmail.com>
2021-08-16 20:11:25 -0300

    completado ate o 13 requisito

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const criarTarefa = document.getElementById('criar-tarefa');
-const ListaTarefa = document.getElementById('lista-tarefas');
+const listaTarefa = document.getElementById('lista-tarefas');
+const apagaTudo = document.getElementById('apaga-tudo');
+const removerFinalizados = document.getElementById('remover-finalizados');
+const salvarTarefas = document.querySelector('#salvar-tarefas');
+const moverCima = document.getElementById('mover-cima');
+const moverBaixo = document.getElementById('mover-baixo');
 
-function addNewToDo() {
-  const newListItemInput = document.getElementById('texto-tarefa');
-  const newItem = document.createElement('li');
-  newItem.textContent = newListItemInput.value;
-  newListItemInput.value = '';
-  ListaTarefa.appendChild(newItem);
+function checkElement(element, stringToCompare) {
+  const typeOfTheElement = element.tagName;
+  return (typeOfTheElement === stringToCompare);
 }
 
 function backgroundChange(event) {
@@ -15,8 +17,7 @@ function backgroundChange(event) {
   if (olElement.classList.contains('selected')) {
     return;
   }
-  if (olElement.tagName === 'LI') {
-    olElement.style.backgroundColor = 'rgb(128,128,128)';
+  if (checkElement(olElement, 'LI')) {
     if (oldSelected) {
       oldSelected.classList.remove('selected');
       oldSelected.style.backgroundColor = '';
@@ -25,6 +26,102 @@ function backgroundChange(event) {
   }
 }
 
+function completedTalk(event) {
+  const liElement = event.target;
+  if (liElement.style.textDecoration.length > 1) {
+    liElement.style.textDecoration = '';
+    liElement.classList.remove('completed');
+    return;
+  }
+  liElement.style.textDecoration = 'line-through';
+  liElement.classList.add('completed');
+}
+
+function addOldToDo(stringStorage, classStorage) {
+  const newItem = document.createElement('li');
+  newItem.textContent = stringStorage;
+  newItem.className = classStorage;
+  listaTarefa.appendChild(newItem);
+  newItem.addEventListener('click', backgroundChange);
+  newItem.addEventListener('dblclick', completedTalk);
+}
+
+const storageElemento = JSON.parse(window.localStorage.getItem('list'));
+const storageElementClass = JSON.parse(window.localStorage.getItem('classes'));
+
+if (storageElemento) {
+  storageElemento.forEach((element, index) => addOldToDo(element, storageElementClass[index]));
+}
+function addNewToDo() {
+  const newListItemInput = document.getElementById('texto-tarefa');
+  const newItem = document.createElement('li');
+  newItem.textContent = newListItemInput.value;
+  newListItemInput.value = '';
+  listaTarefa.appendChild(newItem);
+  newItem.addEventListener('click', backgroundChange);
+  newItem.addEventListener('dblclick', completedTalk);
+}
 criarTarefa.addEventListener('click', addNewToDo);
 
-ListaTarefa.addEventListener('click', backgroundChange);
+function limpaOl() {
+  while (listaTarefa.lastElementChild) {
+    listaTarefa.lastElementChild.remove();
+  }
+  window.localStorage.clear();
+}
+
+apagaTudo.addEventListener('click', limpaOl);
+
+function limpaFinalizados() {
+  const finalizados = document.getElementsByClassName('completed');
+  while (finalizados.length) {
+    finalizados[0].remove();
+  }
+}
+
+removerFinalizados.addEventListener('click', limpaFinalizados);
+
+// https://blog.logrocket.com/localstorage-javascript-complete-guide/
+// guia que usei para fazer o local storage
+
+function salvaTarefa() {
+  const listOfToDo = [];
+  const listOfClasses = [];
+  const htmlCollection = document.querySelectorAll('.completed');
+  for (let index = 0; index < htmlCollection.length; index += 1) {
+    listOfToDo.push(htmlCollection[index].textContent);
+    listOfClasses.push(htmlCollection[index].className);
+  }
+  console.log(listOfToDo);
+
+  window.localStorage.setItem('list', JSON.stringify(listOfToDo));
+  window.localStorage.setItem('classes', JSON.stringify(listOfClasses));
+}
+
+salvarTarefas.addEventListener('click', salvaTarefa);
+
+function moveCima() {
+  const selected = document.querySelector('.selected');
+  if (!selected) {
+    return;
+  }
+  const elementoDeCima = selected.previousSibling;
+  if (elementoDeCima) {
+    elementoDeCima.before(selected);
+  }
+}
+
+function moveBaixo() {
+  const selected = document.querySelector('.selected');
+  if (!selected) {
+    return;
+  }
+  const elementoDeBaixo = selected.nextSibling;
+  if (elementoDeBaixo) {
+    elementoDeBaixo.after(selected);
+  }
+}
+
+moverCima.addEventListener('click', moveCima);
+
+moverBaixo.addEventListener('click', moveBaixo);
