a4748497d41d55cfa564fd8119a53e3e8dd12bf0
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-08-16 17:07:44 -0300

    Exercise 7 and 8 solved

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const inputText = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
-
 const button = document.getElementById('criar-tarefa');
+let taskItens = document.getElementsByClassName('tarefa');
 
-function createTask () {
+//Function to create new task according to inputed text, clearing what the user typed and adding event listener to change background color when clicked
+function createTask() {
     const task = document.createElement('li');
     taskList.appendChild(task);
     task.innerHTML = inputText.value;
-    inputText.value = "";
+    inputText.value = '';
+    task.addEventListener('click', colorClick);
+    task.style.backgroundColor = 'white'
+    task.className = 'tarefa'
 }
 
+//Adding event listener to the button
 button.addEventListener('click', createTask);
+
+//Setting rgb(128, 128, 128) to the background color of the current clicked item and reset the others to white 
+function colorClick(listItem) {
+    for(let index = 0; index < taskItens.length; index = index + 1) {
+        taskItens[index].style.backgroundColor = 'white'
+    }
+    listItem.target.style.backgroundColor = 'rgb(128, 128, 128)'
+}
+
