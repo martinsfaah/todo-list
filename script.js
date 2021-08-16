5db7397a2e23115f795ad8781675689c03240614
LuisFillipeSoares <jobluisfillipe@gmail.com>
2021-08-16 19:01:42 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -8,14 +8,27 @@ function criarTarefa() {
   let textoTarefa = newTarefaText.value
   if (textoTarefa) {
     let liTarefasList = document.createElement('li'); //lista tarefas
+    liTarefasList.className = "listaTarefas"
     liTarefasList.innerText = textoTarefa;
     listaTarefas.appendChild(liTarefasList);
     console.log(newTarefaText);
     newTarefaText.value = "";
+    liTarefasList.addEventListener('click', colorirLista);
   }
   else {
     alert('Não é possível acrescentar tarefas em branco');
   }
 }
 
+function colorirLista(evento) {
+  const colorir = evento.target;
+  qualcor(colorir);
+}
 
+function qualcor(colorir) {
+  let tarefas = document.getElementsByClassName('listaTarefas');
+  for (let index = 0; index < tarefas.length; index+=1) {
+    tarefas[index].style.backgroundColor = 'transparent';
+    colorir.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+}
