02a25e3a262ee00f4d9a20568a579d45402eb5f7
Emmanuelle Pereira <emma.persil@gmail.com>
2021-08-16 17:34:11 -0300

    Requisito 6 feito

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const addTask = document.querySelector('#criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
-let textTask = document.querySelector('#texto-tarefa');
+const textTask = document.querySelector('#texto-tarefa');
 
-addTask.addEventListener("click", novaTarefa);
+addTask.addEventListener('click', novaTarefa);
 
-function novaTarefa(){   
-    const tarefa = document.createElement('li');
-    tarefa.innerText = textTask.value;
-    lista.appendChild(tarefa);
+function novaTarefa() {   
+  const tarefa = document.createElement('li');
+  tarefa.innerText = textTask.value;
+  lista.appendChild(tarefa);
+  textTask.value = '';
 }
