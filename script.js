90d060947a3e94e5c824192839394f01b616ac3b
Alex Sandro Sousa <alexsandro091092@gmail.com>
2021-08-16 17:53:15 -0300

    Adicionado requisito 8

--- script.js
+++ script.js
@@ -8,11 +8,28 @@ function addItem() {
     li.innerHTML = value.value;
 
     value.value = '';
-    li.addEventListener('click', () => {
-      li.style.backgroundColor = 'rgb(128, 128, 128)';
+    // li.addEventListener('click', () => {
+    //   li.style.backgroundColor = 'rgb(128, 128, 128)';
+    // });
+    click();
+  });
+}
+
+function click() {
+  const lisTarefa = document.querySelectorAll('.tarefas');
+
+  function limpLis() {
+    lisTarefa.forEach((e) => {
+      e.classList.remove('selected');
+    });
+  }
+
+  lisTarefa.forEach((e) => {
+    e.addEventListener('click', () => {
+      limpLis();
+      e.classList.add('selected');
     });
   });
 }
 
 addItem();
-//bgLi();
