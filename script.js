2b7fb40305b7ea93c8f07e8641d2587015829fde
MuriloAmaral <muralha2000@gmail.com>
2021-08-17 18:34:34 -0300

    requisito12

--- script.js
+++ script.js
@@ -1,58 +1,75 @@
 let inputTasks = document.getElementById("texto-tarefa");
-  let createTask = document.getElementById("criar-tarefa");
-  let taskList = document.getElementById("lista-tarefas");
-  let body = document.querySelector('body');
-  
-  createTask.addEventListener("click", adicionaTarefa);
-  function adicionaTarefa (){
-      let taskValue = inputTasks.value;
-      let newTask = document.createElement("li");
-      newTask.innerHTML = taskValue;
-      taskList.appendChild(newTask);
-      inputTasks.value = "";
-  }
-  function addSelected(element){
-      let tasks = Array.from(taskList.children);
-      for (let index = 0; index < tasks.length; index += 1){
-          let actualTask = tasks[index];
-          if(actualTask.classList.contains("selected")){
-              actualTask.classList.remove("selected");
-          }
-      }
-      element.classList.add("selected");
-  }
-  function checkItem(originEvent) {
-    const element = originEvent.target;
-    if (element.parentNode.id === 'lista-tarefas') {
-      addSelected(element);
-    }
-  }
-  body.addEventListener('click', checkItem);
-  function markCompleted(element) {
-    if (element.classList.contains('completed')) {
-      element.classList.remove('completed');
-    } else {
-      element.classList.add('completed');
-    }
-  }
-  function checkItem2(originEvent) {
-    let element = originEvent.target;
-    if (element.parentNode.id === "lista-tarefas") {
-      markCompleted(element);
-    }
-  }
-  body.addEventListener("dblclick", checkItem2);
-  function apagaLista(){
-     taskList.innerText = "";
-  }
-  document.getElementById("apaga-tudo").addEventListener("click", apagaLista);
+let createTask = document.getElementById("criar-tarefa");
+let taskList = document.getElementById("lista-tarefas");
+let body = document.querySelector('body');
+let loadlist = window
 
-  function removeFinalizados(){
-    let arrayList = Array.from(taskList.children);
-      for(let index = 0; index < arrayList.length; index += 1){
-          if(arrayList[index].classList.contains("completed")){
-              taskList.removeChild(arrayList[index]);
-          }
-      }
+createTask.addEventListener("click", addTask);
+body.addEventListener('click', checkItem);
+body.addEventListener("dblclick", checkItem2);
+document.getElementById("apaga-tudo").addEventListener("click", eraseList);
+document.getElementById("remover-finalizados").addEventListener("click", removeCheck);
+document.getElementById("salvar-tarefas").addEventListener("click", saveList);
+
+function addTask (){
+    let taskValue = inputTasks.value;
+    let newTask = document.createElement("li");
+    newTask.innerHTML = taskValue;
+    taskList.appendChild(newTask);
+    inputTasks.value = "";
+}
+
+function addSelected(element){
+    let tasks = Array.from(taskList.children);
+    for (let i = 0; i < tasks.length; i += 1){
+        let actualTask = tasks[i];
+        if(actualTask.classList.contains("selected")){
+            actualTask.classList.remove("selected");
+        }
+    }
+    element.classList.add("selected");
+}
+
+function checkItem(originEvent) {
+  const element = originEvent.target;
+  if (element.parentNode.id === 'lista-tarefas') {
+    addSelected(element);
   }
-  document.getElementById("remover-finalizados").addEventListener("click", removeFinalizados); 
+}
+
+function markCompleted(element) {
+  if (element.classList.contains('completed')) {
+    element.classList.remove('completed');
+  } else {
+    element.classList.add('completed');
+  }
+}
+
+function checkItem2(originEvent) {
+  let element = originEvent.target;
+  if (element.parentNode.id === "lista-tarefas") {
+    markCompleted(element);
+  }
+}
+
+function eraseList(){
+    taskList.innerText = "";
+}
+
+function removeCheck(){
+  let arrayList = Array.from(taskList.children);
+    for(let i = 0; i < arrayList.length; i += 1){
+        if(arrayList[i].classList.contains("completed")){
+            taskList.removeChild(arrayList[i]);
+        }
+    }
+}
+
+function loadList() {
+  taskList.innerHTML = localStorage.getItem('tasks');
+}
+window.onload = loadList;
+
+function saveList() {
+  localStorage.setItem('tasks', taskList.innerHTML);
+}
