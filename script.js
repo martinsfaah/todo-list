369449571c97ed3dad4b42c476aa0485352d9e8f
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-08-16 22:07:01 -0300

    requisito 7

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ const orderedList = document.querySelector('#lista-tarefas');
 function addToList() {
   const listItem = document.createElement('li');
   listItem.innerHTML = txtInput.value;
+  listItem.classList.add('list-item');
   orderedList.appendChild(listItem);
   txtInput.value = '';
-
+  listItem.addEventListener('click', changeColor);    
 }
 
 addButton.addEventListener('click', addToList);
+
+function changeColor(event) {
+  const selected = document.querySelector('.selected');
+
+  if (selected === null) {
+    event.target.classList.add('selected');
+  } if (selected !== null) {
+    selected.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+}
+
