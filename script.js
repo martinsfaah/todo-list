37a8ec47effa00ca2bcae26904c31898f77352fe
Arthur <arthurteixeira0805@gmail.com>
2021-08-17 18:14:36 -0300

    req 5 and 6  done

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function getInputToList() {
+const list = document.getElementById('lista-tarefas');
+const taskAdded = document.getElementById('texto-tarefa'); 
+const addTaskButton = document.getElementById('criar-tarefa');
 
+function getInputToList() {
+    const newTask = document.createElement('li');
+    newTask.innerText = taskAdded.value;
+    list.appendChild(newTask);
+    taskAdded.value = '';
 }
 
-console.log(document.getElementById('texto-tarefa'))
+addTaskButton.addEventListener('click', getInputToList)
