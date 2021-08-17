35239ede8fd5b1e75fc9d2bcbd4ea198e6c038fd
Arthur Simões <arthursimoes155@gmail.com>
2021-08-17 09:56:48 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-function createTask() {
-    const buttonTask = document.getElementById("criar-tarefa")
-    let input = document.getElementById("texto-tarefa")
-    let olList = document.getElementById("lista-tarefas")
-    buttonTask.addEventListener("click", function(){
-        let createLi = document.createElement("li")
-        createLi.innerText = input.value
-        olList.appendChild(createLi) 
-        input.value = ""
-    })
+// Desafio 5
+const buttonTask = document.getElementById("criar-tarefa");
+const input = document.getElementById("texto-tarefa");
+const olList = document.getElementById("lista-tarefas");
+
+buttonTask.addEventListener("click", function(){
+    let createLi = document.createElement("li")
+    createLi.innerText = input.value
+    olList.appendChild(createLi) 
+    input.value = ""
+})
+
+//desafio 7 e 8
+function removeColor() {
+    let taskList = document.getElementsByTagName("li")
+    for (let i = 0; i < taskList.length; i +=1) {
+        taskList[i].style.backgroundColor = ""
+    }
 }
-createTask()
+olList.addEventListener("click", function(event) {
+    removeColor()
+    event.target.style.backgroundColor = "rgb(128,128,128)"
+})
+
+
+
+
 
