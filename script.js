5c198b15e0485d6fab3d9b7e47c069d642091ca7
SaraRabelo <rabelorsara@gmail.com>
2021-08-17 18:14:37 -0300

    Solução 7a Questão

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-/*windows.onload =() =>{
+/* windows.onload =() =>{
     const inputTexto = document.querySelector('#texto-tarefa');
     inputTexto.addEventListener('keyup', (evento) => {
     let texto = evento.target.value;
     });
-};*/
+}; */
 
 const tagTarefas = document.querySelector('#tarefas');
 const btCriarTarefas = document.createElement('button');
-const inputTexto = document.querySelector('#texto-tarefa')
+const inputTexto = document.querySelector('#texto-tarefa');
 btCriarTarefas.id = 'criar-tarefa';
 btCriarTarefas.innerHTML = 'Criar Tarefas';
 tagTarefas.appendChild(btCriarTarefas);
 btCriarTarefas.addEventListener('click', (evento) => {
-    const tagOl = document.querySelector('ol');
-    const tagLi = document.createElement('li');
-    tagLi.className = 'item';
-    tagLi.innerText = inputTexto.value;
-    tagOl.appendChild(tagLi);
-    inputTexto.value = '';
+  const tagOl = document.querySelector('ol');
+  const tagLi = document.createElement('li');
+  tagLi.className = 'item';
+  tagLi.innerText = inputTexto.value;
+  tagOl.appendChild(tagLi);
+  inputTexto.value = '';
 });
+
+tagOl = document.querySelector('#lista-tarefas')
+tagOl.addEventListener('click', function(event) {
+  if (event.target.classList.contains('item')) {
+    const atual = document.querySelector('.atual');
+    if (atual !== null) {
+     atual.classList.remove('atual');
+    } 
+    event.target.classList.add('atual');
+  }
+})
+
