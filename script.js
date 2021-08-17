0bac3d5e4431cda7e4965ae95071ac6f8df81dfd
William Schmidt <9williamschmidt@gmail.com>
2021-08-17 18:39:54 -0300

    requisito 9

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const input = document.createElement('input');
 const listaOrdenada = document.createElement('ol');
 const buttonCriarTarefa = document.createElement('button');
 
-
 tagh1.innerText = 'Minha Lista de Tarefas';
 tagSectionCabecalho.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 
@@ -25,36 +24,50 @@ function botaoCriarTarefa() {
   const textoValue = document.querySelector('#texto-tarefa');
   const ordenedList = document.querySelector('#lista-tarefas');
 
-  buttonCriarTarefa.addEventListener('click', function(event) {
-      let newLi = document.createElement('li');
-      newLi.classList.add('lista');
-      newLi.innerText = textoValue.value;
-      ordenedList.appendChild(newLi);
-      textoValue.value = "";
+  buttonCriarTarefa.addEventListener('click', () => {
+    const newLi = document.createElement('li');
+    newLi.classList.add('lista');
+    newLi.innerText = textoValue.value;
+    ordenedList.appendChild(newLi);
+    textoValue.value = '';
   });
 }
 
 function changeBackgroundColoronClick() {
-  let listElmnt = document.querySelectorAll('ol');
+  const listElmnt = document.querySelectorAll('ol');
 
   for (let i = 0; i < listElmnt.length; i += 1) {
-    listElmnt[i].addEventListener('click', function(event) {
-    removeBackgroundColorChanges(event.target);
-    event.target.classList.add('selected');
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-    return event.target;
-    }); 
-  } 
+    listElmnt[i].addEventListener('click', (event) => {
+      removeBackgroundColorChanges();
+      event.target.classList.add('selected');
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+      return event.target;
+    });
+  }
 }
 
-function removeBackgroundColorChanges(element) {
+function removeBackgroundColorChanges() {
   const removeHighlight = document.getElementsByClassName('lista');
 
-  for(let i = 0; i < removeHighlight.length; i += 1) {
-      if(removeHighlight[i].classList.contains('selected')) {
-          removeHighlight[i].classList.remove('selected');
-          removeHighlight[i].style.backgroundColor = "";
+  for (let i = 0; i < removeHighlight.length; i += 1) {
+    if (removeHighlight[i].classList.contains('selected')) {
+      removeHighlight[i].classList.remove('selected');
+      removeHighlight[i].style.backgroundColor = '';
+    }
+  }
+}
+
+function doubleClick() {
+  const olList = document.querySelectorAll('ol');
+
+  for (let i = 0; i < olList.length; i += 1) {
+    olList[i].addEventListener('dblclick', (event) => {
+      if (event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed');
+      } else {
+        event.target.classList.add('completed');
       }
+    });
   }
 }
 
@@ -70,3 +83,4 @@ criaID(listaOrdenada, 'lista-tarefas');
 criaID(buttonCriarTarefa, 'criar-tarefa');
 botaoCriarTarefa();
 changeBackgroundColoronClick();
+doubleClick();
