543b63cc8866645c6bb08243113a29c87e0cbbcd
guilhermecd15 <guilhermecd15@gmail.com>
2021-08-17 14:01:31 -0300

    13 - Adicione dois botões, um com id="mover-cima" e outro com id="mover-baixo", que permitam mover o item selecionado para cima ou para baixo na lista de tarefas

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const corDestaque = 'rgb(128, 128, 128)';
 const btClear = document.querySelector('#apaga-tudo');
 const btRemoverFinalizados = document.querySelector('#remover-finalizados');
 const btRemoverSelecionado = document.querySelector('#remover-selecionado');
+const btMoverCima = document.querySelector('#mover-cima');
+const btMoverBaixo = document.querySelector('#mover-baixo');
 
 function addTarefa() {
   const input = document.querySelector('#texto-tarefa');
@@ -30,7 +32,7 @@ listaTarefas.addEventListener('click', selecTarefa);
 function riscarTarefa(evento) {
   const aux = evento;
   if (aux.target.className === 'completed') {
-    aux.target.className = 'notCompleted';
+    aux.target.classList.remove('completed');
   } else {
     aux.target.className = 'completed';
   }
@@ -62,10 +64,53 @@ function apagarSelecionado() {
   const tarefas = document.querySelectorAll('li');
   for (let i = 0; i < tarefas.length; i += 1) {
     const aux = tarefas[i];
-    if (aux.style.backgroundColor === corDestaque){
+    if (aux.style.backgroundColor === corDestaque) {
       aux.remove();
     }
   }
 }
 
 btRemoverSelecionado.addEventListener('click', apagarSelecionado);
+
+function moverCima() {
+  const tarefas = document.querySelectorAll('li');
+  for (let i = 1; i < tarefas.length; i += 1) {
+    const selecionada = tarefas[i];
+    const acima = tarefas[i - 1];
+    const auxHtlm = acima.innerHTML;
+    const auxColor = 'white';
+    const auxClass = acima.className;
+    if (selecionada.style.backgroundColor === corDestaque) {
+      acima.innerHTML = selecionada.innerHTML;
+      acima.style.backgroundColor = selecionada.style.backgroundColor;
+      acima.className = selecionada.className;
+      selecionada.innerHTML = auxHtlm;
+      selecionada.style.backgroundColor = auxColor;
+      selecionada.className = auxClass;
+    }
+  }
+}
+
+btMoverCima.addEventListener('click', moverCima);
+
+function moverBaixo() {
+  const tarefas = document.querySelectorAll('li');
+  for (let i = tarefas.length - 2; i >= 0; i -= 1) {
+    console.log(tarefas.length);
+    const selecionada = tarefas[i];
+    const aBaixo = tarefas[i + 1];
+    const auxHtlm = aBaixo.innerHTML;
+    const auxColor = 'white';
+    const auxClass = aBaixo.className;
+    if (selecionada.style.backgroundColor === corDestaque) {
+      aBaixo.innerHTML = selecionada.innerHTML;
+      aBaixo.style.backgroundColor = selecionada.style.backgroundColor;
+      aBaixo.className = selecionada.className;
+      selecionada.innerHTML = auxHtlm;
+      selecionada.style.backgroundColor = auxColor;
+      selecionada.className = auxClass;
+    }
+  }
+}
+
+btMoverBaixo.addEventListener('click', moverBaixo);
