3a86fb011a63c32db974217df87865195c6c2a54
Rodrigo Moreira <rodrigossmoreira@hotmail.com>
2021-08-16 21:19:30 -0300

    exercicio 7

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 let listaTarefas = document.getElementById('lista-tarefas');
 let tarefasInput = document.getElementById('texto-tarefa');
 let buttonCriaTarefa = document.getElementById('criar-tarefa');
+let changeBackColor = document.getElementsByClassName('lista');
+let botaoApaga = document.getElementById('apaga-tudo');
+let tarefasLista = document.getElementsByClassName('lista');
+let taferasCriadas = document.querySelectorAll('.lista');
 
-buttonCriaTarefa.addEventListener('click', criaTarefa);
+// Exercicio 5
+
+
+function changeColor(event) {
+    if (event.target.style.backgroundColor != 'rgb(128, 128, 128)') {
+        console.log('rgb(128, 128, 128)');
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+    else {
+        event.target.style.backgroundColor = 'white';
+        console.log('ola');
+    }
+}
 
 function criaTarefa() {
     let tarefas = document.createElement('li');
+    tarefas.className = 'lista';
     tarefas.innerText = tarefasInput.value;
     listaTarefas.appendChild(tarefas);
-    tarefasInput.value = '';
+    tarefasInput.value = ''; 
+    
+    tarefas.addEventListener('click', changeColor)
     
 }
+
+buttonCriaTarefa.addEventListener('click', criaTarefa);
+
+
+
+// Exercicio 10
+
+// botaoApaga.addEventListener('click', apagar);
+
+// function apagar() {
+//         taferasCriadas.reset(); 
+//     }
