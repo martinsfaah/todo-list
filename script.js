71809fbc603ad8a96431e7f90d93d3858948e79f
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-08-17 18:39:29 -0300

    requisito 11

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
-let button = document.querySelector("#criar-tarefa");
-let listaOrdenadaTarefas = document.querySelector("#lista-tarefas");
-let input = document.querySelector("#texto-tarefa");
+//Criando uma Lista Ordenada de Tarefas:
+let buttonCriarTarefa = document.getElementById("criar-tarefa");
+let listaOrdenadaTarefas = document.getElementById("lista-tarefas");
+let input = document.getElementById("texto-tarefa");
+let body = document.getElementsByTagName("body");
+let listaTarefas = document.querySelectorAll(".itemLista");
+let buttonApagaTudo = document.getElementById("apaga-tudo");
+let buttonApagaFinalizados = document.getElementById("remover-finalizados");
 
-button.addEventListener("click",adicionarTarefa);
+buttonCriarTarefa.addEventListener("click",adicionarTarefa);
 function adicionarTarefa() {
     let valueInput = input.value;
     let novaTarefa = document.createElement("li");
     novaTarefa.innerHTML = valueInput;
+    novaTarefa.className = "itemLista";
+    novaTarefa.addEventListener("click",pintaItem);
+    novaTarefa.addEventListener("dblclick",riscaItem);
     listaOrdenadaTarefas.appendChild(novaTarefa);
     input.value = "";
 }
+
+//Mudando o backgroundColor das Tarefas:
+function pintaItem(evento) {
+  let tarefas = Array.from(listaOrdenadaTarefas.children);
+  for (let index = 0; index < tarefas.length; index += 1) {
+    let atualTarefa = tarefas[index];
+    if (atualTarefa.classList.contains("selected")) {
+      atualTarefa.classList.remove("selected");
+    }
+    evento.target.classList.add("selected");
+  }
+};
+
+function riscaItem(evento) {
+  if (evento.target.classList.contains("completed")) {
+    evento.target.classList.remove("completed");
+  }
+  else {
+    evento.target.classList.add("completed");
+  }
+};
+
+//Botão Apaga Tudo:
+buttonApagaTudo.addEventListener("click",function(evento) {
+  listaOrdenadaTarefas.innerHTML = "";
+});
+
+//Botão Apaga Finalizados:
+buttonApagaFinalizados.addEventListener("click",function(evento) {
+  let arrayCompleted = Array.from(listaOrdenadaTarefas.children);
+  for (let index = 0; index < arrayCompleted.length; index += 1) {
+    let itemAtual = arrayCompleted[index];
+    if (itemAtual.classList.contains("completed")) {
+      listaOrdenadaTarefas.removeChild(itemAtual);
+    }
+  }
+});
+
+// function addSelected(element) {
+//   // força recálculo de taskList (agora, com itens adicionados)
+//   const tasks = Array.from(listaOrdenadaTarefas.children);
+//   for (let counter = 0; counter < tasks.length; counter += 1) {
+//     const currentTask = tasks[counter];
+//     if (currentTask.classList.contains('selected')) {
+//       currentTask.classList.remove('selected');
+//     }
+//   }
+//   element.classList.add("selected");
+// }
 
