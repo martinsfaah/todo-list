cd9701d2afd3302e9e57d5e5a8d42b943994e591
Emily Carvalho <nilsen.loli.emily@gmail.com>
2021-08-17 15:53:52 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const header = document.querySelector('header'); // Requisito 2 : cria subtitulo abaixo do titulo
+let botaoAdiciona = document.querySelector('#criar-tarefa');
+const header = document.querySelector('header');
+const caixaTexto = document.createElement('input');
+
+// Requisito 2 : cria subtitulo abaixo do titulo
 const subtitulo = document.createElement('p');
 subtitulo.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
 subtitulo.id = 'funcionamento';
@@ -6,7 +10,6 @@ header.appendChild(subtitulo);
 
 // Requisito 3 : cria uma caixa input para receber valor
 const main = document.querySelector('main');
-const caixaTexto = document.createElement('input');
 caixaTexto.setAttribute('type', 'text');
 caixaTexto.id = 'texto-tarefa';
 main.appendChild(caixaTexto);
@@ -16,21 +19,26 @@ const lista = document.createElement('ol');
 lista.id = 'lista-tarefas';
 main.appendChild(lista);
 
-// Requisito 5 e 6: adiciona botao, add informacao na ol e limpa o input
+// Requisito 5,6  e 7: adiciona botao, add informacao na ol e limpa o input
+const listaOl = document.querySelector('#lista-tarefas');
 function adicionaTexto() {
-    let botaoAdiciona = document.querySelector('#criar-tarefa');
-    let listaOl = document.querySelector('#lista-tarefas');
-
-    botaoAdiciona.addEventListener('click', function () {
+    botaoAdiciona.addEventListener('click', function eventoClick() {
         const itemLista = document.createElement('li');
-        itemLista.addEventListener('click', function () {
-            if (itemLista.style.backgroundColor !== 'rgb(128, 128, 128)') {
-                itemLista.style.backgroundColor = 'rgb(128, 128, 128)';
-            }
-        })
         itemLista.innerText = document.getElementById('texto-tarefa').value;
         listaOl.appendChild(itemLista);
         document.getElementById('texto-tarefa').value = null;
     })
 }
 adicionaTexto();
+
+// Requisito 8:
+function selecionaTexto(texto) {
+    const seleciona = document.querySelector('.selected');
+    if (!seleciona) {
+        texto.target.classList.add('selected');
+    } else {
+        seleciona.classList.remove('selected');
+        texto.target.classList.add('selected');
+    }
+}
+listaOl.addEventListener('click', selecionaTexto);
