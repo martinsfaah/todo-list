35661cd9c38e37f83aafc62ae4f1372911c8cd02
William Schmidt <9williamschmidt@gmail.com>
2021-08-17 17:38:16 -0300

    requisito 8

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const input = document.createElement('input');
 const listaOrdenada = document.createElement('ol');
 const buttonCriarTarefa = document.createElement('button');
 
+
 tagh1.innerText = 'Minha Lista de Tarefas';
 tagSectionCabecalho.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 
@@ -26,10 +27,35 @@ function botaoCriarTarefa() {
 
   buttonCriarTarefa.addEventListener('click', function(event) {
       let newLi = document.createElement('li');
+      newLi.classList.add('lista');
       newLi.innerText = textoValue.value;
       ordenedList.appendChild(newLi);
       textoValue.value = "";
-  }); 
+  });
+}
+
+function changeBackgroundColoronClick() {
+  let listElmnt = document.querySelectorAll('ol');
+
+  for (let i = 0; i < listElmnt.length; i += 1) {
+    listElmnt[i].addEventListener('click', function(event) {
+    removeBackgroundColorChanges(event.target);
+    event.target.classList.add('selected');
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    return event.target;
+    }); 
+  } 
+}
+
+function removeBackgroundColorChanges(element) {
+  const removeHighlight = document.getElementsByClassName('lista');
+
+  for(let i = 0; i < removeHighlight.length; i += 1) {
+      if(removeHighlight[i].classList.contains('selected')) {
+          removeHighlight[i].classList.remove('selected');
+          removeHighlight[i].style.backgroundColor = "";
+      }
+  }
 }
 
 bodyChild(header);
@@ -43,3 +69,4 @@ criaID(input, 'texto-tarefa');
 criaID(listaOrdenada, 'lista-tarefas');
 criaID(buttonCriarTarefa, 'criar-tarefa');
 botaoCriarTarefa();
+changeBackgroundColoronClick();
