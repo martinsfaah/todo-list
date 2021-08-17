2479f679159e4f7a234ca20ec09bf9c9c6edcf27
Matheus Zocratto <matheus.zocratto@gmail.com>
2021-08-17 15:32:54 -0700

    requisito 7

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const buttonAdd = document.getElementById("criar-tarefa");
 function includingTasks(){
     const task = document.createElement("li");
     task.innerHTML = inputText.value;
-
+    task.addEventListener("click", colorCheck);
+    
     tasksList.appendChild(task);
     inputText.value = "";
 }
 buttonAdd.addEventListener("click", includingTasks);
+
+function colorCheck(event){
+    const clickedTask = event.target
+    clickedTask.style.backgroundColor = "rgb(128,128,128)";
+}
