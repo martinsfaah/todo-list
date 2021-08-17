a5c131afa08c85266c52fb9bc0157b3a828d01e1
arturbacilla <artur.bacilla1@gmail.com>
2021-08-17 11:20:38 -0300

    13th requisite: Move item up or down

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const addTaskBtn = document.querySelector('#criar-tarefa');
 const clearListBtn = document.querySelector('#apaga-tudo');
 const clearCompletedBtn = document.querySelector('#remover-finalizados');
 const saveTasksBtn = document.querySelector('#salvar-tarefas');
+const moveUpBtn = document.querySelector('#mover-cima');
+const moveDwnBtn = document.querySelector('#mover-baixo');
 let fullTaskLi = document.querySelectorAll('ol#lista-tarefas li');
 
 // Create a function to reset the task selection
@@ -28,6 +30,48 @@ function toggleTaskCompletion(whichTask) {
   });
 }
 
+function moveTaskUp(selectedTask) {
+  const previousTask = selectedTask.previousElementSibling;
+  const currTask = selectedTask;
+  const currentTaskProps = {
+    classes: selectedTask.className,
+    currValue: selectedTask.innerText,
+  };
+  if (previousTask !== null) {
+    const previousTaskProps = {
+      classes: previousTask.className,
+      prevValue: previousTask.innerText,
+    };
+    currTask.className = previousTaskProps.classes;
+    currTask.innerText = previousTaskProps.prevValue;
+    previousTask.className = currentTaskProps.classes;
+    previousTask.innerText = currentTaskProps.currValue;
+  } else {
+    console.log('Cant move that task up');
+  }
+}
+
+function moveTaskDown(selectedTask) {
+  const nextTask = selectedTask.nextElementSibling;
+  const currTask = selectedTask;
+  const currentTaskProps = {
+    classes: selectedTask.className,
+    currValue: selectedTask.innerText,
+  };
+  if (nextTask !== null) {
+    const nextTaskProps = {
+      classes: nextTask.className,
+      nextValue: nextTask.innerText,
+    };
+    currTask.className = nextTaskProps.classes;
+    currTask.innerText = nextTaskProps.nextValue;
+    nextTask.className = currentTaskProps.classes;
+    nextTask.innerText = currentTaskProps.currValue;
+  } else {
+    console.log('Cant move that task down');
+  }
+}
+
 addTaskBtn.addEventListener('click', () => {
   const taskToAddText = document.querySelector('#texto-tarefa');
   if (taskToAddText.value !== '') {
@@ -49,7 +93,8 @@ clearListBtn.addEventListener('click', () => {
 });
 
 clearCompletedBtn.addEventListener('click', () => {
-  for (const comp of fullTaskLi) {
+  const actualList = document.querySelectorAll('ol#lista-tarefas li');
+  for (const comp of actualList) {
     if (comp.classList.contains('completed')) {
       comp.parentNode.removeChild(comp);
     }
@@ -64,6 +109,24 @@ saveTasksBtn.addEventListener('click', () => {
   } else {
     localStorage.clear();
     console.log('Local storage cleared');
+  }
+});
+
+moveUpBtn.addEventListener('click', () => {
+  const currentTask = document.querySelector('.selected');
+  if (currentTask !== null) {
+    moveTaskUp(currentTask);
+  } else {
+    console.log('No task selected to move up');
+  }
+});
+
+moveDwnBtn.addEventListener('click', () => {
+  const currentTask = document.querySelector('.selected');
+  if (currentTask !== null) {
+    moveTaskDown(currentTask);
+  } else {
+    console.log('No task selected to move down');
   }
 });
 
