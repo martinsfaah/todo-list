3bd8835f40a840258d1645c3f0905ec442cb0098
Gabriel Henrique <G3brielhfsouza@outlook.com>
2021-08-17 13:44:54 -0300

    requisitos 7 e 8

--- scrit.js
+++ scrit.js
@@ -1,10 +1,19 @@
 const input = document.getElementById('texto-tarefa');
 const add = document.getElementById('criar-tarefa');
+const List = document.getElementById('lista-tarefas');
+
+function liColor(event) {
+  if (document.querySelector('.selected')) {
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
 
 function addItens() {
   const Li = document.createElement('li');
-  const List = document.getElementById('lista-tarefas');
   Li.innerHTML = input.value;
+  Li.className = 'listItem';
+  Li.addEventListener('click', liColor);
   input.value = '';
   List.appendChild(Li);
 
