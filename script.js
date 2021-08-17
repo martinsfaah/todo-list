fc0b6fa651c97d3a43c7cb51bdb318fb0db5a672
phillipe <rodrigues153@gmail.com>
2021-08-17 19:41:22 -0300

    Adciona requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const button=document.getElementById("criar-tarefa");
 const input = document.getElementById("texto-tarefa");
 const listaOrdenada = document.getElementById("lista-tarefas");
-const lista = document.querySelector('.listaDeTarefas');
+
 const apagaTudo=document.getElementById("apaga-tudo");
+const removerFinalizados=document.getElementById("remover-finalizados");
+
 
 function adicionaTarefa ()
 {
@@ -17,6 +19,8 @@ function adicionaTarefa ()
 }
 button.addEventListener('click',adicionaTarefa);
 
+
+
 function changeColor (event)
 {
     const selected =document.querySelector('.selected');
@@ -29,26 +33,29 @@ listaOrdenada.addEventListener('click',changeColor);
 function itemCompleto (event)
 {
     const completed =document.querySelector('.completed');
-    if (completed!==null){ event.target.classList.remove('completed');}    
-        else {event.target.classList.add('completed')};   
-     
+    if (event.target=== completed)
+    {
+        event.target.classList.remove('completed');
+    }
+    else {  event.target.classList.add('completed');}
 }
-listaOrdenada.addEventListener('dblclick',itemCompleto);
+listaOrdenada.addEventListener('dblclick' ,itemCompleto);
 
 function apaga (event)
 {
     let tamanhoLista = document.querySelector('ol');
-console.log(document.getElementsByTagName('li')[0]);
 tamanhoLista.innerHTML="";
-
-
-   /*  for ( let i=0;i<tamanhoLista.length;i+=1)
-            {
-                
-                let teste=document.getElementsByTagName('li')[i];
-                teste.innerHTML="";
-                console.log(tamanhoLista[i]);
-            } 
-    */
 }
 apagaTudo.addEventListener('click',apaga);
+
+function removeFinalizados ()
+{   
+    let lista = document.getElementsByClassName('listaDeTarefas');
+    console.log(lista);
+    for (let i=lista.length-1;i>=0;i-=1)
+    {
+        
+        if (lista[i].classList.contains('completed')){ listaOrdenada.removeChild(lista[i]);}
+    }
+}
+removerFinalizados.addEventListener('click',removeFinalizados);
