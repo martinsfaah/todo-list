1f65e32905513726be91d82ca91f57f67a9616a9
Regiane Santos <re.cfs235@gmail.com>
2021-08-17 18:33:58 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ let botaoTarefas = document.getElementById('criar-tarefa'); // Botão
 let lista = document.getElementById('lista-tarefas'); // Lista
 
 function criarTarefa() {
-    const newTask = document.createElement('li');
-    newTask.innerText = campoEntrada.value;
-    lista.appendChild(newTask);
-    campoEntrada.value = '';
-  }
+  const newTask = document.createElement('li');
+  newTask.innerText = campoEntrada.value;
+  lista.appendChild(newTask);
+  campoEntrada.value = '';
+}
   
-  botaoTarefas.addEventListener('click', criarTarefa);
+botaoTarefas.addEventListener('click', criarTarefa);
+
+// Requisito 7
+let itemJaSelecionado = 'false';
+
+function mudarCor (event){
+  // console.log('Teste click');
+  if(itemJaSelecionado === 'false'){
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  itemJaSelecionado = 'True';
+  }
+}
+
+lista.addEventListener('click', mudarCor);
