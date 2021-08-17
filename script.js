2eaffc24c6c571baeb0d9fae731d9db1737de98b
Joao-Ortega <joao_ortega17@hotmail.com>
2021-08-17 11:57:25 -0300

    Parte do Lint

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const button = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 button.addEventListener('click', addListItem);
 function addListItem() {
-  let newLi = document.createElement('li');
+  const newLi = document.createElement('li');
   newLi.innerText = input.value;
   ol.appendChild(newLi);
   input.value = '';
-  let li = document.getElementsByTagName('li');
-  for (let index of li) {
+  const li = document.getElementsByTagName('li');
+  for (const index of li) {
     index.addEventListener('click', addGreyColor);
     index.addEventListener('dblclick', lineThrough);
   }
 }
 
 function addGreyColor(event) {
-  let li = document.getElementsByTagName('li');
+  const li = document.getElementsByTagName('li');
   for (const index of li) {
     index.style.backgroundColor = 'white';
   }
@@ -39,7 +39,7 @@ function eraseTaskList() {
 const eraseDoneTasks = document.getElementById('remover-finalizados');
 eraseDoneTasks.addEventListener('click', eraseOnlyDones);
 function eraseOnlyDones() {
-  let completed = document.querySelectorAll('.completed');
+  const completed = document.querySelectorAll('.completed');
   for (const element of completed) {
     element.remove();
   }
@@ -47,16 +47,16 @@ function eraseOnlyDones() {
 
 const saveTasks = document.querySelector('#salvar-tarefas');
 saveTasks.addEventListener('click', saveToLocal);
-let saveLi = [];
+const saveLi = [];
 function saveToLocal() {
-  let li = document.querySelectorAll('li');
+  const li = document.querySelectorAll('li');
   for (let index = 0; index < li.length; index += 1) {
     saveLi.push(li[index].innerText);
   }
   localStorage.setItem('tasks', JSON.stringify(saveLi));
   addClassToLocal();
 }
-let arrayClasses = [];
+const arrayClasses = [];
 function addClassToLocal() {
   const saveClass = document.querySelectorAll('li');
   for (let index = 0; index < saveClass.length; index += 1) {
@@ -68,28 +68,28 @@ function addClassToLocal() {
 window.onload = function initialState() {
   let savedLi;
   let savedLi2;
-  if (localStorage.getItem('tasks') !== null && localStorage.getItem('classes') !== null ) {
+  if (localStorage.getItem('tasks') !== null && localStorage.getItem('classes') !== null ){
     savedLi = JSON.parse(localStorage.getItem('tasks'));
     savedLi2 = JSON.parse(localStorage.getItem('classes'));
     for (let index = 0; index < savedLi.length; index += 1) {
-    let li = document.createElement('li');
-    li.innerText = savedLi[index];
-    li.className = savedLi2[index];
-    li.addEventListener('click', addGreyColor);
-    li.addEventListener('dblclick', lineThrough);
-    ol.appendChild(li);
+      const li = document.createElement('li');
+      li.innerText = savedLi[index];
+      li.className = savedLi2[index];
+      li.addEventListener('click', addGreyColor);
+      li.addEventListener('dblclick', lineThrough);
+      ol.appendChild(li);
     }
   }
-}
+};
 
 const upButton = document.querySelector('#mover-cima');
 upButton.addEventListener('click', switchIndex);
 function switchIndex() {
-  let lista = document.querySelectorAll('li');
-  let colorRgb = 'rgb(128, 128, 128)';
-  for( let index = 1; index < lista.length; index += 1) {
-    if(lista[index].style.backgroundColor === colorRgb) {
-      ol.insertBefore(lista[index], lista[index-1]);
+  const lista = document.querySelectorAll('li');
+  const colorRgb = 'rgb(128, 128, 128)';
+  for ( let index = 1; index < lista.length; index += 1){
+    if (lista[index].style.backgroundColor === colorRgb) {
+      ol.insertBefore(lista[index], lista[index - 1]);
     }
   }
 }
@@ -97,21 +97,21 @@ function switchIndex() {
 const downButton = document.querySelector('#mover-baixo');
 downButton.addEventListener('click', switchIndex2);
 function switchIndex2() {
-  let lista = document.querySelectorAll('li');
-  let colorRgb = 'rgb(128, 128, 128)';
-  for( let index = 0; index < lista.length; index += 1) {
-    if(lista[index].style.backgroundColor === colorRgb) {
-      ol.insertBefore(lista[index], lista[index+2]);
+  const lista = document.querySelectorAll('li');
+  const colorRgb = 'rgb(128, 128, 128)';
+  for ( let index = 0; index < lista.length; index += 1){
+    if (lista[index].style.backgroundColor === colorRgb) {
+      ol.insertBefore(lista[index], lista[index + 2]);
     }
   }
 }
 
 const removeDoneTask = document.querySelector('#remover-selecionado');
 removeDoneTask.onclick = function onlyDoneTask() {
-  let li = document.querySelectorAll('li');
-  for(let element of li) {
-    if(element.style.backgroundColor === 'rgb(128, 128, 128)') {
+  const li = document.querySelectorAll('li');
+  for (const element of li) {
+    if (element.style.backgroundColor === 'rgb(128, 128, 128)') {
       element.remove();
     }
   }
-}
+};
