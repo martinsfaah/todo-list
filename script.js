c771b44c8123f56cb422ce20d266f42963f98d3b
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-08-16 22:04:40 -0300

    Estilização final da página

--- script.js
+++ script.js
@@ -10,21 +10,28 @@ function completaTarefa(evento) {
   const tarefaCompleta = evento.target;
   if (tarefaCompleta.className.includes('completed')) {
     tarefaCompleta.classList.remove('completed');
+    tarefaCompleta.classList.remove('selected');
   } else {
     tarefaCompleta.classList.add('completed');
+    tarefaCompleta.classList.remove('selected');
   }
 }
 
 // Função para criar tarefas na lista
 function criaTarefa(classe, texto) {
+  const listaTarefas = document.querySelector('#lista-tarefas');
   if (texto) {
     const novoElementoLista = document.createElement('li');
     novoElementoLista.innerText = texto;
     novoElementoLista.addEventListener('click', selecionaTarefa);
     novoElementoLista.addEventListener('dblclick', completaTarefa);
     if (classe) novoElementoLista.className = classe;
-    document.querySelector('#lista-tarefas').appendChild(novoElementoLista);
+    listaTarefas.appendChild(novoElementoLista);
     document.querySelector('#texto-tarefa').value = '';
+    if (listaTarefas.children.length > 20) {
+      listaTarefas.removeChild(listaTarefas.lastChild);
+      alert('limite de tarefas atingido');
+    }
   }
 }
 
@@ -100,3 +107,53 @@ document.querySelector('#salvar-tarefas').onclick = salvaTarefas;
 document.querySelector('#mover-cima').onclick = moveAcima;
 document.querySelector('#mover-baixo').onclick = moveAbaixo;
 document.querySelector('#remover-selecionado').onclick = removeSelecionado;
+
+// Adicionando mouse over nos botões para clareza pare pessoa usuária
+function descreverBotão(evento) {
+  const botaoSelecionado = evento.target;
+  switch (botaoSelecionado.id) {
+  case 'remover-selecionado':
+    botaoSelecionado.innerText = 'X - Remover';
+    break;
+  case 'mover-cima':
+    botaoSelecionado.innerText = '↑ - mover para cima';
+    break;
+  case 'mover-baixo':
+    botaoSelecionado.innerText = '↓ - mover para baixo';
+    break;
+  default:
+    break;
+  }
+}
+
+function retornarOriginal(evento) {
+  const botaoSelecionado = evento.target;
+  switch (botaoSelecionado.id) {
+  case 'remover-selecionado':
+    botaoSelecionado.innerText = 'X';
+    break;
+  case 'mover-cima':
+    botaoSelecionado.innerText = '↑';
+    break;
+  case 'mover-baixo':
+    botaoSelecionado.innerText = '↓';
+    break;
+  default:
+    break;
+  }
+}
+
+const botoesComDescricao = document.getElementsByClassName('hover');
+for (let i = 0; i < botoesComDescricao.length; i += 1) {
+  botoesComDescricao[i].addEventListener('mouseover', descreverBotão);
+  botoesComDescricao[i].addEventListener('mouseout', retornarOriginal);
+}
+
+// Adicionando Keyup no input para que a pessoa usuária possa apertar enter em vez de clicar no botão para criar a tarefa
+function enterTeclado(evento) {
+  if (evento.keyCode === 13) {
+    document.getElementById('criar-tarefa').click();
+  }
+}
+const input = document.getElementById('texto-tarefa');
+input.addEventListener('keyup', enterTeclado);
