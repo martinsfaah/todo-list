691cdfa2ef55f928a758e63c5bcf3f8edad21d03
Lygia Dias <lygiiadias@gmail.com>
2021-08-16 21:52:56 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const textoTarefa = document.querySelector("#texto-tarefa");
-const listaTarefas= document.querySelector("#lista-tarefas");
-const criarTarefa= document.querySelector("#criar-tarefa");
+const listaTarefas = document.querySelector("#lista-tarefas");
+const criarTarefa = document.querySelector("#criar-tarefa");
 
 function addTask(){
 if (textoTarefa.value === ''){
@@ -13,3 +13,19 @@ if (textoTarefa.value === ''){
 }
 }
 criarTarefa.addEventListener('click', addTask)
+
+function oneClick(event){
+let selected = document.querySelectorAll('.selected')
+for (i= 0; i < selected.length; i += 1){
+selected[i].classList.remove('selected')
+selected[i].style.backgroundColor = ''
+}
+event.target.classList.add('selected')
+event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+ 
+
+}
+
+
+listaTarefas.addEventListener ('click', oneClick)
+
