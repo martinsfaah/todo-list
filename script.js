361e9cc10cba2a822c5ea1942273e3284aada4dd
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-08-17 18:44:28 -0300

    09 -> OK

--- script.js
+++ script.js
@@ -5,30 +5,36 @@
 
     
 
-    function mudarCor(event){
+    function changeColor(event){
       for (let i = 0 ; i < liItem.length ; i += 1 ){        
           liItem[i].style.backgroundColor = "transparent";
         event.target.style.backgroundColor = "rgb(128,128,128)"        
       }
     }
 
+     function risk(event){        
+      if (event.target.classList.contains("completed")){
+        event.target.classList.remove("completed");
+      } else { event.target.classList.add("completed");
+       }
+      }           
+      
+    
     function creatLi() {
     if(newInput.value.length > 0){
     const newLi = document.createElement("li");
     taskItens.appendChild(newLi);
-    newLi.addEventListener('click', mudarCor) 
-
+    newLi.addEventListener('click', changeColor) 
+    newLi.addEventListener('dblclick', risk)
     newLi.innerText = newInput.value; 
     newInput.value = "";
+
     } else {
     alert("Erro, digite alguma coisa.")
     }
-    }        
+     }      
     buttonTask.addEventListener('click', creatLi) 
-
-  /* [8 - Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo] */        
-     
-   /* 9 - Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item*/
+           
   
    /* 10 - Adicione um botão com id="apaga-tudo" que quando clicado deve apagar todos os itens da lista] */
 /* 
@@ -36,5 +42,5 @@ function createItem
    const resetListButton = document.createElement("button");
    resetListButton.innerText = "clear";
    resetListButton.id = "apaga-tudo";
-   
+
  */
