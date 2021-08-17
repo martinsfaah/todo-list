03110e4dd149a2e76a237600840cdc5c3d6240ea
Victor Araujo <victor_emanuel9@hotmail.com>
2021-08-17 16:58:57 -0300

    setimo requisito

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-//constants declaration
+//  constants declaration
 const btnAddToList = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
 
-
-//events
-btnAddToList.addEventListener('click', addToList);
-
-
-//Functions
+//  Functions
 function addToList() {
-  let inputValue = document.getElementById('texto-tarefa');
+  const inputValue = document.getElementById('texto-tarefa');
   const list = document.getElementById('lista-tarefas');
   const li = document.createElement('li');
 
   if (!inputValue) { // se o valor for falso o alert eh ativado e sai da funcao.
     alert('Valor invalido!');
-    return
+    return;
   }
 
   li.textContent = inputValue.value;
   list.appendChild(li);
   inputValue.value = '';
 }
+
+function selectListElement(event) {
+  event.target.classList.toggle('selected');
+}
+
+//  events
+btnAddToList.addEventListener('click', addToList);
+list.addEventListener('click', selectListElement)
