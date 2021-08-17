ecf14e81bd960312f65875baf130bdb02d678cf8
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2021-08-17 19:03:04 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ const typeButton = document.getElementById('criar-tarefa');
 const listTasks = document.getElementById('lista-tarefas');
 
 function addTask() {
-    const taskNew = document.createElement('li')
-    taskNew.innerHTML = inputText.value;
-    listTasks.appendChild(taskNew);
-    inputText.value = '';
+  const taskNew = document.createElement('li')
+  taskNew.addEventListener('click', ClickColor)
+  taskNew.innerHTML = inputText.value;
+  listTasks.appendChild(taskNew);
+  inputText.value = '';
+  
 
 }
+typeButton.addEventListener('click', addTask)
 
-typeButton.addEventListener('click', addTask) 
+//Pintando a lista
+
+function ClickColor() {   
+  this.style.backgroundColor = 'rgb(128, 128, 128)';
+  console.log(this);
+}
 
 
-
+// alert('Você chegou na sua lista de tarefas! Cumpra todo o combinado, ok? =D')
 // console.log("kk")
