79b53b23b8c87345c4541b4d972cc43636bceabd
William Schmidt <9williamschmidt@gmail.com>
2021-08-17 10:07:04 -0300

    requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const header = document.createElement('header');
 const tagh1 = document.createElement('h1');
 const tagSectionCabecalho = document.createElement('section');
+const input = document.createElement('input');
+const listaOrdenada = document.createElement('ol');
 
 tagh1.innerText = 'Minha Lista de Tarefas';
 tagSectionCabecalho.id = 'funcionamento';
 tagSectionCabecalho.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+input.id = 'texto-tarefa';
+listaOrdenada.id = 'lista-tarefas';
 
 function bodyChild(element) {
   document.body.appendChild(element);
@@ -17,3 +21,5 @@ function childAppend(elementoPai, elementoFilho) {
 bodyChild(header);
 bodyChild(tagSectionCabecalho);
 childAppend(header, tagh1);
+bodyChild(input);
+bodyChild(listaOrdenada);
