1c69aa7354e94fbe63fc6dcad50d1a0d8d467a27
uauditore <fauditore2912@gmail.com>
2021-08-17 19:20:00 -0300

    tentando resolver os problemas

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let botao = document.getElementById("criar-tarefa")
-let input = document.getElementById("texto-tarefa")
+let inputT = document.getElementById("texto-tarefa")
 let lista = document.getElementById("lista-tarefas")
+let apaga = document.getElementById('apaga-tudo')
+let feito = document.getElementsByClassName('completed')
+let filhos = document.querySelectorAll('#lista-tarefas *')
 botao.addEventListener('click', clicarButton)
 function clicarButton () {
   let lista2 = document.createElement('li')
-  lista2.innerText = input.value
-  input.value = ""
+  console.log(lista2)
+  lista2.innerText = inputT.value
+  inputT.value = ""
   lista.appendChild(lista2)
 }
 lista.addEventListener ('click', clicarTexto)
 function clicarTexto () {
-   let corLista = lista.style.backgroundColor = "rgb(128, 128, 128)"
+  
+  console.log(event.target)
+}
+lista.addEventListener ('dblclick', tarefaCompleta)
+function tarefaCompleta (event) {
+  event.target.className = "completed"
+}
+apaga.addEventListener ('click', apagarTudo)
+function apagarTudo () {
+  lista.innerText = ""
 }
