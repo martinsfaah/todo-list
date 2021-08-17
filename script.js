86ddb55c7b6c9fe141ac337a4b19a60af8f85b19
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-08-16 22:35:38 -0300

    07 ok

--- script.js
+++ script.js
@@ -23,13 +23,14 @@
   }
   createItem();
 
-/*   newInput.addEventListener('keyup', function(event) {
-    if (event.keyCode > 0 && newInput.value.length > 0) {
-      let newLi = document.createElement('li');
-      newLi.innerText = getInputField.value;
+/* 7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)] */
 
-      getTaskList.appendChild(newLi);
-      getInputField.value = '';
-    }
-  });
- */
+  let ol = document.querySelector("#lista-tarefas");
+ 
+     ol.addEventListener('click', function(event) {  
+    event.target.style.backgroundColor = "rgb(128,128,128)"
+  })
+
+  /* [8 - Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo] */
+
+  func
