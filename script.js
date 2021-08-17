c850a1835fb4d34aafb7adfa0702fa5f847ef168
Joao-Ortega <joao_ortega17@hotmail.com>
2021-08-17 01:49:46 -0300

    Identação

--- script.js
+++ script.js
@@ -3,78 +3,78 @@ const button = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 button.addEventListener('click', addListItem);
 function addListItem() {
-    let newLi = document.createElement('li');
-    newLi.innerText = input.value;
-    ol.appendChild(newLi);
-    input.value = '';
-    let li = document.getElementsByTagName('li');
-for(let index of li) {
+  let newLi = document.createElement('li');
+  newLi.innerText = input.value;
+  ol.appendChild(newLi);
+  input.value = '';
+  let li = document.getElementsByTagName('li');
+  for (let index of li) {
     index.addEventListener('click', addGreyColor);
     index.addEventListener('dblclick', lineThrough);
-}
+  }
 }
 
 function addGreyColor(event) {
-    let li = document.getElementsByTagName('li');
-    for(const index of li) {
-        index.style.backgroundColor = 'white';
-    }
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  let li = document.getElementsByTagName('li');
+  for (const index of li) {
+    index.style.backgroundColor = 'white';
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 function lineThrough(event) {
-    if(event.target.className === 'completed') {
-        event.target.classList.remove('completed');
-    } else {
+  if (event.target.className === 'completed') {
+    event.target.classList.remove('completed');
+  } else {
     event.target.classList.add('completed');
-}
+  }
 }
 
 const eraseButton = document.getElementById('apaga-tudo');
 eraseButton.addEventListener('click', eraseTaskList);
 function eraseTaskList() {
-    ol.innerText = '';
+  ol.innerText = '';
 }
 
 const eraseDoneTasks = document.getElementById('remover-finalizados');
 eraseDoneTasks.addEventListener('click', eraseOnlyDones);
 function eraseOnlyDones() {
-    let completed = document.querySelectorAll('.completed');
-    for(const element of completed) {
-        element.remove();
-        }
-    }
+  let completed = document.querySelectorAll('.completed');
+  for (const element of completed) {
+    element.remove();
+  }
+}
 
 const saveTasks = document.querySelector('#salvar-tarefas');
 saveTasks.addEventListener('click', saveToLocal);
 function saveToLocal() {
-    let li = document.querySelectorAll('li');
-    let saveLi = [];
-    for(const element of li) {
-        saveLi.push(element.innerText);
-    }
-    localStorage.setItem('tasks', JSON.stringify(saveLi));
-    addClassToLocal();
+  let li = document.querySelectorAll('li');
+  let saveLi = [];
+  for (const element of li) {
+    saveLi.push(element.innerText);
+  }
+  localStorage.setItem('tasks', JSON.stringify(saveLi));
+  addClassToLocal();
 }
 function addClassToLocal() {
-    const saveClass = document.querySelectorAll('li');
-    let arrayClasses = [];
-    for(let index = 0; index < saveClass.length; index += 1) {
-        arrayClasses.push(saveClass[index].className);
-    }
-    localStorage.setItem('classes', JSON.stringify(arrayClasses));
+  const saveClass = document.querySelectorAll('li');
+  let arrayClasses = [];
+  for (let index = 0; index < saveClass.length; index += 1) {
+    arrayClasses.push(saveClass[index].className);
+  }
+  localStorage.setItem('classes', JSON.stringify(arrayClasses));
 }
 
 window.onload = function initialState() {
-    let savedLi;
-    let savedLi2;
-    if(localStorage.getItem('tasks') !== null) {
-        savedLi = JSON.parse(localStorage.getItem('tasks'));
-        savedLi2 = JSON.parse(localStorage.getItem('classes'));
-    } for(let index = 0; index < savedLi.length; index += 1) {
-        let li = document.createElement('li');
-        li.innerText = savedLi[index];
-        li.className = savedLi2[index];
-        ol.appendChild(li);
-    }
+  let savedLi;
+  let savedLi2;
+  if (localStorage.getItem('tasks') !== null) {
+    savedLi = JSON.parse(localStorage.getItem('tasks'));
+    savedLi2 = JSON.parse(localStorage.getItem('classes'));
+  } for (let index = 0; index < savedLi.length; index += 1) {
+    let li = document.createElement('li');
+    li.innerText = savedLi[index];
+    li.className = savedLi2[index];
+    ol.appendChild(li);
+  }
 }
