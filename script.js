faaccc5d0ef8c9a6a7a37c8252b33a9ed0392dde
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-08-17 13:23:51 -0300

    requisito 12

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const buttonDelete = document.querySelector('#apaga-tudo');
 const buttonRemoveCompleted = document.querySelector('#remover-finalizados');
 const buttonList = document.querySelector('#criar-tarefa');
 const inputList = document.querySelector('#texto-tarefa');
-const  arraylist = listItens.children
-
+const arraylist = listItens.children;
+const buttonSaveList = document.querySelector('#salvar-tarefas');
 
 buttonList.addEventListener('click', createList);
 function createList() {
   let list = document.createElement('li');
   list.innerHTML = inputList.value;
-  
+
   document.querySelector('#lista-tarefas').appendChild(list);
-  
+
   inputList.value = '';
 }
 
@@ -30,6 +30,7 @@ function turnToGrey(str) {
 function stripeText() {
   listItens.addEventListener('dblclick', function (event) {
     event.target.classList.toggle('completed');
+
   });
 }
 
@@ -39,17 +40,39 @@ function deleteAll() {
   for (let index = 0; index < arraylistDelete.length; index += 1) {
     arraylistDelete[index].remove();
   }
+
+  localStorage.clear();
 }
 
 buttonRemoveCompleted.addEventListener('click', deleteCompleted);
 function deleteCompleted() {
   let listStripe = document.querySelectorAll('.completed');
   for (let index = 0; index < listStripe.length; index += 1) {
-    listStripe[index].remove()
+    listStripe[index].remove();
   }
 }
-  
 
-turnToGrey('rgb(128, 128, 128)');
-stripeText()
+buttonSaveList.addEventListener('click', saveList);
+
+function saveList() {
+  let listWillSave = document.querySelector('#lista-tarefas').children;
+
+  for (let index = 0; index < listWillSave.length; index += 1) {
+    localStorage.setItem('li' + [index], listWillSave[index].innerHTML);
+    localStorage.setItem('class' + [index], listWillSave[index].className)
+
+  }
+}
+
+function loadLists() {
+  for (let index = 0; index < localStorage.length / 2; index += 1) {
+    let reloadList = document.createElement('li');
+    reloadList.innerHTML = localStorage.getItem("li" + index);
+    reloadList.className = localStorage.getItem("class" + index)
+    document.querySelector('#lista-tarefas').appendChild(reloadList);
+  }
+}
+stripeText();
 deleteCompleted();
+turnToGrey('rgb(128, 128, 128)');
+loadLists();
