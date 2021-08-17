1816d577b98c869014ef24618eb1140c38a83117
devgeorge <glalmeida.dev@gmail.com>
2021-08-17 17:59:30 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ botao.addEventListener("click", adicionaTarefa);
 function adicionaTarefa (){
     let textoDigitado = document.getElementById("texto-tarefa").value;       
     let makeItem = document.createElement("li");
-    list.addEventListener("click" , adicionaBack);
+    makeItem.className = "tarefa"; 
+    //makeItem.classList.add("selected");   
     makeItem.innerText = textoDigitado;   
     console.log(makeItem);
     list.appendChild(makeItem);
     document.getElementById("texto-tarefa").value = "";
 }
+list.addEventListener("click" , adicionaBack);
 function adicionaBack(event){
-    event.target.style.backgroundColor = "rgb(128, 128, 128)";
+    event.target.style.backgroundColor = "rgb(128, 128, 128)";  
+}
+let botaoApaga = document.getElementById("apaga-tudo");
+botaoApaga.addEventListener("click", apagarTarefa);
+function apagarTarefa(event1){
+    location.reload();
 }
