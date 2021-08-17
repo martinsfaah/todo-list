fe82981a27e0eba3f802aab056d774dcf03a19a9
William Schmidt <9williamschmidt@gmail.com>
2021-08-17 10:50:14 -0300

    requisitos 5 e 6

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const tagh1 = document.createElement('h1');
 const tagSectionCabecalho = document.createElement('section');
 const input = document.createElement('input');
 const listaOrdenada = document.createElement('ol');
+const buttonCriarTarefa = document.createElement('button');
 
 tagh1.innerText = 'Minha Lista de Tarefas';
-tagSectionCabecalho.id = 'funcionamento';
 tagSectionCabecalho.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
-input.id = 'texto-tarefa';
-listaOrdenada.id = 'lista-tarefas';
+
+function criaID(elemento, ID) {
+  elemento.id = ID;
+}
 
 function bodyChild(element) {
   document.body.appendChild(element);
@@ -18,8 +20,26 @@ function childAppend(elementoPai, elementoFilho) {
   elementoPai.appendChild(elementoFilho);
 }
 
+function botaoCriarTarefa() {
+  const textoValue = document.querySelector('#texto-tarefa');
+  const ordenedList = document.querySelector('#lista-tarefas');
+
+  buttonCriarTarefa.addEventListener('click', function(event) {
+      let newLi = document.createElement('li');
+      newLi.innerText = textoValue.value;
+      ordenedList.appendChild(newLi);
+      textoValue.value = "";
+  }); 
+}
+
 bodyChild(header);
 bodyChild(tagSectionCabecalho);
 childAppend(header, tagh1);
 bodyChild(input);
 bodyChild(listaOrdenada);
+bodyChild(buttonCriarTarefa);
+criaID(tagSectionCabecalho, 'funcionamento');
+criaID(input, 'texto-tarefa');
+criaID(listaOrdenada, 'lista-tarefas');
+criaID(buttonCriarTarefa, 'criar-tarefa');
+botaoCriarTarefa();
