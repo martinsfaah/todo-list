6edf81e538fb674919375e320b06ad2dfcf4645a
Pedro Henrique <pedrohrds05@hotmail.com>
2021-08-17 16:15:33 -0300

    Cumprindo o requisito 7

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const accessDelete = document.querySelector('#apaga-tudo')
 function addText() {
   const createLi = document.createElement('li');
   createLi.innerText = accessInput.value;
-  createLi.classList.add('schedule')
+  createLi.classList.add('list');
   accessOl.appendChild(createLi);
   accessInput.value = ''; 
+  createLi.addEventListener('dblclick', completedTask)
+  createLi.addEventListener('click', paint)
 }
 
 accessButton.addEventListener('click', addText)
@@ -18,3 +20,15 @@ function deleteList() {
 }
 
 accessDelete.addEventListener('click', deleteList)
+
+function completedTask(event) {
+ if (event.target.style.textDecoration == ''){
+  event.target.style.textDecoration = 'line-through'
+ }
+ else if (event.target.style.textDecoration = '') {
+  }
+}
+
+function paint(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
