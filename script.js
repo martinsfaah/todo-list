e5270add8257614ebfca8d28730d4eb42e67eda8
Alex <alex_cubas1.0@hotmail.com>
2021-08-17 16:21:43 -0300

    exercicio 7 e 8

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
+let listItem = document.getElementsByClassName("list-item")
+
 function listaTarefas () {
   let textoTarefa = document.querySelector("#texto-tarefa")
   let criarTarefa = document.querySelector("#criar-tarefa")
   let listaTarefas = document.querySelector("#lista-tarefas")
- 
-
+  
   criarTarefa.addEventListener("click", function() {
     let lista = document.createElement("li")
+    lista.className = "list-item"
     listaTarefas.appendChild(lista)
     lista.innerHTML = textoTarefa.value
-  console.log(listaTarefas)
+  // console.log(listaTarefas)
     textoTarefa.value = "";
-  })
+
+    lista.addEventListener("click", colorTarefas)
+})
+}
+
+function colorTarefas (event) {
+  if (event.target.className != "selected") {
+  for (let index = 0; index < listItem.length; index += 1) {
+    listItem[index].classList.remove("selected")
+    }
+    event.target.classList.add("selected")
+  }
 }
 
 listaTarefas()
-// input id="texto-tarefa" type="text">
-//   <button id="criar-tarefa"></button>
-//   <ol id="lista-tarefas"></ol>
 
+  // if (event.target.style.backgroundColor !== "rgb(128, 128, 128)") {
+  //   for (index = 0; index < listItem.length; index += 1) { 
+  //     listItem[index].style.backgroundColor = ""  
+  //   }
+  //   event.target.style.backgroundColor = "rgb(128, 128, 128)"
+  //   }
