7cebbd54d2cfab22d9da2c9b869ef054c46101f9
LHenriquerios <henriquerios4@gmail.com>
2021-08-17 16:02:59 -0300

    requistos até o 10 -8

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const criarTarefa = document.getElementById('criar-tarefa');
 const textInput = document.getElementById('texto-tarefa');
 const textItem = document.getElementById('lista-tarefas');
 criarTarefa.addEventListener('click', makeTask);
+
+function deleteAll() {
+  document.getElementById('lista-tarefas').innerHTML = '';
+}
+
+function risk(event) {
+  if (event.target.className !== 'completed') {
+    event.target.className = 'completed';
+  } else {
+    event.target.className = '';
+  }
+}
+
 function makeTask() {
   const itemTarefa = document.createElement('li');
   itemTarefa.innerText = textInput.value;
@@ -9,9 +22,13 @@ function makeTask() {
   document.getElementById('texto-tarefa').value = '';
   itemTarefa.addEventListener('click', () => {
     if (itemTarefa.style.backgroundColor == false) {
-        itemTarefa.style.backgroundColor = 'rgb(128,128,128)';
-  }else{
-    itemTarefa.style.backgroundColor = 'white';
-  }
-  })
+      itemTarefa.style.backgroundColor = 'rgb(128,128,128)';
+    } else {
+      itemTarefa.style.backgroundColor = 'white';
+    }
+  });
+  const buttonDelete = document.getElementById('apaga-tudo');
+  buttonDelete.addEventListener('click', deleteAll);
+  itemTarefa.className = 'not-risk';
+  itemTarefa.addEventListener('dblclick', risk);
 }
