f16499522ec334e4622204486e94bea71c8a749d
Júnior Zanoni <zanoni.odenir@gmail.com>
2021-08-17 15:01:17 -0300

    12-requisito

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ btnCriarTarefa.addEventListener('click', atualizaLista);
 // Apaga a lista de tarefas
 const apagaTudo = document.querySelector('#apaga-tudo');
 apagaTudo.addEventListener('click', () => {
+  localStorage.clear();
   const listaOl = document.querySelector('#lista-tarefas');
   const elementos = document.querySelectorAll('#lista-tarefas li');
   for (let i = 0; i < elementos.length; i += 1) {
@@ -46,11 +47,35 @@ apagaTudo.addEventListener('click', () => {
 });
 
 // Remove as tarefas finalizadas
-const removeFinalizas = document.querySelector('#remover-finalizados');
-removeFinalizas.addEventListener('click', () => {
+const removerFinalizadas = document.querySelector('#remover-finalizados');
+removerFinalizadas.addEventListener('click', () => {
   const listaFinalizadas = document.querySelectorAll('.completed');
   const listaOl = document.querySelector('#lista-tarefas');
   for (let i = 0; i < listaFinalizadas.length; i += 1) {
     listaOl.removeChild(listaFinalizadas[i]);
   }
 });
+
+// Salvar tarefas
+const btnSalvar = document.querySelector('#salvar-tarefas');
+btnSalvar.addEventListener('click', () => {
+  let contador = 0;
+  localStorage.clear();
+  const lista = document.querySelectorAll('li');
+  for (let i = 0; i < lista.length; i += 1) {
+    contador += 1;
+    localStorage.setItem('Contador', contador);
+    localStorage.setItem('Tarefa ' + [i], lista[i].innerText);
+    localStorage.setItem('Classe ' + [i], lista[i].className);
+    localStorage.setItem('backgroundColor ' + [i], lista[i].style.backgroundColor);
+  }
+});
+for (let i = 0; i < localStorage.getItem('Contador'); i += 1) {
+  const criaElemento = document.createElement('li');
+  const lista = document.getElementById('lista-tarefas');
+  const adicionaElemento = lista.appendChild(criaElemento);
+  adicionaElemento.innerText = localStorage.getItem('Tarefa ' + [i]);
+  adicionaElemento.className = localStorage.getItem('Classe ' + [i]);
+  adicionaElemento.style.backgroundColor = localStorage.getItem('backgroundColor ' + [i]);
+  atualizaLista();
+}
