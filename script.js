089c2355ca41d637f04fe05e0eae761351cca3b9
Rodrigo Moreira <rodrigossmoreira@hotmail.com>
2021-08-17 17:34:33 -0300

    exercicio 9

--- script.js
+++ script.js
@@ -8,33 +8,45 @@ let tarefasLista = document.getElementsByClassName('lista');
 // Exercicio 5,6,7,8
 
 function changeBack(event) {
-    if (event.target.className != 'colorBack') { 
-        for (let index = 0; index < tarefasLista.length; index += 1) {
-        tarefasLista[index].classList.remove('colorBack');
-    }
-    event.target.classList.add('colorBack'); 
-        } 
-    }
-    
-    function criaTarefa() {
-        let tarefas = document.createElement('li');
-        tarefas.className = 'lista';
-        tarefas.innerText = tarefasInput.value;
-        listaTarefas.appendChild(tarefas);
-        tarefasInput.value = ''; 
+ if (!event.target.className.includes('colorBack')) { 
+  for (let index = 0; index < tarefasLista.length; index += 1) {
+   tarefasLista[index].classList.remove('colorBack');
+   event.target.classList.add('colorBack'); 
+  }
+ }
+ else if (event.target.className.includes('colorBack')) {
+   event.target.classList.remove('colorBack');  
+ }
+}
 
-        
-        tarefas.addEventListener('click', changeBack)
-    }
+function complitedTask(event) {
+ if (!event.target.className.includes('completed')) {
+  event.target.classList.add('completed')
+  } else {
+  event.target.classList.remove('completed')
+  }
+}
     
-    buttonCriaTarefa.addEventListener('click', criaTarefa);
+function criaTarefa() {
+ let tarefas = document.createElement('li');
+ tarefas.className = 'lista';
+ tarefas.innerText = tarefasInput.value;
+ listaTarefas.appendChild(tarefas);
+ tarefasInput.value = ''; 
+
+tarefas.addEventListener('click', changeBack)
+tarefas.addEventListener('dblclick', complitedTask)
+}
+    
+buttonCriaTarefa.addEventListener('click', criaTarefa);
+//buttonCriaTarefa.addEventListener('keyup', criaTarefa);
 
 // Exercicio 10
 
 botaoApaga.addEventListener('click', apagar);
 
 function apagar() {
-    while (listaTarefas.hasChildNodes()) {
-        listaTarefas.removeChild(listaTarefas.firstChild);
-    }
+ while (listaTarefas.hasChildNodes()) {
+  listaTarefas.removeChild(listaTarefas.firstChild);
+ }
 }
