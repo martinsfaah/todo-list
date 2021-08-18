93bdf9d3bf3785ee2ff6e57b261dda91157b2fa3
Caio César <caiocesarmcruz@gmail.com>
2021-08-18 00:54:08 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ const addBtn = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const ordList = document.getElementById('lista-tarefas');
 
-addBtn.addEventListener('click', addNErase);
 function addNErase() {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = input.value;
-    ordList.appendChild(listItem);
-    input.value = '';
+  const listItem = document.createElement('li');
+  listItem.innerHTML = input.value;
+  ordList.appendChild(listItem);
+  input.value = '';
 }
+addBtn.addEventListener('click', addNErase);
+
+// console.log('epa nenem')
+function paintItGray(event) {
+  let listText = document.getElementsByTagName('li');
+  for (let i = 0; i < listText.length; i += 1) {
+  if (listText[i].style.backgroundColor = 'gray') {
+      listText[i].style.backgroundColor = 'white';
+      event.target.style.backgroundColor = 'gray';
+  }
+  }
+}
+ordList.addEventListener('click', paintItGray);
