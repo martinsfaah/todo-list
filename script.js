bebccf8293757ae3ad6b6789daf568095b7b6956
lipegois <luizz3.14@hotmail.com>
2021-08-17 22:19:23 -0500

    Requisito 9

--- script.js
+++ script.js
@@ -5,27 +5,41 @@ const toDoList = document.getElementById('lista-tarefas');
 function createListItems() {
   const createItem = document.createElement('li');
   const inputText = document.getElementById('texto-tarefa').value;
-  createItem.className = 'items';
+
   toDoList.appendChild(createItem);
   createItem.innerText = inputText;
 }
 
+/* Função para limpar o input */
 function clear() {
   document.getElementById('texto-tarefa').value = '';
 }
 
+/* Função para reset */
+function reset() {
+  const elementList = document.querySelectorAll('li');
+  for (let index = 0; index < elementList.length; index += 1) {
+    document.querySelectorAll('li')[index].style.backgroundColor = '';
+  }
+}
+
+/* Função para alterar o backgrounColor do item da lista */
 function changeColor(event) {
   reset();
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
+/* Função para riscar a linha */
+function taskCompleted(event) {
+  reset();
+  if (event.target.className !== 'completed') {
+    event.target.className = 'completed';
+  } else {
+    event.target.className = '';
+  }
+}
+
 buttonClick.addEventListener('click', createListItems);
 buttonClick.addEventListener('click', clear);
 toDoList.addEventListener('click', changeColor);
-
-function reset() {
-  const elementList = document.querySelectorAll('li');
-  for(let index = 0; index < elementList.length; index += 1) {
-    document.querySelectorAll('li')[index].style.backgroundColor = '';
-  }
-}
+toDoList.addEventListener('dblclick', taskCompleted);
