96e4f56e6892b6cfcdbf3619dc7b011fe90fcb46
herzogcassia <cassiahherzog@gmail.com>
2021-08-18 00:15:59 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-window.onload = function(){
-
-}
 
 const listOfOrder = document.querySelector("#lista-tarefas")
 const createOrder = document.querySelector("#criar-tarefa")
 const inputButton = document.querySelector("#texto-tarefa")
+const sectionButtonAll = document.getElementById("button-all")
 
 function createListOrder() {
   const elementList = document.createElement("li");
@@ -26,3 +24,23 @@ function markAndUnmark(evento) {
  evento.target.classList.toggle ("completed")
 }
 listOfOrder.addEventListener("dblclick", markAndUnmark);
+
+function CreateButtonClearAll(evento) {
+  const buttonClearAll = document.createElement("button");
+  console.log(buttonClearAll);
+  buttonClearAll.id = "apaga-tudo"
+  buttonClearAll.innerText = "Botão de apagar";
+  sectionButtonAll.appendChild(buttonClearAll);
+  
+  buttonClearAll.addEventListener("click", clearList);
+}
+CreateButtonClearAll();
+
+function clearList(){
+  while (listOfOrder.lastChild) {
+    listOfOrder.removeChild(listOfOrder.lastChild);
+  }
+}
+
+// localButton
+// buttonClearAll
