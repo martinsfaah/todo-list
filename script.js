93d27e3e9c5d9c9365c30efe146f0aacfcebccf5
Regiane Santos <re.cfs235@gmail.com>
2021-08-18 13:27:58 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let campoEntrada = document.getElementById('texto-tarefa'); // Pegar entrada do usuario
-let botaoTarefas = document.getElementById('criar-tarefa'); // Botão
-let lista = document.getElementById('lista-tarefas'); // Lista
+const campoEntrada = document.getElementById('texto-tarefa'); // Pegar entrada do usuario
+const botaoTarefas = document.getElementById('criar-tarefa'); // Botão
+const lista = document.getElementById('lista-tarefas'); // Lista
 
 function criarTarefa() {
   const novaTarefa = document.createElement('li');
@@ -8,20 +8,31 @@ function criarTarefa() {
   lista.appendChild(novaTarefa);
   campoEntrada.value = '';
 }
-  
+
 botaoTarefas.addEventListener('click', criarTarefa);
 
 // Requisito 7
 
+function mudarCor(event) {
+  const tagTarefa = document.querySelector('.liSelecionada');
 
-function mudarCor (event){
-  let tagTarefa = document.querySelector(".liSelecionada");
-
-  if (tagTarefa !== null){
-    tagTarefa.classList.remove("liSelecionada");
+  if (tagTarefa !== null) {
+    tagTarefa.classList.remove('liSelecionada');
   }
-console.log(tagTarefa);
-  event.target.classList.add("liSelecionada");
+
+  event.target.classList.add('liSelecionada');
 }
 
 lista.addEventListener('click', mudarCor);
+
+// Requisito 9
+
+function duploRisco(event){
+  if(event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+
+lista.addEventListener('dblclick', duploRisco);
