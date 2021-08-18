2006839b8eca2567c12f662b81cd78ac651bb42d
Gabriel <gncorreia@protonmail.com>
2021-08-18 14:05:59 -0300

    requisito 8 feito

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let button = document.querySelector('#criar-tarefa');
 let input = document.querySelector('#texto-tarefa');
-let ol = document.querySelector('#lista-tarefas')
-ol.style.backgroundColor = 
-
+let ol = document.querySelector('#lista-tarefas') 
+let cont = 0
 // Requisito 5 e 6:
 // Adicionar na ol com click
 button.addEventListener('click', function() {  
@@ -11,22 +10,32 @@ button.addEventListener('click', function() {
   } else {
     let li = document.createElement('li');
     li.innerText = input.value;
+    li.className = 'list-item'
+    if (cont === 0) {
+      li.className = 'list-item selected'
+    }
     li.addEventListener('click', addBackgroundColor)
     ol.appendChild(li)
     input.value = '';
+    cont += 1;
   }
 })
 // Adicionar na ol com enter
 input.addEventListener('keyup', function(event) {
   if (event.keyCode === 13) {
     if (input.value === '') {
-      alert('Digite algo!')
+      alert('Digite algo!');
     } else {
       let li = document.createElement('li');
       li.innerText = input.value;
-      li.addEventListener('click', addBackgroundColor)
-      ol.appendChild(li)
+      li.className = 'list-item'
+      if (cont === 0) {
+        li.className = 'list-item selected'
+      }
+      li.addEventListener('click', addBackgroundColor);           
+      ol.appendChild(li);
       input.value = '';
+      cont += 1;
     }
   }      
 })
@@ -36,19 +45,35 @@ function addBackgroundColor(event) {
   const newColor = "rgb(128, 128, 128)";
   const defaultColor = 'white';
   let eventTargetBgColor = event.target.style.backgroundColor;
-  
-  if (eventTargetBgColor !== newColor) {
-    event.target.style.backgroundColor = newColor;    
-  } else if (eventTargetBgColor === newColor) {
-    event.target.style.backgroundColor = defaultColor;  
+
+  // Requisito 8:
+  const selected = document.querySelector('.selected');  
+  selected.classList.remove('selected');
+  event.target.classList.add('selected')
+
+  let allItems = document.querySelectorAll('.list-item')  
+
+  for (let index = 0; index < allItems.length; index += 1) {
+    if (allItems[index].classList.length > 1) {
+      event.target.style.backgroundColor = newColor;
+    } else {
+      allItems[index].style.backgroundColor = defaultColor;
+    }
   }
 }
 
-// window.addEventListener('click', function(event) {
-//   console.log(event);
-//   const testeCor = event.target.style.backgroundColor;
-//   // const testElementoSelecionado = event.target;
-//   console.log(testeCor);
-//   // console.log(testElementoSelecionado);
-// })
-  
+// Requisito 8:
+// function addClassSelected(event) {
+//   const selected = document.querySelector('.selected');
+//   selected.classList.remove('selected');
+//   event.target.classList.add('selected');
+//   // selected.style.backgroundColor = 'white'
+// }
+
+window.addEventListener('click', function(event) {
+  console.log(event);
+  const testeCor = event.target.style.backgroundColor;
+  // const testElementoSelecionado = event.target;
+  console.log(testeCor);
+  // console.log(testElementoSelecionado);
+})
