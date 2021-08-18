510010853437ad921325b5616d1de313b110989f
leonardo <leonardo.siepierski@outlook.com>
2021-08-17 21:01:25 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-// requisito 5 
+// requisito 5
 
-let botaoTarefa = document.getElementById("criar-tarefa");
-let inputTarefa = document.getElementById("texto-tarefa");
-let listaTarefa = document.getElementById("lista-tarefas");
+let botaoTarefa = document.getElementById('criar-tarefa');
+let inputTarefa = document.getElementById('texto-tarefa');
+let listaTarefas = document.getElementById('lista-tarefas');
 
 botaoTarefa.addEventListener('click', criaItem); // clicar no botão chama a função que adiciona o li à ol e limpa o campo de input de texto
 
 function criaItem() {
-  let itemLista = document.createElement("li"); // variável para criar uma <li>
+  let itemLista = document.createElement('li'); // variável para criar uma <li>
   itemLista.innerHTML = inputTarefa.value; // coloca o valor do input dentro da li
-  listaTarefa.appendChild(itemLista); // adiciona o li criado à ol
-  inputTarefa.value = ""; // limpa campo de input
+  listaTarefas.appendChild(itemLista); // adiciona o li criado à ol
+  inputTarefa.value = ''; // limpa campo de input
+}
+
+// requisito 10
+
+let botaoApagar = document.getElementById('apaga-tudo');
+
+botaoApagar.addEventListener('click', limpaLista); // clicar no botao de apagar chama a função que remove os li's (childs) da ol
+
+function limpaLista() {
+  let listaCompleta = listaTarefas.children.length; // variável que comporta a length da ol
+  for (let index = 0; index < listaCompleta; index += 1) {
+    // remove cada child (li) da ol através do for
+    listaTarefas.removeChild(listaTarefas.firstChild);
+  }
 }
