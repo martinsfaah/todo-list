8ddcd2bfedf861d6b317af14f7999108088cc3f1
carla <carlabazevedoa@gmail.com>
2021-08-18 00:16:38 -0300

    mexendo no css

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-document.getElementById('criar-tarefa').addEventListener('click', function( event ) {
-	const usersText = document.getElementById('texto-tarefa').value;
-	const li = document.createElement('li');
-	li.textContent = usersText;
-	document.getElementById('lista-tarefas').appendChild(li)
-	document.getElementById('texto-tarefa').value = "";
-})
+document.getElementById('criar-tarefa').addEventListener('click', function () {
+  const usersText = document.getElementById('texto-tarefa').value;
+  const li = document.createElement('li');
+  li.textContent = usersText;
+  document.getElementById('lista-tarefas').appendChild(li);
+  document.getElementById('texto-tarefa').value = "";
+});
 
-document.aloand = function () {
-}
+
