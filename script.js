514103f3692d3943c1a514ca7faf632075b0be4e
Gabriel <gncorreia@protonmail.com>
2021-08-17 23:18:40 -0300

    requisito 7 feito

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
-// Requisito 5:
 let button = document.querySelector('#criar-tarefa');
 let input = document.querySelector('#texto-tarefa');
 let ol = document.querySelector('#lista-tarefas')
+ol.style.backgroundColor = 
 
-
-button.addEventListener('click', function() {
-  let li = document.createElement('li');
-  li.innerText = input.value;
-
-  ol.appendChild(li)
-  input.value = '';
+// Requisito 5 e 6:
+// Adicionar na ol com click
+button.addEventListener('click', function() {  
+  if (input.value === '') {
+    alert('Digite algo!')
+  } else {
+    let li = document.createElement('li');
+    li.innerText = input.value;
+    li.addEventListener('click', addBackgroundColor)
+    ol.appendChild(li)
+    input.value = '';
+  }
 })
+// Adicionar na ol com enter
+input.addEventListener('keyup', function(event) {
+  if (event.keyCode === 13) {
+    if (input.value === '') {
+      alert('Digite algo!')
+    } else {
+      let li = document.createElement('li');
+      li.innerText = input.value;
+      li.addEventListener('click', addBackgroundColor)
+      ol.appendChild(li)
+      input.value = '';
+    }
+  }      
+})
+
+// Requisito 7:
+function addBackgroundColor(event) {
+  const newColor = "rgb(128, 128, 128)";
+  const defaultColor = 'white';
+  let eventTargetBgColor = event.target.style.backgroundColor;
+  
+  if (eventTargetBgColor !== newColor) {
+    event.target.style.backgroundColor = newColor;    
+  } else if (eventTargetBgColor === newColor) {
+    event.target.style.backgroundColor = defaultColor;  
+  }
+}
+
+// window.addEventListener('click', function(event) {
+//   console.log(event);
+//   const testeCor = event.target.style.backgroundColor;
+//   // const testElementoSelecionado = event.target;
+//   console.log(testeCor);
+//   // console.log(testElementoSelecionado);
+// })
+  
