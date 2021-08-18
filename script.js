409a2ec382bfabfcc02b9addb8fc591d3df9285b
Renato Adorno <renatowillian.2000@gmail.com>
2021-08-18 13:22:21 -0300

    requisito-5 e 6

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-function clickToDo() {
-  const btnText = document.querySelector('#criar-tarefa');
-  const text = document.querySelector('#texto-tarefa');
-  const list = document.querySelector('#lista-tarefas');
-  const item = document.createElement('li');
-  
-  btnText.addEventListener('click', function() {
-    if (text !== "") {
-      item.innerHTML = text;
-      list.append(item);
+window.onload = function() {
+  addList();
+  function addList() {
+    const btnText = document.querySelector('#criar-tarefa');
+    btnText.addEventListener('click', addList);
+    function addList(){ //https://stackoverflow.com/questions/19908215/adding-user-input-to-a-list-of-text-items-on-a-html-page/42950658
+      const li = document.createElement("li");
+      let text = document.querySelector("input").value; 
+      const textnode = document.createTextNode(text);
+      li.appendChild(textnode);
+      const list = document.querySelector("#lista-tarefas");
+      list.appendChild(li);
+      document.querySelector("input").value = '';
+      }
     }
-  });
 }
-clickToDo();
+
+
+
