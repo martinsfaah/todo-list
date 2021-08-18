8f148fcdcd6c20279286e783cdec624254bd0df9
Omar Merljak <omerljak@gmail.com>
2021-08-18 08:30:36 -0300

    atuliza erro test npm

--- script.js
+++ script.js
@@ -9,14 +9,43 @@ botaoCriaTarefa()
 
 const buscalistaTarefas = document.getElementById("lista-tarefas");
 const buscaTextoInput = document.getElementById('texto-tarefa')
-const eventoBotao = document.querySelector('#criar-tarefa'); 
+const eventoBotao = document.querySelector('#criar-tarefa');
+const pintalinha = document.getElementsByClassName('line');
 /* criar constantes de uso comum fora da função facilita pois podem ser usadas entre funções */
 function criaLi() {
   let criaLi = document.createElement('li');
+  criaLi.className = 'line';
   criaLi.innerText = buscaTextoInput.value
   buscaTextoInput.value = '';
   buscalistaTarefas.appendChild(criaLi);
 }
-criaLi()
-
 eventoBotao.addEventListener('click', criaLi)
+
+/* function corLi(event) {//ta errado..tenho que criar uma classe dinamica
+  for (let i = o; i < line.length; i += 1) {
+    if (!line.style.backgroundColor === 'rgb(128, 128, 128') {
+    } else {
+      event.target.style.backgroundColor = 'rgb(128, 128, 128';
+    }
+  }
+} */
+
+function retornaCor(event) {
+  let linha = event.target
+  if (!linha.style.backgroundColor === 'rgb(128, 128, 128)')
+    event.target.classList.add('select');
+    linha.appendChild(linha)
+} 
+
+function riscaLi(dpclic) {
+  let linha = dpclic.target
+  linha.style.strike(); // duvida se strike esta sendo usado corretamente
+}
+
+function buscaLi() {
+  for (let linha of pintaLinha) {
+    linha.addEventListener('click', corLi)
+    linha.addEventListener('click', retornaCor)
+    linha.addEventListener('dbclick', riscaLi)
+  }
+}
