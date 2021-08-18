61c0d3939c4c16ef37b49c532fb6496509060f9b
Thiago Lordello <thiagolordello@hotmail.com>
2021-08-18 08:29:09 -0300

    2a questao

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function insertHeader() {
     getBody.appendChild(titulo);
 }
 insertHeader();
+
+function insertP() {
+    let getBody = document.querySelector('body');
+    let paragrafo = document.createElement('p');
+    paragrafo.id = ('funcionamento');
+    paragrafo.innerText = ('Clique duas vezes em um item para marcá-lo como completo');
+    paragrafo.innerHTML;
+    getBody.appendChild(paragrafo);
+}
+insertP();
