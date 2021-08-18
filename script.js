8df68a53d04bd80d63f317c2db4229a8458ebd3a
Leandro Basilio <leandroab07@gmail.com>
2021-08-18 01:22:01 -0300

    Adiciona evento que muda a cor de item da lista quando clicado

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ btnAdicionar.addEventListener('click', () => {
   let tarefa = document.getElementById("texto-tarefa").value;
   let tarefaNova = document.createElement('li');
   tarefaNova.innerHTML = document.getElementById("texto-tarefa").value;
+  tarefaNova.addEventListener('click', backgroundGray);
 
   lista.appendChild(tarefaNova);
 
-  document.getElementById('texto-tarefa').value=''
-})
+  document.getElementById('texto-tarefa').value = '';
+});
+
+function backgroundGray(Event) {
+  Event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
