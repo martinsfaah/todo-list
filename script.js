a9bf5c3a53240a45b8de2b5ce2cf41c617d8448e
Lucas Macedo <lucasrrm22@gmail.com>
2021-08-19 19:09:26 -0300

    requisito 8

--- script.js
+++ script.js
@@ -5,16 +5,29 @@ const listOfTasks = document.querySelector('#lista-tarefas')
 button.addEventListener('click', addTask)
 function addTask() {
     let task = document.createElement('li');
+    task.className = 'notSelected'
     task.innerHTML = input.value;
     input.value = '';
     listOfTasks.appendChild(task);
 }
 listOfTasks.addEventListener('click', greyColor);
 function greyColor(event) {
+    let notSelected = document.querySelector('.notSelected')
+    let white = getComputedStyle(notSelected)
+    let allTask = document.getElementsByTagName('li')
+    for (i = 0; i < allTask.length; i +=1) {
+        allTask[i].className = 'notSelected'
+    }
     event.target.className = 'selected'
-    let selectedTask = document.querySelector('.selected')
-    let style = getComputedStyle(selectedTask)
-    event.target.style.backgroundColor = style.backgroundColor
+    let selected = document.querySelector('.selected')
+    let style = getComputedStyle(selected)
+    for (let l = 0; l < allTask; l += 1) {
+        if (allTask[l].className == 'notSelected') {
+            allTask[l].style.backgroundColor = 'white'
+        } else {
+            allTask[l].style.backgroundColor = style.backgroundColor
+        }
+    }
+    
 }
 }
-
