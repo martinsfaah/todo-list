73fd815a7614c60594790c602dfeb9b22bc20442
Arthur <arthurteixeira0805@gmail.com>
2021-08-18 21:25:59 -0300

    req 7 8 9 done

--- script.js
+++ script.js
@@ -5,8 +5,28 @@ const addTaskButton = document.getElementById('criar-tarefa');
 function getInputToList() {
     const newTask = document.createElement('li');
     newTask.innerText = taskAdded.value;
+
+    
     list.appendChild(newTask);
+    newTask.addEventListener('click', selection)
+    newTask.addEventListener('dblclick', dbselection)
     taskAdded.value = '';
 }
 
-addTaskButton.addEventListener('click', getInputToList)
+// consultado o repositorio https://github.com/tryber/sd-015-a-project-todo-list/pull/1/commits/0253bcab60342d2066b58d112dab124ebd6ac5cf
+function selection (ev) {
+    if (document.querySelector('.select')) {
+        document.querySelector('.select').classList.remove('select');
+    }
+    ev.target.classList.add('select');
+}
+
+function dbselection (ev) {
+    if (ev.target.className.includes('completed')) {
+        document.querySelector('.completed').classList.remove('completed');
+    } else {
+    ev.target.classList.add('completed');
+    }
+}
+
+addTaskButton.addEventListener('click', getInputToList);
