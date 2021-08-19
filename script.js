0cf9950486ba1f63d7cf7d29f32a900e3863fd66
Leandro Basilio <leandroab07@gmail.com>
2021-08-19 00:14:37 -0300

    Colore o fundo de apenas um elemento da list

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 let btnAdicionar = document.getElementById('criar-tarefa');
 let lista = document.getElementById('lista-tarefas');
 
-btnAdicionar.addEventListener('click', () => {
+btnAdicionar.addEventListener('click', adicionaTarefa) 
+
+function adicionaTarefa() {
   let tarefa = document.getElementById("texto-tarefa").value;
   let tarefaNova = document.createElement('li');
-  tarefaNova.innerHTML = document.getElementById("texto-tarefa").value;
-  tarefaNova.addEventListener('click', backgroundGray);
+  tarefaNova.innerHTML = tarefa;
 
   lista.appendChild(tarefaNova);
+  tarefaNova.addEventListener('click', setBackgroundGray)
 
   document.getElementById('texto-tarefa').value = '';
-});
+};
 
-function backgroundGray(Event) {
-  Event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-}
+function setBackgroundGray(evento) {
+  let classLi = document.querySelector('.backgroundGray');
+  
+  if (classLi) {
+    classLi.classList.remove('backgroundGray');
+  }
+  
+  evento.target.classList.add('backgroundGray');
+};
